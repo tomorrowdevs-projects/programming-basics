339934511ab55615920badebcf54c0a545339479
EnryZob <83153234+EnryZob@users.noreply.github.com>
2021-10-05 12:21:11 +0200

    Delete solution_units_of_a_time_again.py

--- projects/m1/005-units-of-time-again/enricozoboli/solution_units_of_a_time_again.py
+++
@@ -1,8 +0,0 @@
-SECONDS_IN_A_DAY = 86400
-
-seconds = int(input("Welcome! Insert the total of seconds: "))
-
-days = seconds // SECONDS_IN_A_DAY
-hours = (seconds % SECONDS_IN_A_DAY) // (SECONDS_IN_A_DAY / 24)
-minutes = ()
-
