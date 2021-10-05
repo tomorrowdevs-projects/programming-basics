18b057cc9e56d701e1d171fea3a2245ae670fa75
Enrico <enricozob@gmail.com>
2021-10-05 10:35:09 +0200

    Fixed solution of making change

---
+++ projects/m1/004-units-of-time/enricozoboli/solution_units_of_time.py
@@ -0,0 +1,17 @@
+SECONDS_IN_A_MINUTE = 60
+SECONDS_IN_AN_HOUR = 3600
+
+
+# Ask the user for the number of days , hours, minutes and seconds  
+number_of_days = int("Insert number of days: ")
+number_of_hours = int("Insert number of hours: ")
+number_of_minutes = int("Insert number of minutes: ")
+number_of_seconds = int("Insert number of seconds: ")
+
+# Compute the total of seconds
+seconds_in_a_day = number_of_days * SECONDS_IN_AN_HOUR * 24
+seconds_in_an_hour = number_of_hours * SECONDS_IN_AN_HOUR
+
+seconds_in_a_minutes = number_of_minutes * SECONDS_IN_A_MINUTE) +
+
+
