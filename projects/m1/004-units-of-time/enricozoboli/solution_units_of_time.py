2cade4fab55c8be27a5786142d65da29e1361850
Enrico <enricozob@gmail.com>
2021-10-05 11:00:49 +0200

    Solution for unit of time exercise

--- projects/m1/004-units-of-time/enricozoboli/solution_units_of_time.py
+++ projects/m1/004-units-of-time/enricozoboli/solution_units_of_time.py
@@ -1,17 +1,18 @@
 SECONDS_IN_A_MINUTE = 60
-SECONDS_IN_AN_HOUR = 3600
-
 
 # Ask the user for the number of days , hours, minutes and seconds  
-number_of_days = int("Insert number of days: ")
-number_of_hours = int("Insert number of hours: ")
-number_of_minutes = int("Insert number of minutes: ")
-number_of_seconds = int("Insert number of seconds: ")
+number_of_days = int(input("Insert number of days: "))
+number_of_hours = int(input("Insert number of hours: "))
+number_of_minutes = int(input("Insert number of minutes: "))
+number_of_seconds = int(input("Insert number of seconds: "))
 
-# Compute the total of seconds
-seconds_in_a_day = number_of_days * SECONDS_IN_AN_HOUR * 24
-seconds_in_an_hour = number_of_hours * SECONDS_IN_AN_HOUR
+# Compute the total of seconds for days, hour and minute
+total_day_seconds = number_of_days * 24 * 60  * SECONDS_IN_A_MINUTE
+total_hour_seconds = number_of_hours * 60  * SECONDS_IN_A_MINUTE
+total_minutes_seconds = number_of_minutes * SECONDS_IN_A_MINUTE
 
-seconds_in_a_minutes = number_of_minutes * SECONDS_IN_A_MINUTE) +
+# Compute the total of seconds 
+total_of_seconds = (total_day_seconds + total_hour_seconds 
+    + total_minutes_seconds + number_of_seconds)
 
-
+print(total_of_seconds)
