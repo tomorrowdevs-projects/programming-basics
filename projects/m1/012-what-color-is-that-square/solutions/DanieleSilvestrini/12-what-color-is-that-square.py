5e75cda4c7de926f2f0767e24bbec27a0638c069
Daniele <danieleslvstrn@gmail.com>
2021-10-16 17:07:17 +0200

    corrections to ex 12

--- projects/m1/012-what-color-is-that-square/solutions/DanieleSilvestrini/12-what-color-is-that-square.py
+++ projects/m1/012-what-color-is-that-square/solutions/DanieleSilvestrini/12-what-color-is-that-square.py
@@ -1,34 +1,13 @@
 col, row = (input('Insert column and row separated by a whitespace:\n').lower()).split()
 
-if col == 'a' and (int(row)) % 2 == 0:
+col_black = ['a', 'c', 'e', 'g']
+col_white = ['b', 'd', 'f', 'h']
+
+if col in col_black and (int(row)) % 2 == 0:
     print('white')
-elif col == 'a' and (int(row)) % 2 > 0:
+elif col in col_black and (int(row)) % 2 > 0:
     print('black')
-elif col == 'b' and (int(row)) % 2 == 0:
+elif col in col_white and (int(row)) % 2 == 0:
     print('black')
-elif col == 'b' and (int(row)) % 2 > 0:
-    print('white')
-elif col == 'c' and (int(row)) % 2 == 0:
-    print('white')
-elif col == 'c' and (int(row)) % 2 > 0:
-    print('black')
-elif col == 'd' and (int(row)) % 2 == 0:
-    print('black')
-elif col == 'd' and (int(row)) % 2 > 0:
-    print('white')
-elif col == 'e' and (int(row)) % 2 == 0:
-    print('white')
-elif col == 'e' and (int(row)) % 2 > 0:
-    print('black')
-elif col == 'f' and (int(row)) % 2 == 0:
-    print('black')
-elif col == 'f' and (int(row)) % 2 > 0:
-    print('white')
-elif col == 'g'and (int(row)) % 2 == 0:
-    print('white')
-elif col == 'g' and (int(row)) % 2 > 0:
-    print('black')
-elif col == 'h'and (int(row)) % 2 == 0:
-    print('black')
-elif col == 'h' and (int(row)) % 2 > 0:
+elif col in col_white and (int(row)) % 2 > 0:
     print('white')
