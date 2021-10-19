ff48a02412ced2cc0cf911ee737938237286dc82
ergirius <91219864+ergirius@users.noreply.github.com>
2021-10-19 09:52:26 +0200

    esercio/009

---
+++ projects/m1/009-vowel-or-consonant/solution/matteobisicchia/009-vowel-or-consonant.py
@@ -0,0 +1,15 @@
+letter =input("inserisci una lettera : ")
+if (letter==("a")):
+	print("la lettera inserita è una vocale")
+elif (letter==("e")):
+	print("la lettera e una vocale ")
+elif (letter==("i")):
+	print("la lettera e una vocale ")
+elif (letter==("o")):
+	print("la lettera e una vocale ")
+elif (letter==("u")):
+	print("la lettera e una vocale ")
+elif (letter==("y")):
+	print("la lettera e una vocale che una consonate")
+else :
+	print("la lettera è una consonate")
