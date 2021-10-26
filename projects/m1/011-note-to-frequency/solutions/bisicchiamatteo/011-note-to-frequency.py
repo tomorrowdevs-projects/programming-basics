bef5e4d8fe6a8c99fa9d10765720426ec80edfc1
ergirius <91219864+ergirius@users.noreply.github.com>
2021-10-26 16:10:28 +0200

    esercizio 11 modificato

--- projects/m1/011-note-to-frequency/solutions/bisicchiamatteo/011-note-to-frequency.py
+++ projects/m1/011-note-to-frequency/solutions/bisicchiamatteo/011-note-to-frequency.py
@@ -6,47 +6,25 @@ G=24.50
 A=27.50
 B=30.87
 notes =input("inserisci una nota (C,D,E,F,G,A,B) con il valore di un ottava (da 0 fino a 9) : ")
-if (notes[0]==("C")):
-	if (int(notes[1])!=0):
-		frequency=C*pow (2,int(notes[1]))
-	else :
-		frequency=C
-	print("la nota " + str(notes)+ " ha una frequeza di : " + str(frequency))
-elif (notes[0]==("D")):
-	if (int(notes[1])!=0):
-		frequency=D*pow(2,int(notes[1]))
-	else :
-		frequency=D
-	print("la nota " + str(notes)+ " ha una frequeza di : " + str(frequency))
-elif (notes[0]==("E")):
-	if (int(notes[1])!=0):
-		frequency=E*pow(2,int(notes[1]))
-	else :
-		frequency=E
-	print("la nota " + str(notes)+ " ha una frequeza di : " + str(frequency))
-elif (notes[0]==("F")):
-	if (int(notes[1])!=0):
-		frequency=F*pow(2,int(notes[1]))
-	else :
-		frequency=F
-	print("la nota " + str(notes)+ " ha una frequeza di : " + str(frequency))
-elif (notes[0]==("G")):
-	if (int(notes[1])!=0):
-		frequency=G* pow(2,int(notes[1]))
-	else :
-		frequency=G
-	print("la nota " + str(notes)+ " ha una frequeza di : " + str(frequency))
-elif (notes[0]==("A")):
-	if (int(notes[1])!=0):
-		frequency=A*pow(2,int(notes[1]))
-	else :
-		frequency=A
-	print("la nota " + str(notes)+ " ha una frequeza di : " + str(frequency))
-elif (notes[0]==("B")):
-	if (int(notes[1])!=0):
-		frequency=B*pow(2,int(notes[1]))
-	else :
-		frequency=B
-	print("la nota " + str(notes)+ " ha una frequeza di : " + str(frequency))
+if (notes.upper()[0]==("C")):
+	temp=C
+elif (notes.upper()[0]==("D")):
+	temp=D
+elif (notes.upper()[0]==("E")):
+	temp=E
+elif (notes.upper()[0]==("F")):
+	temp=F
+elif (notes.upper()[0]==("G")):
+	temp=G
+elif (notes.upper()[0]==("A")):
+	temp=A
+elif (notes.upper()[0]==("B")):
+	temp=B
 else :
-	print("il valore inserito non è una nota")
+	print("il valore inserito non ï¿½ una nota")
+
+if (int(notes[1])!=0):
+		frequency=temp*pow(2,int(notes[1]))
+else :
+	frequency=temp
+print("la nota " + str(notes)+ " ha una frequeza di : " + str(frequency))
