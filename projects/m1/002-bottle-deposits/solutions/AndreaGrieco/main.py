42b5f16d6f41c2eb74accd938d7facb997971803
DevCat2001 <andreagrieco01@gmail.com>
2021-11-13 12:03:11 +0100

    now it works 100%!

--- projects/m1/002-bottle-deposits/solutions/AndreaGrieco/main.py
+++ projects/m1/002-bottle-deposits/solutions/AndreaGrieco/main.py
@@ -6,15 +6,16 @@
 # Format the output so that it includes a dollar sign and two digits to the right of the decimal point.
 print("Welcome in this deposit!\nAdd all the bottle you can, when you finish, we'll give you some moneyy $_$")
 
+container = 1
+deposit = 0
+
 while container != 0:
-  container = input("how much liter this bottle can contain? (put 0 to finish the program and have some moneyy)")
+  container = float(input("how much liter this bottle can contain? (put 0 to finish the program and have some moneyy)"))
   if container != 0:
     if container <= 1:
       deposit = deposit + 0.10
     elif container > 1:
       deposit = deposit + 0.25
   else:
-    print("yay! you have"+deposit+"$!!")
-
-
-
+    result = str(format(round(deposit,2),".2f"))
+    print("yay! you have"+result+"$!!")
