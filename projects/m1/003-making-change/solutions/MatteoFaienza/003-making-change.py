1ff0386b51eb08f6b831ccc512ea3e60dff5d0c3
MatteoFaienza <matteofaienza87@gmail.com>
2021-11-14 12:41:37 +0100

    start exercise 003 making change

---
+++ projects/m1/003-making-change/solutions/MatteoFaienza/003-making-change.py
@@ -0,0 +1,24 @@
+
+cashInserted=float(input('Total cash inserted '))
+
+payment=float(input('The total to be paid is '))
+
+rest=cashInserted-payment
+
+penny=int(rest*100)
+
+toonies=int(penny/200)
+
+loonies=int((penny-(200*toonies))/100)c
+
+print(toonies)
+
+print(loonies)
+
+
+
+
+
+
+
+
