183aac59312e43383313d6fb43c006fa065f2408
Enrico <enricozob@gmail.com>
2021-11-14 19:32:39 +0100

    Merged two functions in extraction

--- projects/m2/010-random-lottery-numbers/solutions/enricozoboli/solution_random_lottery_numbers.py
+++ projects/m2/010-random-lottery-numbers/solutions/enricozoboli/solution_random_lottery_numbers.py
@@ -1,36 +1,27 @@
 import random
 
-def ticket():
-    ticket_numbers = []
-    while len(ticket_numbers) < 6:
-        num = random.randint(1, 49)
-        if num in ticket_numbers:
-            continue
-        else:
-            ticket_numbers.append(num)
-    ticket_numbers.sort()
-    return ticket_numbers
-
-def lottery():
+def extraction():
     extraction_numbers = []
     while len(extraction_numbers) < 6:
-        num = random.randint(1, 49)
-        if num in extraction_numbers:
-            continue
-        else:
-            extraction_numbers.append(num)
+        extraction_number = random.randint(1, 49)
+        if extraction_number not in extraction_numbers:
+            extraction_numbers.append(extraction_number)
     extraction_numbers.sort()
-    return extraction_numbers
+    return  extraction_numbers
 
-if __name__ == "__main__":
-    count = 0
+def main(): 
+    count = 1
     while True:
-        if lottery() != ticket():
-            count += 1
-        else:
+        ticket_numbers = extraction()
+        extraction_numbers =  extraction()
+        if sorted(ticket_numbers) == sorted(extraction_numbers):
+            print(f"You win with {ticket_numbers}")
             print(f"It takes {count} extraction to win")
             break
-
-#Made two extraction: 
-# 1--> "It takes 9850927 extraction to win" in 78.734 seconds
-# 2--> "It takes 47429413 extraction to win" in 387.301 seconds
+        else:
+            count += 1
+    print(ticket_numbers)
+    print(extraction_numbers)
+       
+if __name__ == "__main__":
+    main()
