7e4ad8da097d5e7d840218b79488c0f9c98649bf
ergirius <91219864+ergirius@users.noreply.github.com>
2021-11-17 09:28:58 +0100

    esercizio/025

---
+++ projects/m1/025-caesar-cipher/solutions/bisicchiamatteo/025-caesar-cipher.py
@@ -0,0 +1,23 @@
+import string 
+
+i=0
+new_period=""
+
+alfabeth= list(string.ascii_lowercase)
+
+
+number= input ("inserisci il numero di codifica per il cifrario ")
+new_alfabeth=alfabeth[int(number):]+alfabeth[:int(number)]
+print (alfabeth)
+print (new_alfabeth)
+period=input("inserisci la frase che vuoi codificare :")
+point = len(period)
+period_list=list(period)
+
+for i in range(point) :
+    letter= period_list[i]
+    j=alfabeth.index(letter)
+    new_period = new_period+"".join(new_alfabeth[j])
+    i =i+1
+    
+print(new_period)      
