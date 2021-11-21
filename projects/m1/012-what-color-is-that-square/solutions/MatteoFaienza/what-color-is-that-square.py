e76bc6e0fda7ee1cc5030a53886ae9a998eb848f
MatteoFaienza <matteofaienza87@gmail.com>
2021-11-21 21:04:38 +0100

    solution/m1/012/what a color is that square /modified

--- projects/m1/012-what-color-is-that-square/solutions/MatteoFaienza/what-color-is-that-square.py
+++ projects/m1/012-what-color-is-that-square/solutions/MatteoFaienza/what-color-is-that-square.py
@@ -3,11 +3,11 @@ chessPosition=input('Enter the position of the board with the capital letter and
 # Through nested conditions I get the black or white square
 if chessPosition[0] in 'ACEG':
     if int(chessPosition[1]) % 2 != 0:
-        print('The chosen square is white')
+        print('The chosen square is black')
     else:
         print('The chosen square is white')
 elif chessPosition[0]:
     if int(chessPosition[1]) % 2== 0:
-        print('The chosen square is white')
+        print('The chosen square is black')
     else:
         print('The chosen square is white')    
