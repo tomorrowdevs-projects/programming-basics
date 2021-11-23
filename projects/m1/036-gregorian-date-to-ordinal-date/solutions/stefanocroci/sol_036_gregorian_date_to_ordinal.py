5ee74b1ca2d618e371d9c32dbaddc816ef11d5ce
stefanocroci14 <crocistefano14@gmail.com>
2021-11-23 22:26:16 +0100

    Added comments

--- projects/m1/036-gregorian-date-to-ordinal-date/solutions/stefanocroci/sol_036_gregorian_date_to_ordinal.py
+++ projects/m1/036-gregorian-date-to-ordinal-date/solutions/stefanocroci/sol_036_gregorian_date_to_ordinal.py
@@ -1,8 +1,10 @@
 def leap_year(year):
+    # This function return True if year is a leap year
     if year % 400 == 0 or (year % 100 != 0 and year % 4 == 0):
         return True
 
 def number_day_in_a_month(month, year):
+    # This function return the number of day in a month
     month_30 = [4, 6, 9, 11]
     month_31 = [1, 3, 5, 7, 8, 10, 12]
     if month in month_31:
@@ -17,6 +19,8 @@ def number_day_in_a_month(month, year):
     return day
 
 def gregorian_date(year, month, day):
+    # This function return the ordinal day working in a loop that goes from 1 to the previous month 
+    # compared to the one indicated as parameter. Then add up the remaining days
     day_sum = 0
     for i in range(1, month):
         day_sum += number_day_in_a_month(i, year)
