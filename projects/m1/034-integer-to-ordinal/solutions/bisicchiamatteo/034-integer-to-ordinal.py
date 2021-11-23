2f2291a2f98b08268a461c5dbf66b20a963cb1b7
ergirius <91219864+ergirius@users.noreply.github.com>
2021-11-23 17:29:00 +0100

    esercizio/034

---
+++ projects/m1/034-integer-to-ordinal/solutions/bisicchiamatteo/034-integer-to-ordinal.py
@@ -0,0 +1,33 @@
+def getnumber():
+    number= int(input("insert one number between 1 to 12 :"))
+    if number==1:
+        print( "first")
+    elif number==2:
+        print("second")
+    elif number==3:
+        print("third")
+    elif number==4:
+        print("fourth")
+    elif number==5:
+        print("fifth")
+    elif number==6:
+        print("sixth")
+    elif number==7:
+        print("seventh")
+    elif number==8:
+        print("eighth")
+    elif number==9:
+        print("ninth")
+    elif number==10:
+        print("tenth")
+    elif number==11:
+        print("eleventh")
+    elif number==12:
+        print("twelfth")
+    else:
+        return "error ... you understand?"
+    return ("")
+
+
+cardinal=getnumber()
+print (cardinal)
