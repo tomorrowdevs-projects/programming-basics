5fa624b0b71b3b692c3b8ff996f6c921b7899d33
Enrico <enricozob@gmail.com>
2021-11-28 22:01:26 +0100

    Refactored scrabble score

--- projects/m2/023-scrabble-score/solutions/enricozoboli/solution_scrabble_score.py
+++ projects/m2/023-scrabble-score/solutions/enricozoboli/solution_scrabble_score.py
@@ -1,36 +1,26 @@
-char_point = { 
-    1 : ['a','e', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u'],
-    2 : ['d', 'g'],
-    3 : ['b', 'c', 'm', 'p'],
-    4 : ['f', 'h', 'v', 'w', 'y'],
-    5 : ['k'],
-    8 : ['j', 'x'],
-    10 : ['q', 'z']
-    }
-
-def build_chars_dict(s):
-    d = dict()
-    for c in s.lower() :
-        if ord(c) in range(97, 123):
-            d[c] = d.get(c, 0)
-            d[c] += 1
-    return d
-   
-def compute_point(s):
-    tot_point = 0
-    for k,v in build_chars_dict(s).items():
-        for n in char_point.keys():
-            if k in char_point[n]:
-                tot_point += n * v
-    return tot_point
+def compute_points(s):
+    """
+    Takes a string and return the total points. Every sigle letters
+    is equivalent to a number of point 
+    """
+    characters = ['aeilnorstu','dg', 'bcmp', 'fhvwy','k', 'jx', 'qz']
+    char_point = [1 ,2 ,3 ,4, 5, 8, 10] 
+    scrabble_points = dict(zip(characters, char_point))
+    total_points = 0
+    for letter in s:
+        for key in scrabble_points.keys():
+            if letter in key:
+                total_points += scrabble_points[key]
+                break
+    return total_points
 
 def main():
     while True:
-        s = input("Enter a word(blank for exit): ")
-        if s == " ":
+        string = input("Enter a word(blank for exit): ")
+        if string == " ":
             break
         else:
-            print(f"Total points: {compute_point(s)}")
+            print(f"Total points: {compute_points(string)}")
 
 if __name__ == "__main__":
     main()
