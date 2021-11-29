ae829295e38638e13857d3e4596e920bf111de5e
domix1684 <domenico.traina84@gmail.com>
2021-11-29 14:49:50 +0100

    soluzioni ES.1-2 dI m1

---
+++ projects/m1/001-area-of-a-room/solutions/DomenicoTraina/area_of_a_room.py
@@ -0,0 +1,5 @@
+print ('Questo programma calcola la superficie di una stanza')
+larg= int (input ("inserisci larghezza stanza: "))
+lung= int (input ("inserisci lunghezza stanza: "))
+area=larg*lung
+print ("la superficie è di {} mq".format (area))
