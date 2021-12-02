503443d0ae262a2975a12f2d9e97740255ccd38f
MatteoFaienza <matteofaienza87@gmail.com>
2021-12-02 11:07:11 +0100

    modified solution/m1/009/vowel or consonant

--- projects/m1/009-vowel-or-consonant/solutions/MatteoFaienza/vowel-or-consonant.py
+++ projects/m1/009-vowel-or-consonant/solutions/MatteoFaienza/vowel-or-consonant.py
@@ -1,17 +1,12 @@
 # the user must insert a letter
 letter=str(input('Enter a letter '))
 # i use the conditional statement to find the correct solution
-if letter == 'a':
-    print('a is a vowel')    
-elif letter == 'e':
-    print('e is a vowel')
-elif letter == 'i':
-    print('i is a vowel')
-elif letter == 'o':
-    print('o is a vowel')
-elif letter == 'u':
-    print('u is a vowel')
+
+if letter in 'aeiou':
+    print(letter,'is a vowel') 
+
 elif letter == 'y':
-    print('y is vowel, and sometimes it s a consonant. ') 
+    print(letter,'is vowel, and sometimes it s a consonant. ') 
+
 else :
-    print(letter,' is a consonant')                     
+    print(letter,' is a consonant')                      
