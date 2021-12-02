a506408e6277dcc4c1a644fe1f10be9145e795bb
MatteoFaienza <matteofaienza87@gmail.com>
2021-12-02 21:24:04 +0100

    modified solution/m1/010/month-name-to-number-of-days

--- projects/m1/010-month-name-to-number-of-days/solutions/MatteoFaienza/month-name-to-number-of-days.py
+++ projects/m1/010-month-name-to-number-of-days/solutions/MatteoFaienza/month-name-to-number-of-days.py
@@ -1,9 +1,14 @@
 # the user enters the month
+
 month = str(input('Enter a month '))
+
 # these conditions return the number of days of the chosen month
-if month=='january 'or month=='march'or month=='may'or month=='july'or month=='august'or month=='october'or month=='december':
-    print('The month is 31 days')
-elif month=='april'or month=='june'or month=='september'or month=='november':
-    print('The month is 30 days')  
-elif month=='february':
-    print('The month is 28 or 29 days,depends on the leap year')  
+
+if month[0:1] == 'f' :
+    print('The month is 28 or 29 days,depends on the leap year')
+
+elif month[0:1] in 'asn' or month == 'june' :
+    print('The month is 30 days')
+
+else :
+    print('The month is 31 days') 
