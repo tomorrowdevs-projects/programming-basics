c2cf70979faa296e21f52b5461ba53c13c2e6440
MatteoFaienza <matteofaienza87@gmail.com>
2021-12-02 22:48:10 +0100

    modified solution m1/013/birth-date-to-astrological-sign

--- projects/m1/013-birth-date-to-astrological-sign/solutions/MatteoFaienza/birth-date-to-astrological-sign.py
+++ projects/m1/013-birth-date-to-astrological-sign/solutions/MatteoFaienza/birth-date-to-astrological-sign.py
@@ -3,68 +3,44 @@ month=int(input('Enter the month of birth as a number '))
 # I ask the user to enter the day of birth
 day=int(input('Enter the day of birth '))
 # I use nested conditions to find the zodiac sign
-if month == 1 :
-    if day <= 19:
-        zodiacSign='Capricorn'
-    else :
-        zodiacSign='Acquarius'
-elif month == 2 :
-    if day <= 18:
-        zodiacSign='Acquarius'
-    else :
-        zodiacSign='Pisces'
-elif month == 3 :
-    if day <= 20:
-        zodiacSign='Pisces'
-    else :
-        zodiacSign='Aries'
-elif month == 4 :
-    if day <= 19:
-        zodiacSign='Aries'
-    else :
-        zodiacSign='Taurus'
-elif month == 5 :
-    if day <= 20 :
-        zodiacSign='Taurus'
-    else :
-        zodiacSign='Gemini'
-elif month == 6 :
-    if day <= 20 :
-        zodiacSign='Gemini'
-    else :
-        zodiacSign='Cancer'
-elif month == 7 :
-    if day <= 22 :
-        zodiacSign='Cancer'
-    else :
-        zodiacSign='Leo'  
-elif month == 8 :
-    if day <= 22 :
-        zodiacSign='Leo'
-    else :
-        zodiacSign='Virgo'
-elif month == 9 :
-    if day <= 22 :
-        zodiacSign='Virgo'
-    else :
-        zodiacSign='Libra' 
-elif month == 10 :
-    if day <= 22 :
-        zodiacSign='Libra'
-    else :
-        zodiacSign='Scorpio'
-elif month == 11 :
-    if day <= 21 :
-        zodiacSign='Scorpio'
-    else :
-        zodiacSign='Sagittarius'
-elif month == 12 :
-    if day <= 21 :
-        zodiacSign='Sagittarius'
-    else :
-        zodiacSign='Capricorn'                
 
-print('Your zodiac sign is', zodiacSign)
+if month == 1 and day <= 19 or month == 12 and day >= 22 :
+    zodiacSign='Capricorn'
+
+elif month == 1 and day >= 20 or month == 2 and day <= 18 :
+    zodiacSign='Acquarius'
+    
+elif month == 2 and day >= 19 or month == 3 and day <= 20 :
+    zodiacSign = 'Pisces' 
+
+elif month == 3 and day >= 21 or month == 4 and day <= 19 :
+    zodiacSign = 'Aries'
+
+elif month == 4 and day >= 20 or month == 5 and day <= 20 :
+    zodiacSign = 'Taurus' 
+
+elif month == 5 and day >= 21 or month == 6 and day <= 20 :
+    zodiacSign = 'Gemini' 
+
+elif month == 6 and day >= 21 or month == 7 and day <= 22 :
+    zodiacSign = 'Cancer'
+
+elif month == 7 and day >= 23 or month == 8 and day <= 22 :
+    zodiacSign = 'Leo'
+
+elif month == 8 and day >= 23 or month == 9 and day <= 22 :
+    zodiacSign = 'Virgo'
+
+elif month == 9 and day >= 23 or month == 10 and day <= 22 :
+    zodiacSign = 'Libra'
+
+elif month == 10 and day >= 23 or month == 11 and day <= 21 :
+    zodiacSign = 'Scorpio'
+
+elif month == 11 and day >= 22 or month == 12 and day <= 21 :
+    zodiacSign = 'Sagittarius' 
+
+print('Your zodiac sign is', zodiacSign) 
 
 
 
