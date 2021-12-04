f7bba79497802d61964a687415ae2a778a409b07
Stefano Consonni <stefanoconsonni@ymail.com>
2021-12-04 18:46:45 +0100

    solution for the making-change exercise

--- projects/m1/002-bottle-deposits/solutions/stefanoconsonni/solution.js
+++
@@ -1,8 +0,0 @@
-let numOfSmallBottles = prompt("How many bottles holding one liter (or less) do you have?");
-let numOfBigBottles = prompt("How many bottles holding more than one liter do you have?");
-
-let refundFromSmallBottles = numOfSmallBottles * 0.1;
-let refundFromBigBottles = numOfBigBottles * 0.25;
-
-let expectedRefund = refundFromSmallBottles + refundFromBigBottles;
-alert(`The expected refund is: $ ${expectedRefund.toFixed(2)}`);
