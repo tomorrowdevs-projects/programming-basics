e83f2879544f2e532b426d211ecfff9d98d60073
rz <rz@sanchopanza>
2021-12-06 14:56:01 +0100

    feat: basic structure to solve budget-app program

---
+++ projects/m5/001-budget-app/solutions/robertozecchini/budget.py
@@ -0,0 +1,21 @@
+class Category:
+    def __init__(self, name):
+        pass
+
+    def deposit(self, amount, description=""):
+        pass
+
+    def withdraw(self, amount, description=""):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def transfer(self, amount, category):
+        pass
+
+    def check_funds(self, amount):
+        pass
+
+def create_spend_chart(categories):
+    pass
