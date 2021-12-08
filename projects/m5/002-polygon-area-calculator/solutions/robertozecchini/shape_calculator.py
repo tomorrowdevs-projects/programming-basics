de311c95388c0cfe07805d02064274e3253a3dae
rz <rz@sanchopanza>
2021-12-08 15:10:04 +0100

    feat: added constructor e __str__ to rectangle class

--- projects/m5/002-polygon-area-calculator/solutions/robertozecchini/shape_calculator.py
+++ projects/m5/002-polygon-area-calculator/solutions/robertozecchini/shape_calculator.py
@@ -1,9 +1,10 @@
 class Rectangle:
-    def __init__(width, height):
-        pass
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-    def __str__():
-        pass
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
 
     def set_width(w):
         pass
