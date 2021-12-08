992789acedb3846c8ea02a746f071bdcd61879fa
rz <rz@sanchopanza>
2021-12-08 15:03:04 +0100

    feat: first draft for rectangle and square classes

---
+++ projects/m5/002-polygon-area-calculator/solutions/robertozecchini/shape_calculator.py
@@ -0,0 +1,43 @@
+class Rectangle:
+    def __init__(width, height):
+        pass
+
+    def __str__():
+        pass
+
+    def set_width(w):
+        pass
+
+    def set_height(h):
+        pass
+
+    def get_area():
+        pass
+
+    def get_perimeter():
+        pass
+
+    def get_diagonal():
+        pass
+
+    def get_picture():
+        pass
+
+    def get_amount_inside():
+        pass
+
+class Square(Rectangle):
+    def __init__(side):
+        pass
+
+    def __str__():
+        pass
+
+    def set_side(s):
+        pass
+
+    def set_width(w):
+        pass
+
+    def set_height(h):
+        pass
