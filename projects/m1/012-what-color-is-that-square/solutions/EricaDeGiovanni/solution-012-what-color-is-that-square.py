d8fdefa6f5feefe4b2d7da70513b75054049442a
erydegio <degiovannierica16@gmail.comgit remotegit config --global user.name erydegioclear>
2021-12-09 13:34:30 +0100

    solution 012 improved

--- projects/m1/012-what-color-is-that-square/solutions/EricaDeGiovanni/solution-012-what-color-is-that-square.py
+++ projects/m1/012-what-color-is-that-square/solutions/EricaDeGiovanni/solution-012-what-color-is-that-square.py
@@ -5,23 +5,24 @@ position_user = input('Enter a position of the chess board: ').lower()
 # assume that a valid position will always be entered. 
 
 if len(position_user) == 2 and position_user[1].isdigit():
-
+    
     column = str(position_user[0])
     row = int(position_user[1])
 
-    if column == 'a' or column == 'c'or column == 'e' or column == 'g' and 0 < row <= 8:
+    if row <= 8:
+        if column == 'a' or column == 'c'or column == 'e' or column == 'g':
 
-        # use modular arithmetic to report the color in that row
-        if row % 2 == 0:
-            print('The square is white.')
-        elif row % 2 != 0:
-            print('The square is black.')   
+            # use modular arithmetic to report the color in that row
+            if row % 2 == 0:
+                print('The square is white.')
+            else:
+                print('The square is black.')   
 
-    elif column == 'b' or column == 'd' or column == 'f' or column == 'h' and 0 < row <= 8:
-        if row % 2 == 0:
-            print('The square is black.')
-        elif row % 2 != 0:
-            print('The square is white.')
+        elif column == 'b' or column == 'd' or column == 'f' or column == 'h':
+            if row % 2 == 0:
+                print('The square is black.')
+            else:
+                print('The square is white.')
 
 
 
