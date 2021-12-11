349d28595a933a6555b12d52d3c533f14670bf6f
rz <rz@sanchopanza>
2021-12-11 16:42:47 +0100

    feat: added constructor for class Ticket

--- projects/m6/001-lotto-game/solutions/robertozecchini/lotto_ticket.py
+++ projects/m6/001-lotto-game/solutions/robertozecchini/lotto_ticket.py
@@ -1,10 +1,17 @@
 #Class defining a lotto bill
+
+import random
+
 class Ticket:
     types = ("Ambata", "Ambo", "Terna", "Quaterna", "Cinquina")
     cities = ("Bari", "Cagliari", "Firenze", "Genova", "Milano", "Napoli", "Palermo", "Roma", "Torino", "Venezia", "Tutte")
     
-    def __init__(self, type, numbers, city):
-        pass
+    def __init__(self, type, city, numbers):
+        self.type = type
+        self.city = city
+        self.numbers = []
+        for n in range(numbers):
+            self.numbers.append(random.randrange(1, 90+1))
     
     def __str__(self):
         pass
