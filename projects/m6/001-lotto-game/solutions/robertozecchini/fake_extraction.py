01e51247ecea95b7331d44d70ece27811e6b0607
rz <rz@sanchopanza>
2021-12-13 16:22:07 +0100

    feat: create an Extraction class

---
+++ projects/m6/001-lotto-game/solutions/robertozecchini/fake_extraction.py
@@ -0,0 +1,23 @@
+import random
+    
+class Extraction:
+    cities = ("Bari", "Cagliari", "Firenze", "Genova", "Milano", "Napoli", "Palermo", "Roma", "Torino", "Venezia", "Tutte")
+    
+    def __init__(self):
+        self.results = {}
+        for city in self.cities:
+            if city != "Tutte":
+                self.results[city] = self.extraction()
+    
+    def __str__(self):
+        output_string = ""
+        for city, numbers in self.results:
+            output_string += f"{city} - {' '.join(map(str, numbers))}"
+        return output_string
+    
+    def extraction(self):
+        numbers = []
+        for i in range(6):
+            numbers.append(random.randint(1, 90))
+        numbers.sort()
+        return numbers
