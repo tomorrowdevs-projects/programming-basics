9b39698e8f816251f8a21a573893ae9ed30175fa
rz <rz@sanchopanza>
2021-12-13 17:33:09 +0100

    feat: added methods to Ticket class to verify if the ticket is winning

--- projects/m6/001-lotto-game/solutions/robertozecchini/lotto_ticket.py
+++ projects/m6/001-lotto-game/solutions/robertozecchini/lotto_ticket.py
@@ -1,6 +1,7 @@
 #Class defining a lotto bill
 
 import random
+import fake_extraction
 from ascii_table import ascii_table
 
 class Ticket:
@@ -18,3 +19,41 @@ class Ticket:
     def __str__(self):
         numbers_str = ' '.join(map(str,self.numbers))
         return ascii_table("Lotto ticket", type = self.type, city = self.city, numbers = numbers_str)
+
+    def isValid(self, extraction):
+        if not isinstance(extraction, fake_extraction.Extraction):
+            return False
+        elif self.city != "Tutte" and self.city not in extraction.results.keys():
+            return False
+        else:
+            return True
+
+    def isWinning(self, extraction):
+        if not self.isValid(extraction):
+            return False
+        if self.city == "Tutte":
+            for city in extraction.cities:
+                if self.checkNumbers(extraction.results[city]):
+                    return True
+            else:
+                return False
+        else:
+            return self.checkNumbers(extraction.results[self.city])
+    
+    def checkNumbers(self, extracted_numbers):
+        played = set(self.numbers)
+        extracted = set(extracted_numbers)
+        winnings = played & extracted
+        if self.type == "Ambata" and len(winnings) >= 1:
+            return True
+        elif self.type == "Ambo" and len(winnings) >= 2:
+            return True
+        elif self.type == "Terna" and len(winnings) >= 3:
+            return True
+        elif self.type == "Quaterna" and len(winnings) >= 4:
+            return True
+        elif self.type == "Cinquina" and len(winnings) >= 5:
+            return True
+        else:
+            return False
+    
