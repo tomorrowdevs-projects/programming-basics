e43b8a60bbdf512937db75eb58c46abaa63eda3c
rz <rz@sanchopanza>
2021-12-14 07:15:42 +0100

    feat: added a test to check that no duplicated numbers are generated

--- projects/m6/001-lotto-game/solutions/robertozecchini/test_module.py
+++ projects/m6/001-lotto-game/solutions/robertozecchini/test_module.py
@@ -9,6 +9,13 @@ class UnitTests(unittest.TestCase):
         expected2 = f" |\n+---------+----------+\n"
         self.assertEqual(actual[:173], expected1, 'Expected different string representation of object.')
         self.assertEqual(actual[-26:], expected2, 'Expected different string representation of object.')
+    
+    def test_no_duplicated_numbers(self):
+        #generates 90 numbers to improve error probability
+        self.ticket = lotto_ticket.Ticket("Ambo", "Cagliari", 90)
+        actual = len(self.ticket.numbers)
+        expected = len(set(self.ticket.numbers)) #set automatically removes duplicates
+        self.assertEqual(actual, expected, "Expected not to generate duplicated numbers")
 
 if __name__ == "__main__":
     unittest.main()
