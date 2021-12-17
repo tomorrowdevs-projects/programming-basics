e9c043c03bb553d96241d7e9914d3508efc907e5
ergirius <91219864+ergirius@users.noreply.github.com>
2021-12-17 09:56:48 +0100

    aggiornato

--- projects/m1/026-is-a-string-a-palindrome/solutions/bisicchiamatteo/026-is-a-string-a-palindrome.py
+++ projects/m1/026-is-a-string-a-palindrome/solutions/bisicchiamatteo/026-is-a-string-a-palindrome.py
@@ -1,11 +1,11 @@
 while True:
     
-    line=input ("inserisci la parola oppure premi invio : ")
-    if line=="":
+    input_word=input ("inserisci la parola oppure premi invio : ")
+    if input_word=="":
         break
-    new_line=line.lower[::-1]
+    inverted_input_word=input_word.lower[::-1]
     print (new_line)
-    if (line ==new_line):
-        print("la parola è palindroma")
+    if (input_word ==inverted_input_word):
+        print("la parola ï¿½ palindroma")
     else:
-        print("la parola non è palindroma")
+        print("la parola non ï¿½ palindroma")
