7312ea6c65ea92b4a408a3dcf41be3e99baa421b
ergirius <91219864+ergirius@users.noreply.github.com>
2021-12-17 09:35:47 +0100

    non trovo la funzione che faccia un xor logico su tutti i valori quindi l'unica è fare cosi se no non si riesce a fare il controllo

--- projects/m1/023-parity-bits/solution/bisicchiamatteo/023-parity-bits.py
+++ projects/m1/023-parity-bits/solution/bisicchiamatteo/023-parity-bits.py
@@ -9,9 +9,9 @@ while True:
     elif (parity_bits==""):
         break
     else :
-        print("il valre inserito non è idoneo ")
+        print("il valre inserito non e idoneo ")
     control =list(parity_bits)
     if ((int(control[0])^int(control[1])^int(control[2])^int(control[3])^int(control[4])^int(control[5])^int(control[6])^int(control[7])^0)==0 ):
-           print (" si usa il bit di parità  pari")
+           print (" si usa il bit di parita  pari")
     elif((int(control[0])^int(control[1])^int(control[2])^int(control[3])^int(control[4])^int(control[5])^int(control[6])^int(control[7])^0)==1 ):
-          print (" si usa il bit di parità  dispari")
+          print (" si usa il bit di parita  dispari")
