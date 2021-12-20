11ed6c3c8341a50489b4728321cbd185e4946a38
Enrico <enricozob@gmail.com>
2021-12-20 15:12:38 +0100

    FEAT: added withdraw, get_balance and check_funds methods

--- projects/m5/001-budget-app/solutions/enricozoboli/budget.py
+++ projects/m5/001-budget-app/solutions/enricozoboli/budget.py
@@ -5,19 +5,28 @@ class Category:
         self.ledger = []
 
     def deposit(self, amount, description=""):
-        self.ledger.append({amount: description})
+        self.ledger.append({'amount': amount, 'description': description})
 
-    def withdraw(self):
-        pass
+    def withdraw(self, amount, description=""):
+        if self.check_funds(amount):
+            self.ledger.append({'amount': -amount, 'description': description})
+            return True
+        else:
+            return False
 
     def get_balance(self):
-        pass
+        balance = 0
+        for item in self.ledger:
+            balance += item['amount']
+        return balance
 
     def transfer(self):
         pass
 
-    def check_funds(self):
-        pass
+    def check_funds(self, amount):
+        if amount > self.get_balance():
+            return False
+        return True
 
 
 def create_spend_chart(categories):
