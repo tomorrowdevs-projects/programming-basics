48f273eb25234d1682ee98def4fe945d69e8055e
chiara costanzo <luca.bertoldi89@gmail.com>
2021-12-22 00:32:23 +0100

    budget-spend-chart-first-try

--- projects/m5/001-budget-app/budget.php
+++ projects/m5/001-budget-app/budget.php
@@ -1,12 +1,23 @@
 <?php 
 
-class Categories {
+
+class Categories  {
     public $name;
     public $ledger = [];
     public $totalAmount;
 
     public function __construct($name) {
         $this->name = $name;
+    }
+
+    public function __toString() {
+        $output_string = str_pad("{$this->name}", 40, "*", STR_PAD_BOTH);
+        foreach($this->ledger as $value) {
+        
+        $decoded_array = json_decode($value);
+        $output_string .= "<br/>" . "<div style='width:300px; position:relative'>" . $decoded_array->description . "<span style='text-align:right; position:absolute; right:0'> {$decoded_array->amount} </span>" . "</div>";
+    };
+        return $output_string;
     }
 
     public function deposit($amount, $description = "") {
@@ -16,10 +27,14 @@ class Categories {
     }
 
     public function withdraw($amount, $description = "") {
-        $this->check_funds($amount);
-        $this->totalAmount -= $amount;
-        $depositResult = array("amount" => -$amount, "description" => $description);
-        array_push($this->ledger, json_encode($depositResult));
+        if($this->check_funds($amount)) {
+            $this->totalAmount -= $amount;
+            $depositResult = array("amount" => -$amount, "description" => $description);
+            array_push($this->ledger, json_encode($depositResult));
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function get_balance() {
@@ -27,12 +42,17 @@ class Categories {
     }
 
     public function transfer ($amount, $budget_category) {
-        $this->check_funds($amount);
-        $this->withdraw($amount, "Trasfer to `{$budget_category->name}`");
-        $budget_category->deposit($amount, "Trasfer from `{$this->name}`");
+        if($this->check_funds($amount)) {
+            $this->withdraw($amount, "Trasfer to `{$budget_category->name}`");
+            $budget_category->deposit($amount, "Trasfer from `{$this->name}`");
+            return true;
+        } else {
+            return false;
+        }
+        
     }
 
-    private function check_funds($amount) {
+    protected function check_funds($amount) {
         if($this->totalAmount < $amount) {
             return false;
         } else {
@@ -41,6 +61,49 @@ class Categories {
     }
 }
 
+
+
+function create_spend_chart($categories) {
+    $output_string = "Percentage spent by category<br><table>";
+    $sum = 0;
+    $percentage = [];
+
+    foreach($categories as $category) {
+        $category_array = $category->ledger;
+        foreach($category_array as $value) {
+            
+            $decoded_array = json_decode($value);
+            if($decoded_array->amount < 0) {
+                $sum += $decoded_array->amount;
+            } else {
+            $total = $decoded_array->amount;
+            }
+        };
+        array_push($percentage, (abs($sum / $total) * 100));
+        for($i=100; $i>=0; $i-=10) {
+            $output_string .= "<tr style='text-align:right'><td>" . $i  . "|</td>";
+            foreach($percentage as $value) {
+                
+                if($value == $i) {
+                    $output_string .= "<td>";
+                    for($j=$i; $j>=0; $j-=10) {
+                        //$output_string .= "<tr style='text-align:right'><td>" . $j . "| </td><td>0</td></tr>";
+                        $output_string .= "<td style='text-align:right'>0</td>";
+                    }
+                    break;
+                } 
+        }
+    }
+    
+
+    
+        
+    }
+    
+    $output_string .= "</table>";
+
+    return $output_string;
+}
 //def create_spend_chart(categories):
 
 ?>
--- projects/m5/001-budget-app/main.php
+++ projects/m5/001-budget-app/main.php
@@ -10,12 +10,14 @@ $food->withdraw(200, "fruit");
 
 $clothing = new Categories("Clothing");
 $food->transfer(200, $clothing);
-print_r($clothing->get_balance())
-?>
+echo $food;
+echo create_spend_chart([$food, $clothing]);
 
 
 
 
+
+/*
 # This entrypoint file to be used in development. Start by reading README.md
 import budget
 from budget import create_spend_chart
@@ -41,3 +43,6 @@ print(create_spend_chart([food, clothing, auto]))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
+*/
+
+?>
