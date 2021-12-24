f4a390de1653504d8b0fe1c1a9515acec455c74c
chiara costanzo <luca.bertoldi89@gmail.com>
2021-12-24 22:10:03 +0100

    draw-method-completed

--- projects/m5/003-probability-calculator/solutions/LucaBertoldi/prob_calculator.php
+++ projects/m5/003-probability-calculator/solutions/LucaBertoldi/prob_calculator.php
@@ -1,22 +1,41 @@
 <?php
 
 class Hat {
-    
+    public $content= [];
+
     public function __construct(int ...$argv) {
-        $content= [];
+        
         if(count($argv) === 0) {
             echo "false";
             return;
         }
         foreach($argv as $key => $value) {
             for($i=0; $i < $value; $i++) {
-                array_push($content, $key);  
+                array_push($this->content, $key);  
             }
         }
     }
+
+    public function draw(int $ball) {
+        print_r($this->content);
+        $pick = [];
+        $random_pick = array_rand($this->content,$ball);
+
+        for($i=0; $i < $ball; $i++) {
+            array_push($pick, $this->content[$random_pick[$i]]);
+            unset($this->content[$random_pick[$i]]);
+        }
+        return json_encode($pick);
+    }
 }
 
-$hat = new Hat(red:3, blue:4);
+function experiment($hat, $expected_balls, $num_balls_drawn, $num_experiments) {
+
+};
+
+$hat = new Hat(red:2, blue:1);
+
+$hat->draw(2);
 
 
 
