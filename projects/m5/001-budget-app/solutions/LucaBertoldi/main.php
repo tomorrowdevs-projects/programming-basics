259d674374f2363163052e853dfa584de01088d7
chiara costanzo <luca.bertoldi89@gmail.com>
2021-12-26 18:16:39 +0100

    print-auto-data

--- projects/m5/001-budget-app/solutions/LucaBertoldi/main.php
+++ projects/m5/001-budget-app/solutions/LucaBertoldi/main.php
@@ -13,9 +13,16 @@ $food->transfer(200, $clothing);
 $clothing->withdraw(23.55);
 $clothing->withdraw(40);
 $clothing->withdraw(50);
+
+$auto = new Categories("Auto");
+$auto->deposit(1000, "initial deposit");
+$auto->withdraw(15);
 echo $food;
+echo "<br>";
 echo $clothing;
-echo create_spend_chart([$food, $clothing]);
+echo "<br>";
+echo $auto;
+echo create_spend_chart([$food, $clothing, $auto]);
 
 
 
