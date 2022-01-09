37f93bd2fba6fd8df6e5b044c04cdaa36d253244
Francesco Ricci <francescosave.ricci@gmail.com>
2022-01-09 17:42:31 +0100

    refact: modify function getleapYear using one row solution

--- projects/m1/015-is-it-a-leap-year/solutions/francescoricci/015-is-it-a-leap-year.py
+++ projects/m1/015-is-it-a-leap-year/solutions/francescoricci/015-is-it-a-leap-year.py
@@ -1,9 +1,6 @@
+# exercise 015 by Francesco Ricci
 def getLeapYear(anno):
-    if (anno % 400 == 0) or (anno % 4 == 0 and (not(anno % 100 == 0))):
-        return('leap year ')
-    else:
-        return('not leap year ')
-
+    return "leap year" if (anno % 400 == 0) or (anno % 4 == 0 and (not(anno % 100 == 0))) else "not leap year"
 
 for year in range(2016,2024):
     print(str(year) + ' is ' + getLeapYear(year))
