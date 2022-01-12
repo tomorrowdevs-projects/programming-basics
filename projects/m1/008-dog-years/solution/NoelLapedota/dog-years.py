927f93e79c1f6b5c987be95c4f109095c1b94116
Mrleon123 <Mrleon@hotmail.it>
2022-01-12 14:46:27 +0100

    dog-years

---
+++ projects/m1/008-dog-years/solution/NoelLapedota/dog-years.py
@@ -0,0 +1,17 @@
+first_two = 10.5
+input=int(input("How old is your dog? : "))
+y = (first_two * 2) + (input * 4)
+y_a = first_two * input
+        
+def dog_y (input) :
+   
+    if input > 2 :
+            print('Your dog is {} human years old!'.format(y))
+    elif input == 2 and 1 :
+            print('Your dog is {} human years old!'.format(y_a))
+    elif  input == 0 :
+            print('Your dog is growing fast')
+    else :
+            print('Only positive numbers thank you')
+
+ input   
