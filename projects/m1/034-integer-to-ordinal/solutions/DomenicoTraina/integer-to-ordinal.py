e35f3602c92be862890cb358d90ec2a6d16f1ae7
domix1684 <domenico.traina84@gmail.com>
2022-01-14 10:04:13 +0100

    lasti version of es.34

--- projects/m1/034-integer-to-ordinal/solutions/DomenicoTraina/integer-to-ordinal.py
+++ projects/m1/034-integer-to-ordinal/solutions/DomenicoTraina/integer-to-ordinal.py
@@ -4,6 +4,5 @@ def ordinal_number(number):
     
     position=["first","second","third","fourth","fifth","sixth","seventh","eight","ninth","tenth","eleventh","twelfth"]
     print(f"The position for {number} number is {position[number-1]}")
-    #return position[number-1]
-
+ 
 ordinal_number(number)
