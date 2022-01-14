ab54bf649c4b7b36c5bdfa1188c728707557e1db
Mrleon123 <Mrleon@hotmail.it>
2022-01-14 13:06:15 +0100

    note-to-frequency

---
+++ projects/m1/011-note-to-frequency/solution/NoelLapedota/note-to-frequency.py
@@ -0,0 +1,4 @@
+my_dictionaries ={"C4": 261.63,"D4": 293.66,"E4": 329.63,"F4": 349.23,"G4": 392.00,"A4": 440.00,"B4":403.88}
+def xxx ():
+    q=(input('Write a note : '))
+    print('The frequency of your note is : {}'.format(my_dictionaries[q]))
