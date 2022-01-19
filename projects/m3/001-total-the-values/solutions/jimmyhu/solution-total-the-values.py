6337ac2683e3b679ba8a967875fa9dad5ae39514
jimmyhu997 <jimmywx@outlook.it>
2022-01-19 09:28:00 +0100

    solution total the values

---
+++ projects/m3/001-total-the-values/solutions/jimmyhu/solution-total-the-values.py
@@ -0,0 +1,16 @@
+import numbers
+
+
+def calc():
+    num = input('Test: ')
+    if num == ' ' or num == '':
+        return 0
+    else:
+        return int(num) + calc()
+
+
+def main():
+    print(calc())
+
+if __name__ == '__main__':
+    main() 
