8a890319f01f4b262729b3839d5a4ef34dcb0961
Mrleon123 <Mrleon@hotmail.it>
2022-01-20 16:13:23 +0100

    fizz-buzz

---
+++ projects/m1/024-fizz-buzz/solution/NoelLapedota/fizz-buzz.py
@@ -0,0 +1,12 @@
+n = 1
+f =[]
+while n == 100:
+    if n / 3 ==int():
+        print('Fizz')
+    if n / 5 == int():
+        print('Buzz')    
+    if n / 3 and n / 5 ==int():
+        print('Fizz-buzz')
+    else:
+        f.append(n)        
+    n =+ 1    
