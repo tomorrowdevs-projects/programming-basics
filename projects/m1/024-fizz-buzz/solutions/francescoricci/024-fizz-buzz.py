25c90bac9549cd0480d242e6979b0ada73937bf3
Francesco Ricci <francescosave.ricci@gmail.com>
2022-01-24 14:31:26 +0100

    feat: completed exercise 024.Used recursive nfunctions

---
+++ projects/m1/024-fizz-buzz/solutions/francescoricci/024-fizz-buzz.py
@@ -0,0 +1,27 @@
+
+def fizzBuzz(count,end):
+    message = ''
+    if count % 3 ==0 and count % 5 ==0:
+        #divisible for three and for five
+        message = 'FizzBuzz'
+    elif count % 3 ==0:
+        # divisible for three
+        message = 'Fizz'
+    elif count % 5 ==0:
+        # divisible for five
+        message = 'Buzz'
+    else:
+        message = count
+
+    print(message)
+
+    if count != end:
+        count +=1
+        fizzBuzz(count,end)
+
+fizzBuzz(1,100)
+
+
+
+
+
