0948b8fd7f004918bfbd906a6375731b2da4f323
erydegio <degiovannierica16@gmail.comgit remotegit config --global user.name erydegioclear>
2022-01-29 09:45:29 +0100

    added description

--- projects/m4/014-words-with-six-vowels-in-order/solutions/ericadegiovanni/m4_014.py
+++ projects/m4/014-words-with-six-vowels-in-order/solutions/ericadegiovanni/m4_014.py
@@ -1,5 +1,10 @@
 import string
 
+"""
+Find a word that contains each of the vowels A, E, I, O, U and Y exactly once and in order.
+Write a program that searches a file containing a list of words and displays all of the words that meet this constraint. 
+The user will provide the name of the file that will be searched.
+"""
 file_path = input('Insert the path of your file containing the list of words: ')
 vowels = 'aeiouy'
 
