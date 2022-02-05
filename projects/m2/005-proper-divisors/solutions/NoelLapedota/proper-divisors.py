c58076083488e347f288c65ed1b7bae30823b840
Mrleon123 <Mrleon@hotmail.it>
2022-02-05 16:59:38 +0100

    proper-divisors

--- projects/m2/005-proper-divisors/solutions/NoelLapedota/proper-divisors.py
+++ projects/m2/005-proper-divisors/solutions/NoelLapedota/proper-divisors.py
@@ -1,7 +1,7 @@
-q=int(input('Enter the number please \n'))
+
 def list_Proper_Divisors(q):
     array = []
-    for i in range(1,q):
+    for i in range(1,q + 1):
         if q % i == 0:
          array.append(i)
-    print(arrayray)
+    print(array)
