9db2e3ab2e8379bf2da2f420b8d7b434ad53610e
Mrleon123 <Mrleon@hotmail.it>
2022-02-05 16:32:25 +0100

    sublists

---
+++ projects/m2/014-sublists/solutions/NoelLapedota/sublists.py
@@ -0,0 +1,15 @@
+def isSublist(a,b):
+    a = ''.join(a)
+    b= ''.join(b)
+    
+    if len[b] ==0:
+        print('True')
+
+    if len(a.split(b))== 1:
+        print('False')
+    else:
+        print('True')
+
+
+
+    
