f1967613fc0255459cf0f661a1fda85bc0196af3
erydegio <degiovannierica16@gmail.comgit remotegit config --global user.name erydegioclear>
2022-02-07 10:26:18 +0100

    Feat: create_spend_chart added

--- projects/m5/001-budget-app/solutions/ericadegiovanni/budget.py
+++ projects/m5/001-budget-app/solutions/ericadegiovanni/budget.py
@@ -1,11 +1,18 @@
+import math
 
 class Category:
+    
+    # withdrawal data
+    withdrawals_counter = {}
+    total_withdrawals = 0
 
     def __init__(self, category):
         
         self.category = category
         self.ledger = []
         self.balance = 0
+
+        Category.withdrawals_counter[self.category]  = 0 #execute every time you create an instance
         
     def check_funds(self, amount):
         if amount > self.balance:
@@ -19,8 +26,14 @@ class Category:
 
     def withdraw(self, amount, description = ''):
         if self.check_funds(amount):
+            # update ledger
             self.ledger.append({'amount': -abs(amount), 'description': description})
             self.balance -= amount
+            
+            # update withdrawal data
+            Category.withdrawals_counter[self.category] += abs(amount)
+            Category.total_withdrawals += abs(amount)
+                       
             return True
         else:
             return False
@@ -53,5 +66,40 @@ class Category:
         
 
 def create_spend_chart(categories):
-    pass
+
+    percentages_spent = {}
+    spend_chart = 'Percentage spent by category\n'
+    
+    # find percentages spent for each category
+    for category in Category.withdrawals_counter:
+        percentages_spent[category] = math.floor(((Category.withdrawals_counter[category] / Category.total_withdrawals  ) * 100) / 10)
+    
+    #create bars
+    for i in range(10, -1, -1):
+        spend_chart += f'{str(i * 10).rjust(3)}|'
+
+        for c in categories:
+            if percentages_spent[c.category] == i: 
+                spend_chart += ' o ' 
+                percentages_spent[c.category] -= 1
+            else: spend_chart += '   '  
+
+        spend_chart += '\n'   
+
+    spend_chart += f"    {'---' * len(categories)}-\n"
+    
+    # add category names
+    max_l = max([len(c.category) for c in categories])
+    cat_str = [c.category.ljust(max_l) for c in categories]   
+    category_names = '    '
+    
+    for i in range(max_l): 
+        for i2 in range(len(cat_str)):           
+            category_names += f' {cat_str[i2][i]} '
+        category_names += '\n    '
+
+    spend_chart += category_names
+
+    return spend_chart
+    
 
