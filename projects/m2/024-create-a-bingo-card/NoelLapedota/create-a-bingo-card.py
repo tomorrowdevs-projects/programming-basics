7feac06e7a545f69af2e4f300eba89b0eafdbdbe
Mrleon123 <Mrleon@hotmail.it>
2022-02-09 19:13:43 +0100

    create-a-bingo-card

--- projects/m2/024-create-a-bingo-card/NoelLapedota/create-a-bingo-card.py
+++ projects/m2/024-create-a-bingo-card/NoelLapedota/create-a-bingo-card.py
@@ -8,15 +8,20 @@ def bingo_card():
     r = []
     while len(r) != 5:
         b = random.randint(1,16)
-        z.append(b)
+        if b not in z:
+         z.append(b)
         i = random.randint(16,31)
-        k.append(i)
+        if i not in k:
+         k.append(i)
         n = random.randint(31,45)
-        h.append(n)
+        if n not in h:
+         h.append(n)
         g = random.randint(45,61)
-        w.append(g)
+        if g not in w:
+         w.append(g)
         o = random.randint(61,76)
-        r.append(o)
+        if o not in r:
+         r.append(o)
     b = random.randint(1,16)
     bc.update({"b": z})
     i = random.randint(16,31)
