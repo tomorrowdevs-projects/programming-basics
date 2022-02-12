dfd50d36dc077b4d80aa16dee7fa62600c8fc082
Domenico <94327171+Domecosma@users.noreply.github.com>
2022-02-12 14:51:06 +0100

    Update index.py

--- projects/m1/010-month-name-to-number-of-days/solutions/DomenicoCosma/index.py
+++ projects/m1/010-month-name-to-number-of-days/solutions/DomenicoCosma/index.py
@@ -1,6 +1,6 @@
 
 days_31 = ['jan', 'mar', 'may', 'mul', 'aug', 'oct', 'dec']
-day_30 = ['apr', 'jun', 'sep', 'nov']
+days_30 = ['apr', 'jun', 'sep', 'nov']
 
 request_month = input('Write your month...')
 
@@ -10,7 +10,7 @@ if month[0:3] in days_31:                   #Anche se l'utente inserisca il mese
     print('Your month has 31 days')
 elif month == 'feb':
     print('Your month has usually 28 days but every 4 years has 29 days')
-elif month[0:3] in day_30:
+elif month[0:3] in days_30:
     print('Your month has 30 days')
 else:
     print('Error: This is not a month!!')
