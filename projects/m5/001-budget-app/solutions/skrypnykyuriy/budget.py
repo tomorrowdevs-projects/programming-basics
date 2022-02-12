9f2dd6430281e8a4aa955cd87287f1335af08f76
Skrypnyk Yuriy <skrypnyk@hotmail.it>
2022-02-12 19:05:34 +0100

    All tests ok

--- projects/m5/001-budget-app/solutions/skrypnykyuriy/budget.py
+++ projects/m5/001-budget-app/solutions/skrypnykyuriy/budget.py
@@ -1,39 +1,38 @@
 class Category(object):
     def __init__(self, category):
         self.ledger = []
-        self.category = category
-        self.total = 0
+        self.name = category
 
     def __str__(self):
-        return self.category
+        result = f'{self.name.center(30, "*")}\n'
+        for item in self.ledger:
+            result += '{:<23}{:>7}\n'.format(item['description'][:23], "{:.2f}".format(item['amount']))
+        result += f"Total: {self.get_balance()}"
+        return result
 
     def deposit(self, amount, description=''):
         self.ledger.append({"amount": amount, "description": description})
-        self.total += amount
 
     def withdraw(self, amount, description=''):
         if self.check_funds(amount):
-            return False
-        else:
-            self.ledger[0]['amount'] -= amount
-            self.ledger.append({"amount": '-' + str(amount), "description": description})
+            self.ledger.append({"amount": float(-amount), "description": description})
             return True
+        else:
+            return False
 
     def get_balance(self):
-        print(f'{self.category.center(30, "*")}')
-        print('{:<23}{:>7}'.format('initial deposit', str(self.total)+'.00'))
-        for item in self.ledger[1:]:
-            print('{:<23}{:>7}'.format(item['description'][:22], item['amount']))
-        print('{:<23}{:>7}'.format('Total: ', self.ledger[0]['amount']))
+        total = 0
+        for num in self.ledger:
+            total += float(num['amount'])
+        return total
 
     def transfer(self, amount, category):
         if self.check_funds(amount):
-            return False
-        else:
-            category.ledger.append({"amount": amount})
-            self.ledger.append({"amount": amount, "description": f"Transfer to {category}"})
-            category.ledger.append({"amount": '-' + str(amount), "description": f"Transfer from {self.category}"})
+            self.ledger.append({"amount": -amount, "description": f"Transfer to {category.name}"})
+            category.ledger.append({"amount": amount, "description": f"Transfer from {self.name}"})
             return True
+        else:
+            return False
 
     def check_funds(self, amount):
         if amount > self.ledger[0]['amount']:
@@ -43,20 +42,50 @@ class Category(object):
 
 
 def create_spend_chart(categories):
-    print("Percentage spent by category")
+    total = 0
+    spend_for_category = {}
+    for category in categories:
+        for item in category.ledger[1:]:
+            if 'Transfer' not in item['description']:
+                total += item['amount']
+            if 'Transfer' not in item['description']:
+                if category.name not in spend_for_category:
+                    spend_for_category[category.name] = abs(item['amount'])
+                else:
+                    spend_for_category[category.name] += abs(item['amount'])
 
+    lst_of_percent = []
+    for key, values in spend_for_category.items():
+        rounded = round(values / total * -100) / 10
+        lst_of_percent.append(int(rounded))
 
+    output = "Percentage spent by category\n"
+    label_y = ['  0| ', ' 10| ', ' 20| ', ' 30| ', ' 40| ', ' 50| ', ' 60| ', ' 70| ', ' 80| ', ' 90| ', '100| ']
 
-food = Category("Food")
-food.deposit(1000, "initial deposit")
-food.withdraw(10.15, "groceries")
-food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
-clothing = Category("Clothing")
-food.transfer(50, clothing)
-clothing.withdraw(25.55)
-clothing.withdraw(100)
-auto = Category("Auto")
-auto.deposit(1000, "initial deposit")
-auto.withdraw(15)
+    for label in label_y:
+        str_add = ''
+        for percent in lst_of_percent:
+            if label_y.index(label) <= percent:
+                str_add += 'o  '
+            else:
+                str_add += '   '
+        label_y[label_y.index(label)] += str_add
+    output += '\n'.join(label_y[::-1])
+    output += "\n" + " " * 4 + "-" * 10 + '\n'
 
+    list_length = []
+    for key, values in spend_for_category.items():
+        list_length.append(len(key))
+    max_length = max(list_length)
+
+    for i in range(max_length):
+        output += ' ' * 5
+        for category in categories:
+            if len(category.name) > i:
+                output += category.name[i] + '  '
+            else:
+                output += '   '
+        output += '\n'
+    output = output[:-1]
+
+    return output
