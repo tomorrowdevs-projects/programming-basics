b31bee4b4c3fd42fb3033a8ad45cb6c195c1d2c0
Domecosma <domij96@hotmail.it>
2022-02-19 15:33:48 +0100

    Solution note-to-frequenzy better solution

--- projects/m1/011-note-to-frequency/solutions/DomenicoCosma/index.py
+++ projects/m1/011-note-to-frequency/solutions/DomenicoCosma/index.py
@@ -1,43 +1,27 @@
 
-note = input('Tell me one letter from A to G')
-octave = int(input('Tell me one number From 0 to 9'))
+try:
 
-frequenza_c = 261.63
-calc_frequenza_c = frequenza_c/(2**(4-octave))
-
-frequenza_d = 293.66
-calc_frequenza_d = frequenza_d/(2**(4-octave))
-
-frequenza_e = 329.63
-calc_frequenza_e = frequenza_e/(2**(4-octave))
-
-frequenza_f = 349.23
-calc_frequenza_f = frequenza_f/(2**(4-octave))
-
-frequenza_g = 392.00
-calc_frequenza_g = frequenza_g/(2**(4-octave))
-
-frequenza_a = 440.00
-calc_frequenza_a = frequenza_a/(2**(4-octave))
-
-frequenza_b = 493.88
-calc_frequenza_b = frequenza_b/(2**(4-octave))
+ note = input('Tell me one letter from A to G')
+ octave = int(input('Tell me one number From 0 to 9'))
 
 
 
-if note[0:1] == 'c' and octave <= 9 and octave >= 0:
-    print(f'The frequenzy of {note.upper()}{octave}  is {calc_frequenza_c:.2f}(HZ)')
-elif note[0:1] == 'd' and octave <= 9 and octave >=0:
-    print(f'The frequenzy of {note.upper()}{octave}  is {calc_frequenza_d:.2f}(HZ)')
-elif note[0:1] == 'e' and octave <= 9 and octave >=0:
-    print(f'The frequenzy of {note.upper()}{octave}  is {calc_frequenza_e:.2f}(HZ)')
-elif note[0:1] == 'f' and octave <= 9 and octave >=0:
-    print(f'The frequenzy of {note.upper()}{octave}  is {calc_frequenza_f:.2f}(HZ)')
-elif note[0:1] == 'g' and octave <= 9 and octave >=0:
-    print(f'The frequenzy of {note.upper()}{octave}  is {calc_frequenza_g:.2f}(HZ)')
-elif note[0:1] == 'a' and octave <= 9 and octave >= 0:
-    print(f'The frequenzy of {note.upper()}{octave}  is {calc_frequenza_a:.2f}(HZ)')
-elif note[0:1] == 'b' and octave <= 9 and octave >=0:
-    print(f'The frequenzy of {note.upper()}{octave}  is {calc_frequenza_b:.2f}(HZ)')
-else:
+
+ if note[0:1] == 'c' and octave <= 9 and octave >= 0:
+    print(f'The frequenzy of {261.63 / (2**(4-octave)):.2f}(HZ)')
+ elif note[0:1] == 'd' and octave <= 9 and octave >=0:
+    print(f'The frequenzy of {293.66 / (2**(4-octave)):.2f}(HZ)')
+ elif note[0:1] == 'e' and octave <= 9 and octave >=0:
+    print(f'The frequenzy of {329.63 / (2 ** (4 - octave)):.2f}(HZ)')
+ elif note[0:1] == 'f' and octave <= 9 and octave >=0:
+    print(f'The frequenzy of {349.23 / (2 ** (4 - octave)):.2f}(HZ)')
+ elif note[0:1] == 'g' and octave <= 9 and octave >=0:
+    print(f'The frequenzy of {392.00 / (2 ** (4 - octave)):.2f}(HZ)')
+ elif note[0:1] == 'a' and octave <= 9 and octave >= 0:
+    print(f'The frequenzy of {440.00 / (2 ** (4 - octave)):.2f}(HZ)')
+ elif note[0:1] == 'b' and octave <= 9 and octave >=0:
+    print(f'The frequenzy of {493.88 / (2 ** (4 - octave)):.2f}(HZ)')
+ else:
     print('Error: your note or octave is not correct!!')
+except:
+    print('Error: please follow the request')
