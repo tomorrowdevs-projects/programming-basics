f18cb4ce49cfd04f0a377f62dede4801657f46b6
marcolepdev <marcolepiane@gmail.com>
2022-02-23 15:19:57 +0100

    solution-head-of-line

--- projects/m4/001-display-the-head-of-a-file/solutions/marcolepiane/solution-head-of-line.js
+++ projects/m4/001-display-the-head-of-a-file/solutions/marcolepiane/solution-head-of-line.js
@@ -5,26 +5,33 @@ let warnMessage = document.querySelector('#warn-message');
 
 btn.addEventListener('click', function(){
 
+
+    //beginning of promise
     let readText = new Promise((resolve, reject)=>{
         let files = input.files;
       
+        //if no files are attached
         if (files.length == 0){
             reject('No files attached: ');
 
+                    //if files are attached
         } else if (files.length == 1){
             
+
             resolve('One file properly attached');
             
             const file = files[0];
 
             let reader = new FileReader();
 
+            //loading text file
             reader.onload = (e) => {
                 const file = e.target.result;
                 const lines = file.split(/\r\n|\n/);
                 h2.innerHTML = lines.join('\n');
             }
-
+            
+//showing the first part of the text content
             reader.readAsText(file.slice(0, 40));
             
         }
