cfdf3b72112afaed8f486fad9ca5471590dfb15e
Davide di Giacomo <91060539+DjGiakma94@users.noreply.github.com>
2022-02-24 18:50:42 +0100

    Update sorted_order.py

--- projects/m2/001-sorted-order/solutions/Davide Di Giacomo/sorted_order.py
+++ projects/m2/001-sorted-order/solutions/Davide Di Giacomo/sorted_order.py
@@ -5,7 +5,7 @@ def zero_stop():
     while number!=0:
         number=int(input("Insert your integer, write 0 to stop: "))
         numbers.append(number)
-    #when input is 0 return list e remove 0
+    #when input is 0 return list and remove 0
     else:
         numbers.remove(numbers[-1])
         return sorted(numbers)
