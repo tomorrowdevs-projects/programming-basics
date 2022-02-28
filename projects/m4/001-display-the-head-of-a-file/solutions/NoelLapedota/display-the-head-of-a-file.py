4e2006e31e564efab689e123c5b9ba918b9ddc14
unknown <mrleon@hotmail.it>
2022-02-28 10:06:20 +0100

    display-the-head-of-a-file

--- projects/m4/001-display-the-head-of-a-file/solutions/NoelLapedota/display-the-head-of-a-file.py
+++ projects/m4/001-display-the-head-of-a-file/solutions/NoelLapedota/display-the-head-of-a-file.py
@@ -1,14 +1,9 @@
 import sys
-
-if len(sys.argv) != 2:
-    print('Enter a argoument')
-else:
-    try:
-        f = open(sys.argv[1])
-        x = lambda a : a * 10
-        print(x(f.readline())) 
-        f.close()
-    except FileNotFoundError:
+try:
+    with open(sys.argv[1],encoding='utf-8') as f:
+        f.readline()[-10:]
+        
+except FileNotFoundError:
          print("The file does not exist.")
-    except IndexError:
+except IndexError:
          print("Please provide a file to read as argument")     
