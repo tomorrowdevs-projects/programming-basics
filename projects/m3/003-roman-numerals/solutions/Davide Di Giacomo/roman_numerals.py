fb71abd6389650f2dd8b6ca9efec136e47815c6e
Davide di Giacomo <davidedigiacomo94@gmail.com>
2022-03-07 16:45:31 +0100

    roman numerals

---
+++ projects/m3/003-roman-numerals/solutions/Davide Di Giacomo/roman_numerals.py
@@ -0,0 +1,22 @@
+#Create a recursive function that converts a Roman numeral to an integer.
+#write a main program that reads a Roman numeral from the user and displays its value.
+
+#create a dictionary
+#slip the input in one single letter, bring the respective value from the dictionary
+
+roman={"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+
+def roman_to_int(rom):
+    
+    
+    
+    
+    
+def main(number):
+    
+    
+rom=str(input("Enter a roman number to convert in a int."))
+number=int(input("enter a number to convert in roman."))
+
+roman_to_int(rom)
+main(number)
