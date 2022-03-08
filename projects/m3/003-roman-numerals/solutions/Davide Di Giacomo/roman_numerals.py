65593971fab087b79a6d5273bcda8174973f7be5
Davide di Giacomo <davidedigiacomo94@gmail.com>
2022-03-08 09:42:27 +0100

    solution/m3-003

--- projects/m3/003-roman-numerals/solutions/Davide Di Giacomo/roman_numerals.py
+++ projects/m3/003-roman-numerals/solutions/Davide Di Giacomo/roman_numerals.py
@@ -5,18 +5,35 @@
 #slip the input in one single letter, bring the respective value from the dictionary
 
 roman={"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+change_roman_in_integer=[]
 
-def roman_to_int(rom):
+#use two counters: n for move throught index and y to find "special characther" like "IV"
+def convert_roman_to_int(letter, n=0, y=1):
+    #if there is only one roman letter to translate
+    if len(letter)==1: return roman[letter[n]]
+    elif y<=len(letter)-1:
+        #if the letters aren't in decreasing way
+        if roman[letter[y-1]]<roman[letter[y]]:
+            #take the respective value from the dictionary
+            correct_number=roman[letter[y]]-roman[letter[y-1]]
+            change_roman_in_integer.append(correct_number)
+            letter.pop(y-1)
+        #if the letters are in decreasing way
+        else: change_roman_in_integer.append(roman[letter[n]])
+        n+=1
+        y+=1
+        convert_roman_to_int(letter, n, y)
+    elif n<=len(letter)-1: 
+        change_roman_in_integer.append(roman[letter[n]])
+        n+=1
+        y+=1
+        convert_roman_to_int(letter, n, y)
+    return sum(change_roman_in_integer)    
     
-    
-    
-    
-    
-def main(number):
-    
-    
-rom=str(input("Enter a roman number to convert in a int."))
-number=int(input("enter a number to convert in roman."))
+def main():
+    rom=str(input("Enter a roman number to convert in a int: "))
+    #put each single letter from the input in a list
+    letter= list(rom)
+    print(f"The value of yout roman numbers is: {convert_roman_to_int(letter)}")
 
-roman_to_int(rom)
-main(number)
+main()
