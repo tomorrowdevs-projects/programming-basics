249d11016fcc805f43695c2a414671a3c14e7f74
Mrleon123 <Mrleon@hotmail.it>
2022-03-10 14:24:49 +0100

    concatenate-multiple-files

--- projects/m4/003-concatenate-multiple-files/NoelLapedota/concatenate-multiple-files.py
+++ projects/m4/003-concatenate-multiple-files/NoelLapedota/concatenate-multiple-files.py
@@ -2,7 +2,7 @@ import sys
 try:
     for file in range(1, len(sys.argv)):
             with open(sys.argv[file],encoding='utf-8') as f:
-             print(f.strip()) 
+             print(f.read().strip()) 
 except FileNotFoundError:
          print("The file does not exist.")
 except IndexError:
