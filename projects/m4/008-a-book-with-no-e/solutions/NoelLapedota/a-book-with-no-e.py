ae818c28b356dc3e20f2a8048993dfa78af58621
Mrleon123 <Mrleon@hotmail.it>
2022-03-10 12:26:12 +0100

    a-book-with-no-e

--- projects/m4/008-a-book-with-no-e/solutions/NoelLapedota/a-book-with-no-e.py
+++ projects/m4/008-a-book-with-no-e/solutions/NoelLapedota/a-book-with-no-e.py
@@ -1,20 +1,28 @@
-import sys 
+import sys
+import re 
 if len(sys.argv) < 1:
     print('The command line argument is omitted.')    
-list={}
+dict={}
 count = 0
 try:
 
   with open(sys.argv[1], 'r')as f:
     for line  in  f: 
-         words = line.lstrip().rstrip().split(" ")  
-         for  word in words:
-           
-            print(word)
-            
-    #for x , y in list.items():   
-     #print(f'The Letter{x} is present in the file {y} times , and Frequency is : {round((y/count)*100, 2)}%")\n The text has {count} words ')
-
+         re.sub(r'[^\w\s]','',line)
+         words = line.lstrip().rstrip().split()  
+         for word in words:
+           if  word.isalpha():
+             if word not in dict :
+                  dict[word] = 1
+                  count += 1
+             else:
+                dict[word] +=1      
+                count += 1
+                   
+    for x , y in dict.items():
+      x= x.upper()   
+      print(f'The word {x} is present in the file {y} times , and Frequency is : {round((y/count)*100, 2)}%")\n') 
+  print(f'The text has {count} words ')
 except FileNotFoundError:
         print('Error')
    
