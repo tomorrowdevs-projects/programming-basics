c8b55e027c4e84ea2663265a425bf725da497799
Mrleon123 <Mrleon@hotmail.it>
2022-03-11 14:18:28 +0100

    names-that-reachned-number-one

--- projects/m4/009-names-that-reachned-number-one/solutions/NoelLapedota/names-that-reachned-number-one.py
+++ projects/m4/009-names-that-reachned-number-one/solutions/NoelLapedota/names-that-reachned-number-one.py
@@ -1,13 +1,7 @@
-#    Write a program that reads every file in the data set and identifies 
-#    all of the names that were most popular in at least one year.
-#    Your program should output two lists: one containing the most popular names
-#    for boys and the other containing the most popular names for girls.
-
-
 # File: names
 boys = []
 girls = []
-year = input('Choose the year you are interested in - use 4 digits -')
+year = input('Choose the year you are interested in - use 4 digits - between 1900 and 2012')
 r = 'names\yob'
 f = '.txt'
 directory = r + year + f
@@ -15,26 +9,19 @@ directory = r + year + f
 with open(directory, 'r') as opened_file:
     #process file
     name = opened_file.read()
-    for find in name:
-        if find == ',F,':
-             if find not in girls:
-               girls.append(find)
-        else:
-            if find not in boys:
-             boys.append(find)
-
-
-    print(f'the female names are:\n {girls}')
+    list=name.split('\n')
+    for  a in list:
+      if ',F,' in a:
+         if a not in girls:
+           girls.append(a)
+           
+      else:
+         if a not in boys:
+             boys.append(a) 
+  # display result      
+        
+print(f'the female names are:\n {girls}')
    
-    print(f'the male names are:\n {boys}')
-     
-       
-
- 
-
-
-
-
-
-
-         
+print(f'the male names are:\n {boys}')
+   
+  
