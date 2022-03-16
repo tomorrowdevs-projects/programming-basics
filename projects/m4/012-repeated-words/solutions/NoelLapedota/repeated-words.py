c36bee34b849fe6026980e733d0dbbe522daff1d
Mrleon123 <Mrleon@hotmail.it>
2022-03-16 11:15:04 +0100

    repeated-words

---
+++ projects/m4/012-repeated-words/solutions/NoelLapedota/repeated-words.py
@@ -0,0 +1,25 @@
+import os, re
+
+
+#   creiamo due listeuna per confronatre le singole parole,
+#   l'altra per inserire le parole ripetute
+
+box = []
+rep_words = []
+count = 0
+
+with open('prova.txt','r') as f:
+  for line in f:
+    count += 1
+    out = re.sub(r'[^\w\s]','',line)
+    sep = out.lower().split()
+    for a in sep :
+         
+        if a not in box:
+              box.append(a)
+          
+        elif a  == box[-1]:
+             rep_words.append(a)
+             print(f'You have repeatued the word : {a} in line {count}')
+              
+
