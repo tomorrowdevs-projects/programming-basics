d962cf9bedd0f6f2c910355a225570cfab56ce79
DinoPC <dino_1991@outlook.it>
2022-03-18 00:09:26 +0100

    solutions area of a room

--- projects/m1/001-area-of-a-room/solutions/DinoJasari/area.py
+++ projects/m1/001-area-of-a-room/solutions/DinoJasari/area.py
@@ -1,5 +1,5 @@
 print ("Questo programma calcola l'area di un stanza in metri.")
-larghezza = input("Inserisci la larghezza col numero decimale ")
-lunghezza = input("inserisci la lunghezza col numero decimale ")
+larghezza = input("Inserisci la larghezza con numero decimale ")
+lunghezza = input("inserisci la lunghezza con numero decimale ")
 risultato = float(larghezza) * float(lunghezza)
 print (risultato)
