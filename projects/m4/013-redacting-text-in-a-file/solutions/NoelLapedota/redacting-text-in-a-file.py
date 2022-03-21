1a7c99be5159edb9e05c8afda8ee6bc06b7f0c80
Mrleon123 <Mrleon@hotmail.it>
2022-03-21 08:57:19 +0100

    redacting-text-in-a-file

--- projects/m4/013-redacting-text-in-a-file/solutions/NoelLapedota/redacting-text-in-a-file.py
+++ projects/m4/013-redacting-text-in-a-file/solutions/NoelLapedota/redacting-text-in-a-file.py
@@ -4,22 +4,29 @@ output_file = input("Enter the name for the new redacted file -including extensi
 
 
 #   files reading + new file
-
+                                              
 try:
     
- with open(input_file_path,'r') as sen, open(output_file,'a') as new_file ,open(sensitive_words_path,'r')as f:
+ with open(input_file_path,'r') as sen, open(output_file,'w') as new_file ,open(sensitive_words_path,'r')as f:
      
-    files = f.read().lower()
-    lista_sen=  sen.read().split()
-    for words in lista_sen:
-         words.lower()
-         for a in files:
-           a.lower()
-#    replace file
+    files = f.read().lower().split()
+    book = sen.read().lower()
+    words= [words.lower().rstrip() for words in files]
+    # il testo viene cifrato
+    for word in words:
+     
+      replaced_text = book.replace(word, '*' * len(word))
+      book = replaced_text         # I had to do this otherwise every time he encrypted only 1 word
+    
+   # write new file
 
-    replaced_text = files.replace(words,'*' * len(words))
     new_file.write(replaced_text)
- print(replaced_text)
+ with open('new_file.txt','r')as file:
+ 
+  print(file.read())
+    
+ 
       
 except FileNotFoundError:
     print('File not found') 
+
