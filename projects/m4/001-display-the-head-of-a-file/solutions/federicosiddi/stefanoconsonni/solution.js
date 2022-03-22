60026b0e23b62a23b959a34d396574206de5577c
Stefano Consonni <stefanoconsonni@ymail.com>
2022-03-22 21:13:32 +0100

    solution for: display the head of a file

--- projects/m4/001-display-the-head-of-a-file/solutions/federicosiddi/stefanoconsonni/solution.js
+++ projects/m4/001-display-the-head-of-a-file/solutions/federicosiddi/stefanoconsonni/solution.js
@@ -1,16 +1,18 @@
 const fileInput = document.querySelector("#file-selector");
 
-fileInput.addEventListener("input", function () {
+fileInput.addEventListener("input", () => {
 	const file = fileInput.files[0];
 	const textType = /.*/;
 
 	if (file.type.match(textType)) {
 		const reader = new FileReader();
-		reader.onload = function () {
-			alert(reader.result);
+		reader.onload = () => {
+			const lines = reader.result.split("\n");
+			const finalArr = lines.filter((line, index) => index < 10);
+			console.log(finalArr);
 		};
 		reader.readAsText(file);
 	} else {
-		alert("File not supported! Please reload the page and enter a new file.");
+		console.log("File not supported! Please reload the page and enter a new file.");
 	}
 });
