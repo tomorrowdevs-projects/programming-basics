45abea55cccfc8dd81d9136c5664b1099465fabe
Domecosma <domij96@hotmail.it>
2022-03-28 14:13:51 +0200

    solution for pig-latin

--- projects/m2/011-pig-latin/DomenicoCosma/index.py
+++ projects/m2/011-pig-latin/DomenicoCosma/index.py
@@ -10,7 +10,7 @@ def pig_latin():
         elif n[0] in cons:
             n = n[1:] + n[0] + 'ay'              #returns the word from the second letter and concatenats the first letter of the word and 'ay' if the first letter of the word is a consonant
         elif n[0] in vowel or n[0].isupper():
-            n = n + 'way'                        #concatenates the word with 'way' if the first letter of the word is a vowel
+            n = n + 'way'                      #concatenates the word with 'way' if the first letter of the word is a vowel
         text_str = ''.join(n)
         print(text_str, end=' ')
 
