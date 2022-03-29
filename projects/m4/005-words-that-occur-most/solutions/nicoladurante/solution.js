19784cf0f854b60dae21039701de7abef1cee0a3
ndurante90 <nickdurante90@gmail.com>
2022-03-29 17:06:20 +0200

    add solution for 005-words-that-occur-most

---
+++ projects/m4/005-words-that-occur-most/solutions/nicoladurante/solution.js
@@ -0,0 +1,73 @@
+let fileSelector = document.getElementById("file-selector");
+fileSelector.addEventListener("change", getWordThatOccursMost);
+
+/**
+ *
+ * @param {*} occurencies
+ * @returns max value of occurencies
+ */
+function getMaxOccurency(occurencies) {
+  let arrayOfValues = Object.entries(occurencies).map((value) => {
+    return value[1];
+  });
+
+  return Math.max(...arrayOfValues);
+}
+
+/**
+ *
+ * @param {*} evt
+ */
+function getWordThatOccursMost(evt) {
+  let file = evt.target.files[0];
+
+  let reader = new FileReader();
+  reader.readAsText(file, "utf-8");
+
+  reader.onload = function (evt) {
+    let wordsOccurencies = {};
+    try {
+      if (evt.target.error) {
+        throw evt.target.error;
+      }
+
+      let rows = [...evt.target.result.split("\n")];
+
+      //remove empty rows
+      rows = rows.filter((row) => row !== "" && row != "\r");
+
+      rows.forEach((row) => {
+        let words = row.split(" ");
+
+        words.forEach((word) => {
+          word = word.toLowerCase().replace(/[^a-z0-9]/gi, "");
+          wordsOccurencies.hasOwnProperty(word)
+            ? wordsOccurencies[word]++
+            : (wordsOccurencies[word] = 1);
+        });
+      });
+
+      //getMaxOccurencyValue
+      let maxOccurencies = getMaxOccurency(wordsOccurencies);
+
+      //filter words by the maxOccurency value
+      let wordsMostOccured = Object.keys(wordsOccurencies).filter(
+        (key) => wordsOccurencies[key] === maxOccurencies
+      );
+
+      //print result
+      document.getElementsByTagName("p")[0].innerText =
+        wordsMostOccured.join(", ");
+
+      if (maxOccurencies > 0) {
+        document.getElementsByTagName(
+          "p"
+        )[0].innerText += `\n occurs ${maxOccurencies} times`;
+      } else {
+        document.getElementsByTagName("p")[0].innerText = `File is empty!`;
+      }
+    } catch (err) {
+      console.log(`${err.name}: ${err.message}`);
+    }
+  };
+}
