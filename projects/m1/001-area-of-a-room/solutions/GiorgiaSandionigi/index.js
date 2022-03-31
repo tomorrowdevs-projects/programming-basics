a2c8984b7b19e19cf6df165478a2c14c76e9dfde
giorgia.sandionigi <giorgiasandionigi2@gmail.com>
2022-03-31 13:21:07 +0200

    SOLUZIONE area of the room

--- projects/m1/001-area-of-a-room/solutions/GiorgiaSandionigi/index.js
+++ projects/m1/001-area-of-a-room/solutions/GiorgiaSandionigi/index.js
@@ -1,1 +1,9 @@
-// prova prova prova
+//inserisco variabili + utilizzo "prompt" per far iserire dati all'utente
+
+let roomWidth = prompt("enter measure Width in meter")
+let roomLenght = prompt("enter measure Lenght in meter")
+let totalArea = roomWidth * roomLenght
+
+//utilizzo "alert" per dare messaggio all'utente
+
+alert ( 'the area of the room is ${totalArea} m²')
