ffcae94ecd45fd442f584e03c080e6532e7dc359
Nicolò Manfredi <nicoondigital@gmail.com>
2022-05-02 09:14:28 +0200

    solution for area-of-a-room exercise

---
+++ projects/m1/001-area-of-a-room/solution/nicolòmanfredi/solution.py
@@ -0,0 +1,16 @@
+x = float(input("Enter width * m"))
+
+y = float(input("Enter length * m"))
+
+# units = [str(value) + "m" for value in x]
+
+# units = [str(value) + "m" for value in y]
+
+z = "area of a room"
+
+z = x * y
+
+print(z)
+
+
+
