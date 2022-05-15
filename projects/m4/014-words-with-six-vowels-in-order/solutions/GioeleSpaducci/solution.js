7610f610d57c57ec6b891870f84d2dec84728ab0
Gioele Spaducci <gioele.spaducci@outlook.it>
2022-05-15 20:11:26 +0200

    m4/014 promise

--- projects/m4/014-words-with-six-vowels-in-order/solutions/GioeleSpaducci/solution.js
+++ projects/m4/014-words-with-six-vowels-in-order/solutions/GioeleSpaducci/solution.js
@@ -1,3 +1,4 @@
+
 let letters = ["a", "e", "i", "o", "u"]
 
 //read file
@@ -31,3 +32,28 @@ async function containsLetters(letters) {  //main function
   }
   alert(matchingWords);  // output words
 }
+
+
+const letters = ["a", "e", "i", "o", "u"];
+
+const text = fetch(prompt("url?"))  //read file
+.then((result)=>result.text());
+
+function checkLetters(word, letters) {  // check if word is containing every letter in list 
+  for (let i = 0; i < letters.length; i++) {
+    if (word.includes(letters[i])) continue
+    else return false
+  }
+  return true
+}
+
+function matchingWords(text) {  //main function
+  const words = text.split(" ");  // separate words
+  let matchingWords = [];
+  for (let word of words) {
+    if (checkLetters(word, letters)) matchingWords.push(word)
+  }
+  alert(matchingWords);  // output words
+}
+
+text.then(result => matchingWords(result));
