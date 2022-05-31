324c54970d37dd079bff6ff095f45e229664cb0d
Ferdinando <ferdi.dagostino@gmail.com>
2022-05-31 14:33:03 +0200

    fixed regex expression

--- projects/m2/013-tokenizing-a-string/solutions/ferdinandodagostino/script.js
+++ projects/m2/013-tokenizing-a-string/solutions/ferdinandodagostino/script.js
@@ -1,7 +1,7 @@
 const input = prompt('Inserisci un espressione matematica');
 
-//regex per rimuovere i caratteri speciali, e li sostituisco con lo spazio
-const str = input.replace(/[^a-z0-9]/g, ' ');
+//regex per rimuovere i caratteri speciali e le lettere, e li sostituisco con lo spazio
+const str = input.replace(/[^0-9]/g, ' ');
 //splitto ciò che rimane usando lo spazio come delimitatore, avendo come risultato un array
 const splitInput = str.split(' ');
 
