91808a8fff1880f9603cd3e80d6704da0bf7e221
gianni <g.ghidoni3@gmail.com>
2022-06-06 21:50:56 +0200

    solution for morse-code

---
+++ projects/m2/019-morse-code/solutions/giannighidoni/solution-morse-code.js
@@ -0,0 +1,53 @@
+const morseCode = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "Á": ".--.-",
+    "É": "..-..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----"
+};
+
+const phrase = "The cat is on the table.";
+const phraseArr = phrase.split("");
+const outputArr = [];
+
+phraseArr.forEach(e => {
+    for(let key in morseCode){
+        if(key === e.toUpperCase()){
+            outputArr.push(morseCode[key]);
+        }
+    }
+})
+const outResult = outputArr.join(" ");
+console.log(outResult);
