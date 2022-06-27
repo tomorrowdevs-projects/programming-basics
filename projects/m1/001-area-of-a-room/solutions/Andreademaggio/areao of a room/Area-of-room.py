93b4c50be339f72c146c41c6424db4a44b49245d
Ademaggio <107548632+Ademaggio@users.noreply.github.com>
2022-06-27 11:05:08 +0200

    Solution area-of-room

---
+++ projects/m1/001-area-of-a-room/solutions/Andreademaggio/areao of a room/Area-of-room.py
@@ -0,0 +1,14 @@
+#Write a program that asks the user to enter the width and length of a room
+#Once these values have been read, your program should compute and display the area of the room
+#The length and the width will be entered as floating-point numbers
+#Include units in your prompt and output message; either feet or meters, depending on which
+#unit you are more comfortable working with.
+
+
+
+width=float(input("width of a room in M"))
+length=float(input("length of a room in M"))
+
+print("area of a room"f'{width*length}')
+
+
