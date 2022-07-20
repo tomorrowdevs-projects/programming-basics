06ca6aedbc553b319c8cbca348a9bcdc66f7af84
Piervito Rinaldi <piervitorinaldi23@gmail.com>
2022-07-20 22:01:58 +0200

    Soluzione dei seguenti esercizi: -area of a room -bottle deposit -units of times

---
+++ projects/m1/001-area-of-a-room/solutions/PiervitoRinaldi/solution_area-of-a-room.py
@@ -0,0 +1,8 @@
+# Calcola l'area di una stanza in metri.
+
+lenght =  5.5 # Inserisci la lunghezza
+width = 6 # Inserisci la larchezza
+
+area = lenght*width
+
+print("%.2f" %(area), 'm')
---
+++ projects/m1/004-units-of-time/solutions/PiervitoRinaldi/units-of-times.py
@@ -0,0 +1,10 @@
+# UNITS OF TIMES
+
+# Calcola i secondi totali assegnando dei parametri in giorni, ore, minuti e secondi.
+
+giorni = 2
+ore = 1
+minuti = 43
+secondi = 23
+
+print('Numero totale di secondi: ', 86400*giorni + 3600*ore + 60*minuti + secondi)
