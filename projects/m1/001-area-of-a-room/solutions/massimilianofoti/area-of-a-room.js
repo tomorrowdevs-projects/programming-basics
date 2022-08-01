06beddccbc9ee717b309805d14c0b5a61f1496af
Massimiliano Foti <max.foti93@hotmail.it>
2022-08-01 23:21:23 +0200

    1° revision of the "area-of-a-room" exercise

---
+++ projects/m1/001-area-of-a-room/solutions/massimilianofoti/area-of-a-room.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")();
+let width = prompt(meters);
+let lenght = prompt(meters);
+let areaOfARoom = (width*lenght);
+
+console.log(areaOfARoom);
+
+/* Write a program that asks the user to enter the width and length of a room. 
+Once these values have been read, your program should compute and display the area of the room. 
+The length and the width will be entered as floating-point numbers. 
+Include units in your prompt and output message; either feet or meters, depending on which unit you are more comfortable working with.*/
