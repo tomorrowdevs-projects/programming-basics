4be89e04e48e91215effdaa7e54b5f0c66647c1f
Lisa <hongli.sasa@gmail.com>
2022-08-29 13:27:45 +0200

    Soluzione bottle_deposits esercizio

---
+++ projects/m1/002-bottle-deposits/solutions/lisaHu/bottle_deposits.py
@@ -0,0 +1,7 @@
+
+sopra1L = input("Enter total number of containers holding more than 1 liter: ")
+sottoUguale1L = input("Enter total number of containers holding 1 liter or less: ")
+
+totalRefund = (float(sopra1L) * 0.25) + (float(sottoUguale1L) * 0.1)
+
+print("Your totale refund is $" + str(round(totalRefund, 2)))
