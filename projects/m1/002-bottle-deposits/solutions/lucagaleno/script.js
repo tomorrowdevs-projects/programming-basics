b659b4b16f4fcedceb5d1da5c9736628fc773869
LG <luca.galeno@gmail.com>
2022-09-13 09:59:55 +0200

    solutions/bottles-deposits

--- projects/m1/002-bottle-deposits/solutions/lucagaleno/script.js
+++ projects/m1/002-bottle-deposits/solutions/lucagaleno/script.js
@@ -2,17 +2,17 @@
 
 alert("Please input the amount of bottles over 1 liter and the amount of bottles equal or less than 1 liter");
 
-let botleOver1Liter = prompt ('How many botles over 1 liter?',0);
-let bottleLess1Liter = prompt ('How many botles less than 1 liter?',0);
+let bottleOver1Liter = prompt ('How many bottles over 1 liter?',0);
+let bottleLess1Liter = prompt ('How many bottles less than 1 liter?',0);
 
-let deposit = botleOver1Liter * 0.25 + bottleLess1Liter * 0.10;
+let deposit = bottleOver1Liter * 0.25 + bottleLess1Liter * 0.10;
 let roundedDeposit = deposit.toFixed(2);
 
 alert('your deposit is: ' + roundedDeposit + ' $');
 
 
 // calcolo del deposito totale rispetto alle bottiglie con funzione
-//function deposit(botleOver1Liter, bottleLess1Liter) {
-//return (botleOver1Liter * 0.25 + bottleLess1Liter * 0.10);
+//function deposit(bottleOver1Liter, bottleLess1Liter) {
+//return (bottleOver1Liter * 0.25 + bottleLess1Liter * 0.10);
 //}
 //console.log(deposit(1, 1));
