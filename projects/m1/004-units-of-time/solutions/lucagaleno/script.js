a13fcc87b8e84a32d3d2f27891bf5ebacdb3f8a5
LG <luca.galeno@gmail.com>
2022-09-14 17:41:56 +0200

    solution of units of time exercise

---
+++ projects/m1/004-units-of-time/solutions/lucagaleno/script.js
@@ -0,0 +1,37 @@
+
+
+// Create a program that reads a duration from the user as a number of days, hours, minutes, and seconds. 
+// Compute and display the total number of seconds represented by this duration.
+
+
+
+let daysInput =  parseFloat(prompt("Please enter the days to be converted:", 0)); 
+let hoursInput = parseFloat(prompt("Please enter the hours to be converted:", 0));
+let minutesInput = parseFloat(prompt("Please enter the minutes to be converted:", 0));
+let secondsInput = parseFloat(prompt("Please enter the seconds to be converted:", 0));
+
+let time = [daysInput,hoursInput,minutesInput,secondsInput];
+
+/*
+function convertNull(nullElement) {
+    if (nullElement=null){
+        alert("trovato");
+        nullElement =  0;
+    };
+};
+
+alert (time)
+
+time.forEach(function(element,index,array) {
+        //convertNull(element);
+        if(element = 'NaN') {
+       // element = 0;
+        array[index] = 0;
+}});   
+*/
+
+alert (time)
+
+let totalSeconds = secondsInput + (minutesInput*60) + (hoursInput*60*60) + (daysInput*24*60*60);
+
+alert ('This is the total seconds: ' + totalSeconds)
