4c43d9e27c0187755b183f8e8eb3f1a75a7bd965
unknown <l.valente99@yahoo.it>
2022-09-15 17:31:30 +0200

    lucavalente--001-Total-the-values.js

--- projects/m3/001-total-the-values/solutions/lucavalente/001-Total-the-values.js
+++ projects/m3/001-total-the-values/solutions/lucavalente/001-Total-the-values.js
@@ -3,7 +3,7 @@ const prompt = require("prompt-sync")();
 
 function sum(){
     var input = prompt("Enter a digit: ");
-    return input === "" ? 0.0 : isNaN(parseFloat(input)) ? 0.0 : parseFloat(input) + sum();
+    return input === "" && isNaN(parseFloat(input)) ? 0.0 : parseFloat(input) + sum();
 }
 
 console.log(sum());
