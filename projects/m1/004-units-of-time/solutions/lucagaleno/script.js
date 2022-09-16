92a795bb176381e6e5a6a6865e8fef2a4d8890e5
LG <luca.galeno@gmail.com>
2022-09-16 11:10:22 +0200

    solution of units of time again

--- projects/m1/004-units-of-time/solutions/lucagaleno/script.js
+++ projects/m1/004-units-of-time/solutions/lucagaleno/script.js
@@ -3,35 +3,23 @@
 // Create a program that reads a duration from the user as a number of days, hours, minutes, and seconds. 
 // Compute and display the total number of seconds represented by this duration.
 
+let time = [
+    parseFloat(prompt("Please enter the days to be converted:", 0)), 
+    parseFloat(prompt("Please enter the hours to be converted:", 0)),
+    parseFloat(prompt("Please enter the minutes to be converted:", 0)),
+    parseFloat(prompt("Please enter the seconds to be converted:", 0))
+];
 
 
-let daysInput =  parseFloat(prompt("Please enter the days to be converted:", 0)); 
-let hoursInput = parseFloat(prompt("Please enter the hours to be converted:", 0));
-let minutesInput = parseFloat(prompt("Please enter the minutes to be converted:", 0));
-let secondsInput = parseFloat(prompt("Please enter the seconds to be converted:", 0));
+time.forEach((element, index) => { 
+    if( isNaN(element) )  {
+        console.log("input: ",element);
+        time[index]=0;
+        }            
+});
+ 
+console.log(time);
 
-// let time = [daysInput,hoursInput,minutesInput,secondsInput];
+let totalSeconds = time[3] + (time[2]*60) + (time[1]*60*60) + (time[0]*24*60*60);
 
-/*
-function convertNull(nullElement) {
-    if (nullElement=null){
-        alert("trovato");
-        nullElement =  0;
-    };
-};
-
-alert (time)
-
-time.forEach(function(element,index,array) {
-        //convertNull(element);
-        if(element = 'NaN') {
-       // element = 0;
-        array[index] = 0;
-}});   
-*/
-
-// alert (time)
-
-let totalSeconds = secondsInput + (minutesInput*60) + (hoursInput*60*60) + (daysInput*24*60*60);
-
-alert ('This is the total seconds: ' + totalSeconds)
+console.log('This is the total seconds: ', totalSeconds);
