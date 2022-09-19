00f560dcfa7871d5fef54ea92709fbec9ff8e60e
Piervito Rinaldi <piervitorinaldi23@gmail.com>
2022-09-19 16:38:58 +0200

    updated_exe_023

--- projects/m1/023-parity-bits/solutions/PiervitoRinaldi/023-parity-bits.py
+++ projects/m1/023-parity-bits/solutions/PiervitoRinaldi/023-parity-bits.py
@@ -5,27 +5,27 @@ while True:
     bit= str(bit)
     len_bit = len(bit)
 
-    for not_0not_1 in bit:
-        if not_0not_1 != '0' and not_0not_1 != '1':
-            print('ERROR: Enter only 0 or 1!')
-            
     if len_bit != 8:
         print('ERROR: Enter an eight line number!')
+        continue
+
+    check = True
+    for not_0not_1 in bit:
+        if not_0not_1 != '0' and not_0not_1 != '1':
+            check = False
+            print('ERROR: Enter only 0 or 1!')
+
             
-    elif bit == '':
-        print('ERROR: Enter an eight line number!')
-            
+    if check == True:
+        count = 0
+        for one in bit:
+            if one == '1':
+                count = count + 1
 
-
-    count = 0
-    for one in bit:
-        if one == '1':
-            count = count + 1
-
-    if count % 2 == 0:
-        print('The parity bit is 0!')
-    else:
-        print('The parity bit is 1!')
-    
+        if count % 2 == 0:
+            print('The parity bit is 0!')
+        else:
+            print('The parity bit is 1!')
+        
     continue
 
