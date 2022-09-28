b02093941cf1fc13a03d3a2529449ba3d2f89acc
Alessia-C <caporasoalessia1611@gmail.com>
2022-09-28 21:17:27 +0200

    solutions/m1-area-of-room

---
+++ projects/m1/001-area-of-a-room/solutions/alessiacaporaso/index.js
@@ -0,0 +1,6 @@
+//prompt length and width. + convert string in number
+const length = parseInt(prompt("Enter the length of room"));
+const width = parseInt(prompt("Enter the width of room"));
+//print area of room
+const area = length * width;
+alert("Area is " + area);
