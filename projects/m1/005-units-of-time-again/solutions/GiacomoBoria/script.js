b916489888b1e37c0551af95f6d56f0af03597b7
Giacomo Boria <g.boria91@gmail.com>
2022-09-28 23:27:40 +0200

    feat(m1): add sinal solution exe005 Units Of Tima Again

--- projects/m1/005-units-of-time-again/solutions/GiacomoBoria/script.js
+++ projects/m1/005-units-of-time-again/solutions/GiacomoBoria/script.js
@@ -19,9 +19,20 @@ const secondsInMinute = seconds; // 60 sec
 const secondsInputUsers = Number(prompt("Insert your time in seconds", 0));
 
 // Convert seconds users in day, hours, minute, seconds
-const userDay = secondsInputUsers / secondsInDay;
-const userHours = (secondsInputUsers % secondsInDay) / secondsInHours;
-const userMinute = (secondsInputUsers % secondsInHours) / secondsInMinute;
-const userSeconds = (secondsInputUsers % secondsInMinute);
+let userDay = secondsInputUsers / secondsInDay;
 
-alert(`I tuoi secondi equivalgono a ${Math.trunc(userDay)}: ${Math.trunc(userHours)}: ${Math.trunc(userMinute)}: ${Math.trunc(userSeconds)}`);
+let userHours = (secondsInputUsers % secondsInDay) / secondsInHours;
+    let numberHoursInt = Math.trunc(userHours); // Take numeber integer
+    let hoursUserInString = String(numberHoursInt); // Convert number in string for to use method pasStart
+
+let userMinute = (secondsInputUsers % secondsInHours) / secondsInMinute;
+    let numberMinuteInt = Math.trunc(userMinute); 
+    let minuteUserInString = String(numberMinuteInt);
+
+let userSeconds = (secondsInputUsers % secondsInMinute);
+    let numberSecondsInt = Math.trunc(userSeconds);
+    let secondsUserInString = String(numberSecondsInt);
+    
+// Output
+// I use the padStart method to give the two-digit result
+alert(`I tuoi secondi equivalgono a ${Math.trunc(userDay)}: ${hoursUserInString.padStart(2, '0')}: ${minuteUserInString.padStart(2, '0')}: ${secondsUserInString.padStart(2, '0')}`);
