920a8d7f4af62a12d5f91a4e4d767fddda8f588c
LG <luca.galeno@gmail.com>
2022-09-30 17:03:26 +0200

    edit of the solution exercise 024

--- projects/m2/024-create-a-bingo-card/solutions/lucagaleno/script.js
+++ projects/m2/024-create-a-bingo-card/solutions/lucagaleno/script.js
@@ -1,6 +1,6 @@
 //024-create-a-bingo-card
 
-function randomBingoCards() {
+function randomBingoCard() {
 
     const bingoCard = {
         'B': [],
@@ -14,60 +14,46 @@ function randomBingoCards() {
         
           while (Object.values(bingoCard[element]).length != 5) {
 
-            //funzione per la creazione di un numero casuale
+            //funzione per la creazione di un numero casuale intero
             function getRndInteger(min, max) {
                 return Math.floor(Math.random() * (max - min + 1) ) + min;
             };
 
-            //con questa funzione l'output non è corretto, come mai?
-            /*
+            //funzione per avere un array con numeri univoci e in ordine crescente            
             function uniqueAndSort(bingoLine){
-                const uniqueNumber = Array.from(new Set(bingoLine)); //! verifico che i numeri casuali siano univoci
+                let uniqueNumber = Array.from(new Set(bingoLine)); //! verifico che i numeri casuali siano univoci
                 bingoLine = uniqueNumber; // assegno i numeri univoci all'array
                 return bingoLine.sort((a,b) => a-b ); //ordino i numeri
             }
-            */
-
+            
             if (element === 'B'){
                 let randomNumber = getRndInteger(1, 15);
                 bingoCard[element].push(randomNumber); //push del numero casuale
-                const test = bingoCard[element];
-                //uniqueAndSort(test);
-                let uniqueNumber = Array.from(new Set(bingoCard[element])); //! creo una copia dell'array con elementi univoci, grazie al Set
-                bingoCard[element] = uniqueNumber; // assegno i numeri univoci all'array
-                bingoCard[element].sort((a,b) => a-b ); //ordino i numeri considerati come integer
+                bingoCard[element] = uniqueAndSort(bingoCard[element]);
             }
             if (element === 'I'){
                 let randomNumber = getRndInteger(16, 30);
                 bingoCard[element].push(randomNumber);
-                let uniqueNumber = Array.from(new Set(bingoCard[element]));
-                bingoCard[element] = uniqueNumber; 
-                bingoCard[element].sort(); 
+                bingoCard[element] = uniqueAndSort(bingoCard[element]);
             }
             if (element === 'N'){
                 let randomNumber = getRndInteger(31 ,45);
                 bingoCard[element].push(randomNumber);
-                let uniqueNumber = Array.from(new Set(bingoCard[element]));
-                bingoCard[element] = uniqueNumber; 
-                bingoCard[element].sort(); 
+                bingoCard[element] = uniqueAndSort(bingoCard[element]);
             }
             if (element === 'G'){
                 let randomNumber = getRndInteger(46, 60);
                 bingoCard[element].push(randomNumber);
-                let uniqueNumber = Array.from(new Set(bingoCard[element]));
-                bingoCard[element] = uniqueNumber;  
-                bingoCard[element].sort(); 
+                bingoCard[element] = uniqueAndSort(bingoCard[element]);
             }
             if (element === 'O'){
                 let randomNumber = getRndInteger(61, 75);
                 bingoCard[element].push(randomNumber);
-                let uniqueNumber = Array.from(new Set(bingoCard[element]));
-                bingoCard[element] = uniqueNumber;
-                bingoCard[element].sort();   
+                bingoCard[element] = uniqueAndSort(bingoCard[element]);
             }
         }
     });
     console.table(bingoCard)
 };
-randomBingoCards();
+randomBingoCard();
 
