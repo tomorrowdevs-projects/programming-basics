9a843827d5b63da3793852bd6d8e23accd431bb9
Andrea <andrea.andolfato90@gmail.com>
2022-10-02 22:58:34 +0200

    M1 - 002

---
+++ projects/m1/002-bottle-deposits/solutions/Andrea Andolfato/002-bottle-deposiy.py
@@ -0,0 +1,10 @@
+
+##BOTTLE DEPOSITS
+
+def = small drink container (less than one liter)
+def = big drink container (one liter or more)
+small drink container = int (input(number of small drink container))
+big drink container = int (input(number of big drink container))
+refund = (small drink container * 0.10) + (big drink container * 0.25)
+print = you earned $(refund)
+
