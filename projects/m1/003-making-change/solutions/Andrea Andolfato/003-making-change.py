72f942649908ea4d192bb71eb968600f9a1a9790
Andrea <andrea.andolfato90@gmail.com>
2022-10-03 16:07:32 +0200

    M1 - 003

---
+++ projects/m1/003-making-change/solutions/Andrea Andolfato/003-making-change.py
@@ -0,0 +1,21 @@
+
+#Definizione delle monete usate:
+
+Def dollar = 1
+Def pennie = 1% dollar
+Def nickel = 5% dollar
+Def dime = 10% dollar
+Def quarter = 25% dollar
+Def loonie = 1 dollar
+Def toonie = 2 dollar
+
+## Definizione delle monete inserite e del prezzo del prodotto:
+
+Def total money = dollar + pennie + nickel + quarter + lonnie + tonnie
+Def total price = dollar* + pennie* + nickel* + quarter* + loonie* + toonie*
+
+### Compito da eseguire:
+
+return = total money - total price
+print return
+
