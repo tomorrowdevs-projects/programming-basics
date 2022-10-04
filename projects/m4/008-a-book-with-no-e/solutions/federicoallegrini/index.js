ba00f5f566a7589eaca2907eefe006c2f5fa7350
Federico Allegrini <federico.allegrini.dev@gmail.com>
2022-10-04 23:59:42 +0200

    Logic completed, to do display method refactoring

---
+++ projects/m4/008-a-book-with-no-e/solutions/federicoallegrini/index.js
@@ -0,0 +1,140 @@
+const fs = require("fs");
+
+const argsFilePath = process.argv.slice(2);
+
+if (checkArgsLength(argsFilePath)) {
+  lettersProportion(argsFilePath[0]);
+}
+
+function lettersProportion(filePath = "") {
+  if (isValidPath(filePath) && fileExists(filePath)) {
+    const fileContent = readFileContent(filePath);
+    if (fileContent) {
+      const words = removeNewLine(fileContent).split(" ");
+      const alphabetLetters = Object.fromEntries(
+        [...Array(26).keys()].map((n) => [
+          String.fromCharCode(n + 97),
+          { totalTimes: 0, inWords: 0, alreadyInWord: false },
+        ])
+      );
+      words.forEach((word) =>
+        removePunctuation(word)
+          .toLowerCase()
+          .split("")
+          .forEach((letter, index, letterList) => {
+            alphabetLetters[letter].totalTimes++;
+            // Check if the letter is already present in the current word
+            if (!alphabetLetters[letter].alreadyInWord) {
+              // If not increment the word counter and set the already value to true
+              alphabetLetters[letter].inWords++;
+              alphabetLetters[letter].alreadyInWord = true;
+            }
+            // If this is the last letter of the word
+            if (index + 1 === letterList.length) {
+              // Reset the already value for all letters of the word
+              letterList.forEach(
+                (letterOfWord) =>
+                  (alphabetLetters[letterOfWord].alreadyInWord = false)
+              );
+            }
+          })
+      );
+
+      // Display proportion for each word
+      const alphabetLettersList = Object.entries(alphabetLetters);
+      const lettersProportion = alphabetLettersList
+        .map(
+          ([letter, { totalTimes, inWords }]) =>
+            `- Letter "${letter.toUpperCase()}" is present ${totalTimes} in ${inWords} words.\n`
+        )
+        .join("");
+      console.log(
+        `[Result] Proportion of the letters in current text:\n${lettersProportion}`
+      );
+
+      // Calculate percentage of presence of each letter in all words
+      // Display letter with less value
+      let calculatedPercentage = 0;
+      const lowProportionLetter = alphabetLettersList.reduce(
+        ({ lowLetter, percentrage }, [letter, { totalTimes }]) => {
+          calculatedPercentage = (totalTimes / words.length) * 100;
+          if (calculatedPercentage < percentrage) {
+            return {
+              lowLetter: letter,
+              percentrage: calculatedPercentage,
+            };
+          }
+          return { lowLetter, percentrage };
+        },
+        {
+          lowLetter: "",
+          percentrage: 100,
+        }
+      );
+      console.log(
+        `[Result] The letter "${lowProportionLetter.lowLetter.toUpperCase()}" is less present, only in ${lowProportionLetter.percentrage.toFixed(
+          2
+        )}% of words.`
+      );
+    }
+  }
+}
+
+function removeNewLine(text = "") {
+  return text.replaceAll("\n", " ").replaceAll("\r", " ");
+}
+function removePunctuation(word) {
+  return word
+    .split("")
+    .filter((char) => /[a-zA-Z]/gim.test(char))
+    .join("");
+}
+
+function checkArgsLength(argsFilePath = "") {
+  const length = argsFilePath.length;
+  if (length === 1) {
+    return true;
+  }
+  console.error(
+    `[Error] You have provided ${length} arguments, you must provide only one path!`
+  );
+  return false;
+}
+
+function isValidPath(path = "") {
+  const regexValidPath =
+    /^(?:[a-z]:)?[\/\\]{0,2}(?:[.\/\\ ](?![.\/\\\n])|[^<>:"|?*.\/\\ \n])+$/gim;
+  if (regexValidPath.test(path)) {
+    return true;
+  }
+  console.error(`[Error] Invalid path ${path}!`);
+  return false;
+}
+
+function fileExists(path = "") {
+  try {
+    const exists = fs.existsSync(path);
+    if (exists) {
+      return true;
+    }
+    console.error(`[Error] File "${path}" doesn't exists!`);
+    return false;
+  } catch (err) {
+    console.error(`[Error] File "${path}" doesn't exists!`);
+    return false;
+  }
+}
+
+function readFileContent(path) {
+  try {
+    const fileContent = fs.readFileSync(path, "utf8");
+    if (fileContent.trim() === "") {
+      console.error(`[Error] The file "${path}" is empty!`);
+      return false;
+    }
+    return fileContent;
+  } catch (err) {
+    console.error(`[Error] Cannot read the file "${path}"!`);
+    return false;
+  }
+}
