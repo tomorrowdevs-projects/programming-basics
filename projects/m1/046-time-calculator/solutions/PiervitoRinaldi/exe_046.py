6d6fa23d73be417574ffbdd306528c95c080b5d0
Piervito Rinaldi <piervitorinaldi23@gmail.com>
2022-10-06 23:38:45 +0200

    update_exe_046

--- projects/m1/046-time-calculator/solutions/PiervitoRinaldi/exe_046.py
+++ projects/m1/046-time-calculator/solutions/PiervitoRinaldi/exe_046.py
@@ -1,47 +1,63 @@
-def add_time_no_day(start, duration, day): # Enter 'start' and 'duration' with the time format 00:00
-    s_hour = int(start[:2])                # I crated differnt variables: start minutes and start hours
-    s_minuts = int(start[-2:])
-
-    d_hour = int(duration[:2])             # The seem for the duration minutes and duration hours
-    d_minuts = int(duration[-2:])
-    sum_minuts = s_minuts + d_minuts
-    sum_hour = s_hour + d_hour
-    n = 0
-
-    if sum_minuts >= 60:                   # The remaning minutes will be come hours
-        sum_minuts = sum_minuts - 60
-        sum_hour += 1
-    while sum_hour >= 24:                  # The remaning hour will be come days
-        sum_hour = sum_hour - 24
-        n += 1
+def add_time(start, duration, day=None):
+    s_hour = int(start[:-6])                # I crated differnt variables: start minutes and start hours
+    s_minutes = int(start[-5:-3])
+    day_period = str(start[-2:])
     
-    if day == '':       # I insert a condition if the function is given the optional day of the week parameter
-        
-        if n == 1:
-            result = str("%02i" %(sum_hour)) + ':' + str("%02i" %(sum_minuts)) + ', next day'
-        else:
-            result = str("%02i" %(sum_hour)) + ':' + str("%02i" %(sum_minuts)) + ', ' + str(n) + ' days later'
+    d_hour = int(duration[:-3])             # The seem for the duration minutes and duration hours
+    d_minutes = int(duration[-2:])
 
+
+    sum_minutes = s_minutes + d_minutes
+    sum_hour = s_hour + d_hour
+    count_day = 0
+
+    if sum_minutes >= 60:                   # The remaning minutes will be come hours
+        sum_minutes = sum_minutes - 60
+        sum_hour += 1
+
+    while sum_hour >= 12:                  # The reamning hours will be come days
+        if day_period == 'PM':
+            sum_hour -= 12
+            count_day += 1
+            day_period = 'AM'
+            
+        elif day_period == 'AM':
+            sum_hour -= 12
+            day_period = 'PM'
+
+    if sum_hour == 0:                       # Alter the zero in tewlve
+        sum_hour = 12
+        
+    if day == None:                         # I insert a condition if the function is given the optional day of the week parameter
+        
+        if count_day == 1:
+            result = f'{sum_hour:02}' + ':' + f'{sum_minutes:02}' + ' ' + str(day_period) + ', next day'
+        elif count_day > 1:
+            result = f'{sum_hour:02}' + ':' + f'{sum_minutes:02}' + ' ' + day_period + ', ' + str(count_day) + ' days later'
+        elif count_day == 0:
+            result = f'{sum_hour:02}' + ':' + f'{sum_minutes:02}' + ' ' + day_period
         return result
 
     else:
-        while n > 7:
-            n -= - 7
+        while count_day > 7:
+            count_day -= - 7
 
-        if day == 'monday' or day == 'Monday':
-            n_day_w = 1 + n 
-        elif day == 'Tuesday' or day == 'tuesday':
-            n_day_w = 2 + n 
-        elif day == 'wednesday' or day == 'Wednesday':
-            n_day_w = 3 + n
-        elif day == 'Thursday' or day == 'thursday':
-            n_day_w = 4 + n
-        elif day == 'friday' or day == 'Friday':
-            n_day_w = 5 + n 
-        elif day == 'saturday' or day == 'Saturday':
-            n_day_w = 6 + n 
-        elif day == 'sunday' or day == 'Sunday':
-            n_day_w = 7 + n
+        day = day.casefold()                # Return a string where all characters are lower case
+
+        if day == 'monday':
+            n_day_w = 1 + count_day 
+        elif day == 'tuesday':
+            n_day_w = 2 + count_day
+        elif day == 'wednesday':
+            n_day_w = 3 + count_day
+        elif day == 'thursday':
+            n_day_w = 4 + count_day
+        elif day == 'friday':
+            n_day_w = 5 + count_day 
+        elif day == 'saturday':
+            n_day_w = 6 + count_day 
+        elif day == 'sunday':
+            n_day_w = 7 + count_day
             
         if n_day_w == 1 or n_day_w == 8:
             n_day_w = 'Monday'
@@ -58,10 +74,23 @@ def add_time_no_day(start, duration, day): # Enter 'start' and 'duration' with t
         elif n_day_w == 7 or n_day_w == 14:
             n_day_w = 'Sunday'
 
-        if n == 1:
-            result = str("%02i" %(sum_hour)) + ':' + str("%02i" %(sum_minuts)) + ' ' + str(n_day_w) + ', next day'
-        else:
-            result = str("%02i" %(sum_hour)) + ':' + str("%02i" %(sum_minuts)) + ' ' + str(n_day_w) + ', ' + str(n) + ' days later'
-
+        if count_day == 1:
+            result = f'{sum_hour:02}' + ':' + f'{sum_minutes:02}' + ' ' + day_period + ' ' + str(n_day_w) + ', next day'
+        elif count_day > 1:
+            result = f'{sum_hour:02}' + ':' + f'{sum_minutes:02}' + ' ' + day_period + ' ' + str(n_day_w) + ', ' + str(count_day) + ' days later'
+        elif count_day == 0:
+            result = f'{sum_hour:02}' + ':' + f'{sum_minutes:02}' + ' ' + day_period + ' ' + str(n_day_w)
         return result
     
+def main():
+ 
+    print(add_time("3:00 PM", "3:10"))
+    print(add_time("11:30 AM", "2:32", "monday"))
+    print(add_time("11:43 AM", "00:20"))
+    print(add_time("10:10 PM", "3:30"))
+    print(add_time("11:43 PM", "24:20", "tueSday"))
+    print(add_time("6:30 PM", "205:12"))
+
+main()
+
+
