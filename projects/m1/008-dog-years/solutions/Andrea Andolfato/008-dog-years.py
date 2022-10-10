493266ec6ca892c522c28ae863b058419c701fd8
Andrea <andrea.andolfato90@gmail.com>
2022-10-10 22:30:49 +0200

    M1 - 008

---
+++ projects/m1/008-dog-years/solutions/Andrea Andolfato/008-dog-years.py
@@ -0,0 +1,10 @@
+
+human year = int (1;2;3;4;5;6;7;8;9;10)
+dog year = float (10.5)
+if human year = 1
+human year = dog year * 1
+if human year = 2
+human year = dog year * 2
+if human year > 2 ≤ 10
+human year = dog year * 4
+elif print (“Year’s number cant be negative, equal to 0 or > 10”)
