fc036fb52f2c4342716a59601e4d14f53fddf1aa
LG <luca.galeno@gmail.com>
2022-10-11 07:34:16 +0200

    feat(m2): solution exe 001

---
+++ projects/m3/001-total-the-values/solutions/lucagaleno/main.js
@@ -0,0 +1,17 @@
+//001-total-the-value
+
+const prompt = require('prompt-sync')();
+
+function totalTheValues(){
+
+    let value = parseFloat(prompt('Enter the value (empty to stop enetring value): '));
+    
+    if ((isNaN(value))){
+        return 0.0;
+    } else {
+        return value += totalTheValues();
+    }; 
+};
+
+console.log(`This is the total values ${totalTheValues().toFixed(1)} `)
+
