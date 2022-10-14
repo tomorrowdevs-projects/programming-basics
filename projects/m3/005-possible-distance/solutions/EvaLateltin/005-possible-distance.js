67a2c1dddb09fb4de61bd5bf2e9dd9bbe8578242
Eva Lateltin <evalateltin@gmail.com>
2022-10-14 21:17:46 +0200

    feat(m3): improved solution 005 possible distance

--- projects/m3/005-possible-distance/solutions/EvaLateltin/005-possible-distance.js
+++ projects/m3/005-possible-distance/solutions/EvaLateltin/005-possible-distance.js
@@ -1,39 +1,33 @@
-/*
-LOGIC
-Create a recursive function that create all the possible combinations of n coins. 
-Initially, store these combinations as arrays and check if their sum is equal to the entered number of coins. 
-Then, multiply each coin for their value and then add them all together. 
-Store all of these combination in an array.
-When the recursive function has ended, check if the array of combinations contains the entered dollar amount. 
-If yes, return true, else return false.
-*/
+function checkResult(
+  coins,
+  dollarAmount,
+  coinValues = [0.25, 0.1, 0.05, 0.01]
+) {
+  // Remove approximation errors
+  dollarAmount = dollarAmount.toFixed(2);
 
-const coins = +prompt("Enter a number of coins."); // Number of coins
-const coinValues = [0.25, 0.1, 0.05, 0.01]; // Value of coins: quarters, dimes, nickels and pennies
-const combinationArray = []; // This array will contain the possible values of the combination of the n entered coins
-const dollarAmount = +prompt("Enter an amount of dollars.");
+  // This is the solution because it is the only condition that might return true
+  const solution = coinValues.some(
+    (element) => (element * coins).toFixed(2) === dollarAmount
+  );
 
-// The array in the argument is used for recursion purpose; a default value is set when initialize the function so it is not required any parameter.
-function getCombinations(array = []) {
-  // This loop create all possible combinations of coins in array form: [quarters, dimes, nickels, pennies]
-  for (let i = coins; i >= 0; i--) {
-    let tempResult = [i].concat(array);
-    if (tempResult.length === coinValues.length) {
-      // Check if the sum of the coins in the combination is equal to the entered coin amount
-      if (tempResult.reduce((acc, curr) => acc + curr) === coins) {
-        //Map: multiply each coin's quantity times the coin's value
-        // Reduce: return the sum of all the coins used
-        tempResult = tempResult
-          .map((value, index) => value * coinValues[index])
-          .reduce((acc, curr) => acc + curr);
-        combinationArray.push(tempResult);
-      }
-    } else {
-      // Run the function again if the coin's combination is not long enough
-      getCombinations(tempResult);
-    }
+  // base case: true
+  if (solution) {
+    return true;
+
+    // base case: false
+  } else if (
+    dollarAmount < 0 ||
+    coins === 0 ||
+    coins * Math.min(...coinValues) > dollarAmount ||
+    coins * Math.max(...coinValues) < dollarAmount
+  ) {
+    return false;
+
+    // recursion step
+  } else {
+    return coinValues.some((value) => {
+      return checkResult(coins - 1, dollarAmount - value);
+    });
   }
 }
-getCombinations();
-
-console.log(combinationArray.includes(dollarAmount));
