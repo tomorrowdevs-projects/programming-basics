7368c4873d585e193975d141b2798a736fb8fd00
Andrea <andrea.andolfato90@gmail.com>
2022-10-21 19:58:33 +0200

    M1 - 004

--- projects/m1/004-units-of-time/solutions/AndreaAndolfato/Units-Of-Time.py
+++ projects/m1/004-units-of-time/solutions/AndreaAndolfato/Units-Of-Time.py
@@ -1,12 +1,12 @@
 
-#Definizione delle singole unita' di tempo
+#Definizione delle singole unita' di tempo:
 
-days=int(input(insert the number of days)
-hours=int(input(insert the number of hours)))
-minutes=int(input(insert the number of minutes))
-seconds=int(input(insert the number of seconds))
+days = int(input("insert the number of days"))
+hours = int(input("insert the number of hours"))
+minutes = int(input("insert the number of minutes"))
+seconds = int(input("insert the number of seconds"))
 
-#Calcolo del tempo totale in secondi
+#Calcolo del tempo totale in secondi:
 
-total=((60*60*24*days)+(60*60*hours)+(60*minutes)+seconds)
-print=("il tempo totale in secondi è" + str (totale))
+total_seconds = (60*60*24*days) + (60*60*hours) + (60*minutes) + seconds
+print(total_seconds)
