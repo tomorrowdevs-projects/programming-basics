64659166f07a0983b45cb50d5024890c6e3b97f2
Andrea <andrea.andolfato90@gmail.com>
2022-10-21 19:30:05 +0200

    M1 - 003

--- projects/m1/003-making-change/solutions/Andrea Andolfato/003-making-change.py
+++ projects/m1/003-making-change/solutions/Andrea Andolfato/003-making-change.py
@@ -1,21 +1,15 @@
 
-#Definizione delle monete usate:
-
-Def dollar = 1
-Def pennie = 1% dollar
-Def nickel = 5% dollar
-Def dime = 10% dollar
-Def quarter = 25% dollar
-Def loonie = 1 dollar
-Def toonie = 2 dollar
-
-## Definizione delle monete inserite e del prezzo del prodotto:
-
-Def total money = dollar + pennie + nickel + quarter + lonnie + tonnie
-Def total price = dollar* + pennie* + nickel* + quarter* + loonie* + toonie*
-
-### Compito da eseguire:
-
-return = total money - total price
-print return
+cents_user = (int(input("please insert how much cent you have")))
+penny = cents_user
+nickel = cents_user * 5
+dime = cents_user * 10
+quarter = cents_user * 25
+loonie = cents_user * 100
+toonie = cents_user * 200
+dollar = loonie
+total_price = (int(input("please insert total price product")))
+if cents_user < total_price:
+    print("you need to insert more money")
+if total_price < cents_user:
+    print("Your change is" " " (total_price)
 
