4356c01ae459560cb71ff6427f847e004c17d732
Andrea <andrea.andolfato90@gmail.com>
2022-10-22 13:33:16 +0200

    M1 - 006

--- projects/m1/006-sum-of-digits-in-a-integer/solutions/Andrea Andolfato/sum-of-digits.py
+++ projects/m1/006-sum-of-digits-in-a-integer/solutions/Andrea Andolfato/sum-of-digits.py
@@ -1,12 +1,10 @@
 
-#read a four digit integer from the user:
-integer = input (‘insert a four digit number’)
+integer = input ("insert a four digit number")
 
-dig_1 = int (integer [0])
+dig_1 = int(integer[0])
 dig_2 = int(integer[1])
 dig_3 = int(integer[2])
 dig_4 = int(integer[3])
 
-#display the sum of its digits:
-print (f’sum [(dig_1) + (dig_2) + (dig_3) + (dig_4)])
-
+sum = dig_1 + dig_2 + dig_3 + dig_4
+print(sum)
