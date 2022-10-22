3032891968c95bd1dc589dcc78bb7fb4e43fb524
Andrea <andrea.andolfato90@gmail.com>
2022-10-22 14:21:16 +0200

    M1 - 005

--- projects/m1/005-units-of-time-again/solutions/Andrea Andolfato/Units-of-time.py
+++ projects/m1/005-units-of-time-again/solutions/Andrea Andolfato/Units-of-time.py
@@ -1,12 +1,17 @@
 
-# Calcolo delle variabili day-hour-min, dato il totala dei secondi:
 
-total sec = int(input('Enter the number of seconds'))
+from ctypes.wintypes import HHOOK
 
-day = int(input(total_sec / 86400))
-hour = int(input(total_sec / 3600))
-min = int(input(total_sec / 3600) / 60)
+
+total_sec = int(input('Enter the number of seconds'))
+
+D = float(total_sec / 86400)
+HH = float(total_sec / 3600)
+MM = float(total_sec / 3600) / 60
+S = float[(total_sec - D) - HH]
+SS = float(S - MM)
 
 # Inserire il risultato precedente nella forma D:HH:MM:SS
 
-print(day "+" hour "+" min)
+final_count = (D) (HH) (MM) (SS)
+print(final_count)
