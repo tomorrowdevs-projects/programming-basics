ac64a39cfe7805af73ca9ba88e7485bd962c565c
LucaValente99 <105355850+LucaValente99@users.noreply.github.com>
2022-10-22 12:07:57 +0200

    Update utils.js

--- projects/m4/013-redacting-text-in-a-file/solutions/lucavalente/utils.js
+++ projects/m4/013-redacting-text-in-a-file/solutions/lucavalente/utils.js
@@ -1,27 +1,3 @@
-
- /**
-  * given a text, the function return the list of words contained
-  * @param {string} arg of words to check; 
-  * @return {Array<string>} List of words to check; 
-  */ 
- 
-  function errorCheck(wordList){
-
-    if (!Array.isArray(wordList)){
-        throw new Error("You have to use an array of words as input!");
-    }
-    /**  the function isStringArray is necessary to check if array contains just strings
-        * @param {Array<>} arg array to check; 
-        * @return {Bool} true: array contains just strings, false otherwise; 
-    */
-    const isStringsArray = arr => arr.every(i => typeof i === "string")
-
-    if (!isStringsArray(wordList)){
-        throw new Error("You could use just an array of words!");
-    }
-
- }
- 
 function stringOfWords(text){
 
   /** words into the text
