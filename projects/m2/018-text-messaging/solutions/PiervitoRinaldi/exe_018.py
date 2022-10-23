94150d72e0c0556682f32b6596a709c6dbba177b
Piervito Rinaldi <piervitorinaldi23@gmail.com>
2022-10-23 11:51:52 +0200

    feat(m2): added_exe_018

---
+++ projects/m2/018-text-messaging/solutions/PiervitoRinaldi/exe_018.py
@@ -0,0 +1,24 @@
+text = input('Enter text: ')
+text = text.upper()
+
+letters_keys = {
+    '1': ['.', ',', '?', '!', ':'], 
+    '2': ['A', 'B', 'C'], 
+    '3': ['D', 'E', 'F'],
+    '4': ['G', 'H', 'I'],
+    '5': ['J', 'K', 'L'],
+    '6': ['M', 'N', 'O'],
+    '7': ['P', 'Q', 'R', 'S'],
+    '8': ['T', 'U', 'V'],
+    '9': ['W', 'X', 'Y' ,'Z'],
+    '0': [' ']
+    }
+
+result = ''
+for letter in text:   
+    for k in letters_keys:       
+        if letter in letters_keys[k] :
+            letter_index = letters_keys[k].index(letter)
+            result += k * (letter_index + 1)     # Moltiplico la chiave per il suo indice, togliendo lo 0 come indice.
+
+print(result)
