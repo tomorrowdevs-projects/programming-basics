764caed0699f48a872f74260a3d538a1f911e5cd
LG <luca.galeno@gmail.com>
2022-10-25 14:03:30 +0200

    feat(m3): encoding & decoding solutions

--- projects/m3/007-run-length-decoding/solutions/lucagaleno/main.js
+++ projects/m3/007-run-length-decoding/solutions/lucagaleno/main.js
@@ -1,10 +1,10 @@
-//007-run-length-decoding
+//007-run-length-decoding & 008-run-length-encoding
 
 const list = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B","B","A","A","A","A","A","A","B"]; //[A,1,B,2]
 
-function decoding(list, count) {
+function codingCompression(list, count) {
 
-    let count = count || 1;
+    count = count || 1;
     const listLength = list.length 
 
     //base
@@ -16,8 +16,8 @@ function decoding(list, count) {
     //recursion
     if (list[0] != list[1]) {
 
-        let list0 = decoding([list[0]], count)
-        let list1 = decoding(list.slice(1))
+        let list0 = codingCompression([list[0]], count)
+        let list1 = codingCompression(list.slice(1))
 
         return list0.concat(list1)
     }
@@ -25,10 +25,29 @@ function decoding(list, count) {
     if (list[0] === list[1]) {
 
         tempList = list.slice(1)
-        return decoding(tempList, count += 1)
+        return codingCompression(tempList, count += 1)
     }
 }
 
-console.log(`decoded list of ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B","B","A","A","A","A","A","A","B"]:
-${decoding(list)}`)
 
+
+function decoding(compressedList){
+
+let first2Elements = compressedList.slice(0,2)
+
+if (compressedList.length === 0) {
+    return compressedList;
+}
+    for (let i = 1; i < first2Elements[1]; i++) {       
+        first2Elements.push(first2Elements[0]);
+    }
+    first2Elements = first2Elements.filter(element => typeof element === 'string');
+    if (compressedList.splice(0,2).length != 0) { //splice alredy "cut" the array here
+        lastElements = decoding(compressedList);
+    } else { lastElements = [] }
+return first2Elements.concat(lastElements)
+}
+
+console.log(`input: ${list}`)
+console.log(`encoding function: ${codingCompression(list)}`)
+console.log(`decoding function: ${decoding(codingCompression(list))}`)
