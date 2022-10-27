d9ddf3065ee8fecbce56ce0ad621a99cc38f0f25
Piervito Rinaldi <piervitorinaldi23@gmail.com>
2022-10-28 01:01:14 +0200

    feat(m2): added_exe_020

---
+++ projects/m2/020-write-out-numbers-in-english/solution/Piervitorinaldi/exe_020.py
@@ -0,0 +1,50 @@
+numbers = {
+    '1': 'one', '2': 'two', '3': 'trhee', 
+    '4': 'four', '5': 'five', '6': 'six',
+    '7': 'seven', '8': 'eight', '9': 'nine'
+}
+
+tens_under_twenty = {
+    '10': 'ten', '11': 'eleven', '12': 'twelve', '13': 'thirteen',
+    '14': 'fourteen', '15': 'fiveteen', '16': 'sixteen', 
+    '17': 'seventeen', '18': 'eigtheen', '19': 'nineteen'
+}
+
+tens_over_twenty = {
+    '2': 'twenty', '3': 'therty', '4': 'fourty',
+    '5': 'fifty', '6': 'sixty', '7': 'seventy',
+    '8': 'eighty', '9': 'ninty'
+}
+
+number = input('Enter number: ')
+
+def number_2_english(number):
+    result = ''
+    for n in number:
+        if len(number) == 3:
+            result += f'{numbers[n]} hundred '
+            number = number[1:]
+
+        elif len(number) == 2:
+            if n == '0':
+                number = number[1:]
+                continue
+            if n == '1':
+                result += f'{tens_under_twenty[number]} '
+                break
+            else:
+                result += f'{tens_over_twenty[n]} '
+                number = number[1:]
+
+        elif len(number) == 1:
+            if n == '0':
+                break
+            else:         
+                result += f'{numbers[n]}'
+
+    return result
+
+print(number_2_english(number))
+        
+            
+
