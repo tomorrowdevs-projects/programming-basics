4fbb48c8c9f6c849fea0a77a80a0d13c70e94133
Andrea <andrea.andolfato90@gmail.com>
2022-10-27 21:55:10 +0200

    M1 - 010

--- projects/m1/010-month-name-to-number-of-days/solutions/Andrea Andolfato/month-name-to-number-of.py
+++ projects/m1/010-month-name-to-number-of-days/solutions/Andrea Andolfato/month-name-to-number-of.py
@@ -1,14 +1,15 @@
 
 
-month_user = input("Enter the name of a month")
-days_30 = "november" + "april" + "june" + "september"
-days_31 = "january" + "march" + "may" + "july" + "august" + "october" + "december"
+month_user = input("Enter the name of a month: ")
+days_30 = "novemberapriljuneseptember"
+days_31 = "januarymarchmayjulyaugustoctoberdecember"
 
 if month_user in days_30:
-  print("month_user has 30 days")
-
-if month_user in days_31:
-  print("month_user has 31 days")
-
+  month_user_one = "This month has 30 days"
+  print(month_user_one)
+elif month_user in days_31:
+  month_user_two = "This month has 31 days"
+  print(month_user_two)
 else:
   print("February is a leap year")
+
