58f233e10beb198978f79ae338a810c0be0f6aed
Andrea <andrea.andolfato90@gmail.com>
2022-10-27 23:13:50 +0200

    M1 - 008

--- projects/m1/008-dog-years/solutions/Andrea Andolfato/008-dog-years.py
+++ projects/m1/008-dog-years/solutions/Andrea Andolfato/008-dog-years.py
@@ -3,11 +3,10 @@
 
 human_years = int(input("Please enter human years: "))
 
-if human_years < 1:
-  print("please no negative numbers")
-if human_years >=1 <=2:
+if human_years <=2:
   dog_years = human_years * 4
   print(dog_years)
-if human_years > 2:
+
+else:
   dog_years = human_years * 7.5
   print(dog_years)
