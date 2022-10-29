da116558ccfd03aa94aa5ba5a2a3cde368359034
Stefano Alfeo <stealfeo@gmail.com>
2022-10-29 19:50:48 +0200

    solution-making-change

---
+++ projects/m1/003-making-change/solutions/stefanoalfeo/solution-making-change.py
@@ -0,0 +1,52 @@
+read_penny = 2953
+import math
+
+# definizione valore monete
+penny = 1
+nickel = 5
+dime = 10
+quarter = 25
+loonie = 100
+toonie = 200
+
+print("total change:")
+# inizio con toonie
+coin = toonie
+change = read_penny / coin
+next_change = read_penny % coin
+print("toonies: " + str(math.trunc(change)))
+
+# passo a loonie
+coin = loonie
+if(next_change >= coin):
+    change = next_change / coin
+    next_change = next_change % coin
+    print("loonies: " + str(math.trunc(change)))
+
+# passo a quarter
+coin = quarter
+if(next_change >= coin):
+    change = next_change / coin
+    next_change = next_change % coin
+    print("quarters: " + str(math.trunc(change)))
+
+# passo a dime
+coin = dime
+if(next_change >= coin):
+    change = next_change / coin
+    next_change = next_change % coin
+    print("dimes: " + str(math.trunc(change)))
+
+# passo a nickel
+coin = nickel
+if(next_change >= coin):
+    change = next_change / coin
+    next_change = next_change % coin
+    print("nickel: " + str(math.trunc(change)))
+
+# passo a penny
+coin = penny
+if(next_change >= coin):
+    change = next_change / coin
+    next_change = next_change % coin
+    print("penny: " + str(math.trunc(change)))
