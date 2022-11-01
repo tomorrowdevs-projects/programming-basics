ea8a1252c503b6c808c7ea249041071f9f600615
shilux42 <141simone@gmail.com>
2022-11-01 14:57:54 +0100

    add a comment to solution012

--- projects/m1/012-what-color-is-that-square/solutions/simoneRoscio/solution012.py
+++ projects/m1/012-what-color-is-that-square/solutions/simoneRoscio/solution012.py
@@ -11,10 +11,11 @@ input_square = str.casefold(input('Write the name of a square in a chess board: 
 column = input_square[0]
 row = int(input_square[1])
 
+# column starting color
 black_column = 'aceg'
 white_column = 'bdfh'
 
-# ceck if the column start whit a black or a white square then if the row is even (the starting color is even) or odd and print the result
+# ceck if the column start whit black or white square then if the row is even (the starting color is even) or odd and print the result
 if column in black_column:
     if row % 2 == 0:
         print('Your square is white')
