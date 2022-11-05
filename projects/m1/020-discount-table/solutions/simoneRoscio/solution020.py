685d021e082564d3f0cdda73674d7f61ca3b5612
shilux42 <141simone@gmail.com>
2022-11-05 15:01:05 +0100

    commit solution 020-discount-table

--- projects/m1/020-discount-table/solutions/simoneRoscio/solution020.py
+++ projects/m1/020-discount-table/solutions/simoneRoscio/solution020.py
@@ -7,10 +7,10 @@ for purchases of $4.95, $9.95, $14.95, $19.95 and $24.95.
 Ensure that the discount amounts and the new prices are rounded to 2 decimal places when they are displayed.
 """
 
+# Collect the price
 price_list = [4.95, 9.95, 14.95, 19.95, 24.95]
 
-index = 0
-
+# Format the table
 title_1 = 'Original price'
 title_2 = 'Discount amount'
 title_3 = 'New price'
@@ -21,6 +21,8 @@ row_3 = title_3 + ' ' * (20 - len(title_3))
 
 print(f'{row_1} {row_2} {row_3}')
 
+# For loop with operation and pseudo-formatting
+index = 0
 for i in price_list:
     discount_amount = round(price_list[index] / 100 * 60, 2)
     new_price = round(price_list[index] - discount_amount, 2)
