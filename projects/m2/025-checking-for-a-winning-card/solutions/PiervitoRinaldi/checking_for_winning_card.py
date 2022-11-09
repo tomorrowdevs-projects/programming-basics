d4065f509226e00ce1e6797c4874b6d88bfd7a85
Piervito Rinaldi <piervitorinaldi23@gmail.com>
2022-11-10 00:58:17 +0100

    update_checking-winning-card

--- projects/m2/025-checking-for-a-winning-card/solutions/PiervitoRinaldi/checking_for_winning_card.py
+++ projects/m2/025-checking-for-a-winning-card/solutions/PiervitoRinaldi/checking_for_winning_card.py
@@ -9,22 +9,25 @@ def checking_for_vertical_line(bingo_card):
 def checking_for_diagonal_line(bingo_card):
     i_d_0 = 0
     i_d_4 = 4
-    diagonal_line = 0
+    diagonal_line_one = 0
+    diagonal_line_two = 0
     result = False
     for key in bingo_card:
         if bingo_card[key][i_d_0] == 0:
-            diagonal_line += 1
+            diagonal_line_one += 1
             i_d_0 += 1
-            if diagonal_line == 5:     
+            if diagonal_line_one == 5:     
                 result = True
-                break
-        elif bingo_card[key][i_d_4] == 0:
-            diagonal_line += 1
+                
+    for key in bingo_card:
+        if bingo_card[key][i_d_4] == 0:
+            diagonal_line_two += 1
             i_d_4 -= 1
-            if diagonal_line == 5:     
+            if diagonal_line_two == 5:     
                 result = True
-                break
+                
     return result
+
 
 def checking_for_horizontal_line(bingo_card):
     result = False
@@ -57,7 +60,7 @@ def display_bingo_card(bingo_card):
 
 def main():
     diagonal_winning_card = {
-        'B': [0,7,10,11,0], 'I': [17,0,20,0,29], 'N': [32,34,0,39,45], 'G': [47,0,51,0,59], 'O': [0,64,67,71,0]
+        'B': [0,7,10,11,14], 'I': [17,0,20,0,29], 'N': [32,34,0,39,45], 'G': [47,0,51,0,59], 'O': [0,64,67,71,0]
     }
     vertical_winning_card = {
         'B': [3,7,10,11,14], 'I': [0,0,0,0,0], 'N': [32,34,36,39,45], 'G': [47,50,51,55,59], 'O': [63,64,67,71,74]
