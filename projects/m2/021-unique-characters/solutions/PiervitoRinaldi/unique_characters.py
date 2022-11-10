e9f5f562b8d4bee9ccb101b8e64765f76ace5a13
Piervito Rinaldi <piervitorinaldi23@gmail.com>
2022-11-10 22:19:44 +0100

    update: unique_characters

--- projects/m2/021-unique-characters/solutions/PiervitoRinaldi/unique_characters.py
+++ projects/m2/021-unique-characters/solutions/PiervitoRinaldi/unique_characters.py
@@ -1,6 +1,6 @@
 # Create a dictionary where the characters are the keys, and the values correspond how many times a character appears.
 # At the end, the function return the number of unique characters in a text entered by the user. 
-def unique_characters(text):    
+def count_characters(text):    
     count_of_characters = dict()
     for character in text:
         if character not in count_of_characters:
@@ -8,16 +8,22 @@ def unique_characters(text):
         else:
             count_of_characters[character] += 1
 
-    n_unique_character = f'Your text has {len(count_of_characters)} unique characters!'
+    return count_of_characters
 
-    return n_unique_character
+def unique_character(text):
+    n_unique_character = len(count_characters(text))
+    result = f'Your text has {n_unique_character} unique characters!'
+
+    return result
 
 def main():
     text = input('Enter text to display the number of unique characters: ')
-    print(unique_characters(text))
+    print(unique_character(text))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
+
 
 
     
