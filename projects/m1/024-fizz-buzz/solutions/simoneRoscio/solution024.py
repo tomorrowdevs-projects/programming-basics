5ec1a932b01a3999ac0f35dcc5cd563e56b3501b
shilux42 <141simone@gmail.com>
2022-11-10 09:05:12 +0100

    FIX solution024

--- projects/m1/024-fizz-buzz/solutions/simoneRoscio/solution024.py
+++ projects/m1/024-fizz-buzz/solutions/simoneRoscio/solution024.py
@@ -22,13 +22,11 @@ for i in range(1, 100 + 1):
     margin = 20 - len(str(i))
     next = i + 1
 
-    if (i % 3 == 0):
+    if (i % 5 == 0 and i % 3 == 0):
+        print(i, ' ' * margin, 'Fizz buzz!')
+    elif (i % 3 == 0):
         print(i, ' ' * margin, 'Fizz!')
-        if (i % 5 == 0):
-            print(i, ' ' * margin, 'Fizz buzz!')
-
     elif (i % 5 == 0):
-        print(i, ' ' * margin, 'Buzz!')
-        
+        print(i, ' ' * margin, 'Buzz!')   
     else:
         print(i, ' ' * margin, next)
