bb953a853c93b4d044e96cd4a54be0c92b4f2a65
Alessia-C <caporasoalessia1611@gmail.com>
2022-11-14 19:37:53 +0100

    feat(m1): add solution exe 031-coin-flip-simulation

---
+++ projects/m1/031-coin-flip-simulation/solutions/alessiacaporaso/coin-flip-simulation.js
@@ -0,0 +1,28 @@
+let flipSimulation;
+let flips;
+let averageFlips = 0;
+
+// program is run it should perform the simulation 10 times
+for (let i = 0; i <= 10; i++) {
+  let coin = [];
+  let j = 0;
+  let control = true;
+  //  simulated coins until either 3 consecutive heads of 3 consecutive tails occur
+  while (control) {
+    flipSimulation = Math.floor(Math.random() * 10 + 1);
+    if (flipSimulation % 2 === 0) {
+      coin.push("H");
+    } else {
+      coin.push("T");
+    }
+    if(coin.length >= 3 && coin[j] === coin[j - 1] && coin[j - 1] === coin[j - 2]){
+        control = false
+    }
+    j++;
+    flips = coin.length  
+}
+averageFlips += flips
+console.log( `${coin} (${flips} flips)` );
+}
+averageFlips = averageFlips / 10
+console.log(`On average, ${averageFlips} flips were needed`)
