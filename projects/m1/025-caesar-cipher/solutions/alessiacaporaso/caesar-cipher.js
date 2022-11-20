f83bff318b27239571aaa6f365319bedf34d205e
Alessia-C <caporasoalessia1611@gmail.com>
2022-11-20 21:20:26 +0100

    fix(m1): add solution exe 025-caesar-cipher

--- projects/m1/025-caesar-cipher/solutions/alessiacaporaso/caesar-cipher.js
+++ projects/m1/025-caesar-cipher/solutions/alessiacaporaso/caesar-cipher.js
@@ -6,31 +6,25 @@ const userMessage = prompt("Enter the message to encode / decode:").toUpperCase(
 const encodes = parseInt(prompt("Enter the message encodes"));
 // divide message in character
 const splitMessage = userMessage.split("");
-//console.log(splitMessage)
-// declared variable for loop
-let characterPosition;
-let punctuationPosition;
-let newPosition;
-let changePosition;
-let newMessage = "";
-// find the position character in the alphabet, support negative,positive character and punctuation marks
-for (let i = 0; i < splitMessage.length; i++) {
-  characterPosition = alphabet.indexOf(splitMessage[i]);
-  //console.log('character position: ' + characterPosition)
-  newPosition = characterPosition + encodes;
-  //console.log('new position: ' + newPosition)
-  if (characterPosition == -1) {
-    newMessage += splitMessage[i];
-  } else if (newPosition > 25) {
-    changePosition = alphabet[newPosition - alphabet.length];
-    newMessage += changePosition;
-  } else if (newPosition < 0) {
-    changePosition = alphabet[alphabet.length + newPosition];
-    newMessage += changePosition;
+let message = "";
+// find the position in the alphabet
+for (const character of splitMessage) {
+  const positionCharacter = alphabet.indexOf(character);
+  let calcNewPosition = positionCharacter + encodes;
+  if (positionCharacter === -1) {
+    message += character;
   } else {
-    changePosition = alphabet[newPosition];
-    newMessage += changePosition;
+    if (calcNewPosition > alphabet.length) {
+      while (calcNewPosition >= alphabet.length) {
+        calcNewPosition -= alphabet.length;
+      }
+    } else if (calcNewPosition < alphabet.length) {
+      while (calcNewPosition <= 0) {
+        calcNewPosition += alphabet.length;
+      }
+    }
+    message += alphabet[calcNewPosition];
+    // console.log(character, calcNewPosition, alphabet[calcNewPosition]);
   }
-  //console.log('change position '+ changePosition)
 }
-alert(newMessage);
+alert(message);
