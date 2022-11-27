7676a86614c2055b273a818d5818811ee4469773
Stefano Santomaggio <stefano.santomaggio02@gmail.com>
2022-11-27 17:35:52 +0100

    feat(m1): solution exe019

--- projects/m1/019-average/solutions/StefanoSantomaggio/019_average.py
+++ projects/m1/019-average/solutions/StefanoSantomaggio/019_average.py
@@ -1,6 +1,6 @@
 numbers=[]
 while 0 not in numbers:
-    number=int(input("inserisci un valore diverso da 0:"))
+    number=int(input("Inserisci un valore diverso da 0:"))
     if number==0 and len(numbers)==0:
         print ("error")
     elif number ==0:
