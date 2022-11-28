498676a1b39943d68e950a23e2ea8df0a16ee730
Stefano Santomaggio <stefano.santomaggio02@gmail.com>
2022-11-28 11:39:15 +0100

    feat(m1): corr exe19

--- projects/m1/019-average/solutions/StefanoSantomaggio/019_average.py
+++ projects/m1/019-average/solutions/StefanoSantomaggio/019_average.py
@@ -1,9 +1,11 @@
-numbers=[]
-while 0 not in numbers:
-    number=int(input("Inserisci un valore diverso da 0:"))
-    if number==0 and len(numbers)==0:
-        print ("error")
-    elif number ==0:
-       average= sum(numbers)/len(numbers)
-       print ("The average is:", average)
-    numbers.append(number)
+n= float(input("insert a number:"))
+som=0
+mid=0
+if n==0:
+    print("First number can't be 0")
+else:
+    while n!=0:
+        som=som+n
+        mid +=1
+        n=float(input("insert a number:"))
+    print ("Average is:", som/mid)
