2604a56602b7005eb9bf218132061474113a03a3
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2022-12-01 17:12:48 +0100

    exercise completed

---
+++ projects/m1/024-fizz-buzz/js/index.js
@@ -0,0 +1,19 @@
+
+let counter = [];
+
+for (let i = 1; i < 101; i ++) {
+    if ((i % 3 !== 0) && (i % 5 !== 0)) {
+        counter.push(i);
+
+    } else if ((i % 3 === 0) && (i % 5 !== 0)) {
+        counter.push("Fizz!");
+
+    } else if ((i % 3 !== 0) && (i % 5 === 0)) {
+        counter.push("Buzz!");
+
+    } else if ((i % 3 === 0) && (i % 5 === 0)) {
+        counter.push("Fizz-Buzz!");
+
+    }
+}
+alert(counter.join('\n'));
