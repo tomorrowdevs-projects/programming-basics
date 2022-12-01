59eaa1837f866f271bf4cf73a6ce73ec89b33438
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2022-12-01 15:44:46 +0100

    exercise completed

---
+++ projects/m1/001-area-of-a-room/js/index.js
@@ -0,0 +1,5 @@
+
+let  width = prompt("Please, insert the width in meters of your room:");
+let  length = prompt("Please, insert the length in meters of your room:");
+let area = width * length;
+alert("The area of your room is:" + " " + area +  " " + "square meters");
