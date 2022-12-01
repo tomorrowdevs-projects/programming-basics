a9d0d36aa4f3f64f75b66dd162859f3775686bc4
shilux42 <141simone@gmail.com>
2022-12-01 15:59:31 +0100

    solution to M1-010-month-name-to-number-of-days

---
+++ projects/m1/010-month-name-to-number-of-days/python/main.py
@@ -0,0 +1,14 @@
+user_month = str.casefold(input('Hello USER, write a month name: '))
+
+month31 = 'january', 'march', 'may', 'july', 'august', 'october', 'december'
+month30 = 'april', 'june', 'september', 'november'
+february = 'febraury'
+
+if user_month in month31:
+    print(f'{user_month.capitalize()} have 31 days')
+elif user_month in month30:
+    print(f'{user_month.capitalize()} have 30 days')
+elif user_month in february:
+    print(f'{user_month.capitalize()} have 28 or 29 days')
+else:
+    print(f'Are you sure that {user_month} is a month?')
