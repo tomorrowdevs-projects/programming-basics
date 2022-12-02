87c9b12f3a47decbe4dd80b068573f62b94b0b18
Giacomo Boria <g.boria91@gmail.com>
2022-12-02 10:12:30 +0100

    refactor(m2): add new log ic forEach line 18-20

--- projects/m2/012-shuffling-a-deck-of-cards/solutions/GiacomoBoria/script.js
+++ projects/m2/012-shuffling-a-deck-of-cards/solutions/GiacomoBoria/script.js
@@ -9,75 +9,33 @@ Nine of spades	Ns
 
 mix card*/
 
-// First version
-/*function createDeack() {
-  
+function createDeck() {
   const suitCard = ["s", "c", "d", "h"];
-  const abbreviationCard = [ "T", "J", "Q", "K", "A"];
+  const abbreviationCard = [ 2, 3, 4, 5, 6, 7, 8 , 9, "T", "J", "Q", "K", "A"];
 
-  const cardDeck = [];
-  let indexSuit = 0;
-  let indexAbbreviationCard = 0;
-  let i = 1;
+  const cardDeck = [] 
   
-  while (cardDeck.length < 52) {
+  suitCard.forEach(element => {
+    abbreviationCard.forEach(value => {
+      cardDeck.push(value + element);
+    })
+  })
+
+  return cardDeck;
+
+}
+
+function suffleDeck(deckCard) {
     
-    if (i === 1) {
+    for (i = deckCard.length - 1; i > 0; i--) {
       
-      cardDeck.push(abbreviationCard[4] + suitCard[indexSuit])
-      i += 1;
-      
-    } else if (i > 1 && i < 10) {
-      
-      cardDeck.push(i + suitCard[indexSuit]);
-      i += 1;
-    
-    }  else if (i >= 10 && i < 14) {
-      
-      cardDeck.push(abbreviationCard[indexAbbreviationCard] + suitCard[indexSuit])
-      indexAbbreviationCard += 1;
-      i += 1;
-    
-    } else if (i >= 12) {
-      
-      indexAbbreviationCard = 0;
-      i = 1;
-      indexSuit += 1;
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = deckCard[i];
+        deckCard[i] = deckCard[j];
+        deckCard[j] = temp;
       
     }
-    
-  }
-  return cardDeck;
-}*/
+  return deckCard;
+}
 
-// second and fast vrsion
-function createDeck() {
-    let suitCard = ["s", "c", "d", "h"];
-    let abbreviationCard = [ 2, 3, 4, 5, 6, 7, 8 , 9, "T", "J", "Q", "K", "A"];
-    const cardDeck = [];
-  
-    for (suitCard of suitCard) {
-      
-      for(let value of abbreviationCard) {
-        let card = value + suitCard;
-        cardDeck.push(card);
-        
-      }
-    } 
-    return cardDeck
-  }
-  
-  function suffleDeck(deckCard) {
-  
-      for (i = deckCard.length - 1; i > 0; i--) {
-        
-          let j = Math.floor(Math.random() * (i + 1));
-          let temp = deckCard[i];
-          deckCard[i] = deckCard[j];
-          deckCard[j] = temp;
-        
-      }
-    return deckCard;
-  }
-  
-  alert(`The new deck of poker cards is composed as follows:\n${createDeck().join(" ")}\n\nThe Deck after it has been shuffled:\n${suffleDeck(createDeck()).join(" ")}`);
+alert(`The new deck of poker cards is composed as follows:\n${createDeck().join(" ")}\n\nThe Deck after it has been shuffled:\n${suffleDeck(createDeck()).join(" ")}`);
