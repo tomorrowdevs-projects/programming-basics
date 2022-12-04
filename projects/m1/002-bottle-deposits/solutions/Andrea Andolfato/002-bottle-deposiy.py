256733d44f5266c5d9340c154521489886671d1d
Andrea <andrea.andolfato90@gmail.com>
2022-12-04 13:56:28 +0100

    M1 - 002

--- projects/m1/002-bottle-deposits/solutions/Andrea Andolfato/002-bottle-deposiy.py
+++ projects/m1/002-bottle-deposits/solutions/Andrea Andolfato/002-bottle-deposiy.py
@@ -1,6 +1,6 @@
 
-small_drink = int(input("Please insert the number of small container: "))
-big_drink = int(input("Please insert the number of big container: "))
+s_drink = int(input("Please insert the number of small container: "))
+b_drink = int(input("Please insert the number of big container: "))
 
 refund = (small_drink * 0.10) + (big_drink * 0.25)
 print(refund)
