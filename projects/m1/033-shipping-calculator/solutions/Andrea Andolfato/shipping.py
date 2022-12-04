6a2928494bdda9dd97808bd16c5bb78ecfdb100d
Andrea <andrea.andolfato90@gmail.com>
2022-12-04 16:56:57 +0100

    M1-033

--- projects/m1/033-shipping-calculator/solutions/Andrea Andolfato/shipping.py
+++ projects/m1/033-shipping-calculator/solutions/Andrea Andolfato/shipping.py
@@ -1,8 +1,8 @@
 
-def order(items):
+def orders(items):
     total_price = 10.99 + (2.99 * items)
     print(f'Your shipping cost consist in € {total_price}')
 
 
 ordini = int(input("Please insert total number of items purchased: "))
-order(ordini)
+orders(ordini)
