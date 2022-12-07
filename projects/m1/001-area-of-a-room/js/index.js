77d08f9cc8fc343e0baa9744ecaad6348faf2493
Gabriele Omar Lakhal <69049549+gabrielelakhal@users.noreply.github.com>
2022-12-07 18:44:41 +0100

    correz<zione finale esercizio

--- projects/m1/001-area-of-a-room/js/index.js
+++ projects/m1/001-area-of-a-room/js/index.js
@@ -2,4 +2,4 @@ x = prompt("Qual è la alrghezza della stanza?");
 y = prompt("Qual è la profondità della stanza? ");
 
 z = x * y;
-"La tua stanza è" + z + "mq";
+alert("La tua stanza è " + z + "mq");
