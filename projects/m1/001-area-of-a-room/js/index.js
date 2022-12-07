ac61a9f81ebc5df01fcfb5619dec65cdf6498ddd
Gabriele Omar Lakhal <69049549+gabrielelakhal@users.noreply.github.com>
2022-12-07 18:03:22 +0100

    Prima prova esercizio 001 area-of-a-room

---
+++ projects/m1/001-area-of-a-room/js/index.js
@@ -0,0 +1,5 @@
+var larghezza = prompt("Qual è la alrghezza della stanza?");
+var profondita = prompt("Qual è la profondità della stanza? ");
+
+z = larghezza + profondita;
+"La tua stanza è" + z + "mq";
