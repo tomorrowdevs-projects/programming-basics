4677755d6b5bbf28b1700d971edd0c7f00d0765d
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2022-12-08 19:24:44 +0100

    exercise completed

---
+++ projects/m3/002-recursive-decimal-to-binary/js/index.js
@@ -0,0 +1,26 @@
+
+let number = Number(prompt("Please, enter a positive integer to convert it into binary form:"));
+
+
+
+    function getBinary (number, string = "") {
+
+
+//get string or error based on number value; add the remainder of number to string if number > 0.
+        if (number === 0) {
+            return string;
+        } else if (Number.isInteger(number) === false || number < 0) {
+            return "Please, enter a valid input!"
+        } else {
+            string = (number % 2).toString() + string;
+            return getBinary(Math.floor(number / 2), string);
+        }
+
+    }
+
+
+
+alert(getBinary(number));
+
+
+
