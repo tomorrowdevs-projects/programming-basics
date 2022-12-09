c65563ab60e6af5976d30053deb094d4c0aab415
Gabriele Omar Lakhal <69049549+gabrielelakhal@users.noreply.github.com>
2022-12-09 14:44:08 +0100

    Esercizio 3 completato

--- projects/m1/002-bottle-deposits/js/index.js
+++
@@ -1,8 +0,0 @@
-let bottigliePiccole = prompt("Quante bottiglie da ≤1 litro hai?");
-let bottiglieGrandi = prompt("Quante bottiglie da >1 litro hai?");
-
-convBottPicc = bottigliePiccole * 0.10;
-convBottGran = bottiglieGrandi * 0.25;
-totRImborso = convBottPicc + convBottGran;
-
-alert("Rimborso totale di " + totRImborso + "$");
