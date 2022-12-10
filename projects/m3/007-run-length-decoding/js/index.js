a82092a7364f51934694b997fc8fc021ed503437
Mattia Cherchi <mattiacherchi93@gmail.com>
2022-12-10 14:48:10 +0100

    feat(m3): add exercise 007-run-length-decoding

---
+++ projects/m3/007-run-length-decoding/js/index.js
@@ -0,0 +1,18 @@
+const list = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "A", "A", "A", "A", "A", "A", "B"];
+
+const runLengthDec = (arr) =>{
+
+   const newArr = [];
+   if(arr.length === 0) return arr;
+   
+   let counter = 1;
+   for(let element of arr){
+      if(element === arr[counter]){
+         counter++;
+      }else{
+         return newArr.concat(element , counter, runLengthDec(arr.splice(counter)));
+      }
+   }  
+}
+
+console.log(runLengthDec(list));
