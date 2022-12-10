b9ef7bda2bb9443b2416917c35885ceaa6e3f517
shilux42 <141simone@gmail.com>
2022-12-10 16:42:55 +0100

    FIX added a function to print each value in a new line

--- projects/m2/001-sorted-order/python/main.py
+++ projects/m2/001-sorted-order/python/main.py
@@ -2,15 +2,21 @@ unsorted_int = []
 
 def sort_input(unsorted_int):
     sorted_int = sorted(unsorted_int)
-    print(sorted_int)
+    return sorted_int
+
+def print_list(sorted_int):
+    for elem in sorted_int:
+        print(elem)
 
 def collect_input():
     user_int = int(input('Enter and integer (0 to end and sort): '))
+
     if user_int != 0:
         unsorted_int.append(user_int)
         collect_input()
+
     if user_int == 0:
-        sort_input(unsorted_int)
+        print_list(sort_input(unsorted_int))
         
 
 if __name__ == '__main__':
