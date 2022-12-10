7939e4e82f126d172d2485c7c412e4fe4f5cad25
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2022-12-10 21:14:20 +0100

    feat(m2): refactored solution 025 checking for a winning card

--- projects/m2/024-create-a-bingo-card/js/index.js
+++ projects/m2/024-create-a-bingo-card/js/index.js
@@ -1,19 +1,17 @@
-
-//i use the constructor function "BingoCard" to randomly generate numbers for the various Bingo keys.
-    function BingoCard () {
-        this.B = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
-        this.I = Math.floor(Math.random() * (30 - 16 + 1)) + 16;
-        this.N = Math.floor(Math.random() * (45 - 31 + 1)) + 31;
-        this.G = Math.floor(Math.random() * (60 - 46 + 1)) + 46;
-        this.O = Math.floor(Math.random() * (75 - 61 + 1)) + 61;
+function BingoCard () {
+    this.B = Array.from({length: 5}, () => Math.floor(Math.random() * (15 - 1 + 1)) + 1);
+    this.I = Array.from({length: 5}, () => Math.floor(Math.random() * (30 - 16 + 1)) + 16);
+    this.N = Array.from({length: 5}, () => Math.floor(Math.random() * (45 - 31 + 1)) + 31);
+    this.G = Array.from({length: 5}, () => Math.floor(Math.random() * (60 - 46 + 1)) + 46);
+    this.O = Array.from({length: 5}, () => Math.floor(Math.random() * (75 - 61 + 1)) + 61);
     }
 
-let card = new BingoCard();
+    const card = new BingoCard();
 
-//then i create a string nicely formatted iterating over every keys of the object "card".
+
     function showCard (card) {
         
-        let outputString = "";
+        const outputString = "";
 
         for (i = 0; i < Object.keys(card).length; i++) {
             outputString += Object.keys(card)[i] + " => " + Object.values(card)[i] + "\n"; 
