d8e736002b5c23e8422ac5fcdcf64a4028a96e93
Gabriele.Gabellone <gabellonegabriele@gmail.com>
2022-12-16 19:38:39 +0100

    improved code readability

--- projects/m2/012-shuffling-a-deck-of-cards/solutions/GabrieleGabellone/shuffling_a_deck_of_cards.py
+++ projects/m2/012-shuffling-a-deck-of-cards/solutions/GabrieleGabellone/shuffling_a_deck_of_cards.py
@@ -1,4 +1,5 @@
 from random import *
+
 def create_deck():
     deck = []
     value_character = "A23456789TJQK"
@@ -7,7 +8,7 @@ def create_deck():
         for value in value_character:
             card = value + suit
             deck.append(card)
-    return(deck)
+    return deck
 
 def shuffle_deck(unshuffled_deck):
     shuffled_deck = unshuffled_deck [:]
@@ -15,11 +16,11 @@ def shuffle_deck(unshuffled_deck):
         random_index = randint(0,51)
         shuffled_deck[shuffled_deck.index(card)] = shuffled_deck[random_index]
         shuffled_deck[random_index] = unshuffled_deck[unshuffled_deck.index(card)]
-    return(shuffled_deck)
+    return shuffled_deck
 
 def main():
     deck_of_cards = create_deck()
-    print ("Unshuffled deck:", deck_of_cards,"\n")
+    print("Unshuffled deck:", deck_of_cards,"\n")
     shuffled_deck = shuffle_deck(deck_of_cards)
     print("Deck after it has been shuffled:", shuffled_deck)
 
