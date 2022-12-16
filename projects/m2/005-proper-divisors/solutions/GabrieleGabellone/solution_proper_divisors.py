5996185cfd883f1d967789fb6a079d329b57e3e4
Gabriele.Gabellone <gabellonegabriele@gmail.com>
2022-12-16 18:23:02 +0100

    remove_duplicates function removed and used a set

--- projects/m2/005-proper-divisors/solutions/GabrieleGabellone/solution_proper_divisors.py
+++ projects/m2/005-proper-divisors/solutions/GabrieleGabellone/solution_proper_divisors.py
@@ -1,35 +1,27 @@
-def remove_duplicates(original_list):
-    """Takes a list of values, returns a new list without duplicates."""
-    new_list = []
-    for values in original_list:
-        if values not in new_list:
-            new_list.append(values)
-    return (new_list)
-
 def proper_divisors(integer):
     """Takes an integer, returns the list of its divisors."""
     if integer.isnumeric() == False:
-        print ("You must enter a positive integer!")
-        return (False)
-    else: integer = int(integer)
-    temp_list = []
+        print("You must enter a positive integer!")
+        return False
+    integer = int(integer)
+    temp_set = set()
     divisor = 1
-    while divisor not in temp_list:
+    while divisor not in temp_set:
         if integer % divisor == 0:
             quotient = integer // divisor
-            temp_list.append(quotient)
-            temp_list.append(divisor)
+            temp_set.add(quotient)
+            temp_set.add(divisor)
         divisor += 1
-    temp_list.sort()
-    list_of_divisors = remove_duplicates(temp_list)
+    list_of_divisors = list(temp_set)
+    list_of_divisors.sort()
     del list_of_divisors[-1]
-    return (list_of_divisors)
+    return list_of_divisors
 
 def main():
     n = input("Enter a number: ")
-    if proper_divisors(n) != False:
+    if proper_divisors(n):
         divisors = proper_divisors(n)
-        print ("Divisors of {}: {}".format(n, divisors))
+        print("Divisors of {}: {}".format(n, divisors))
 
 if __name__ == "__main__":
     main() 
