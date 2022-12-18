3d935f49e668e36ad9a52cd8474aff43c32b9ab1
Gabriele.Gabellone <gabellonegabriele@gmail.com>
2022-12-18 11:43:15 +0100

    optimized the speed of the list_of_primes function

--- projects/m2/015-the-sieve-of-eratosthenes/solutions/GabrieleGabellone/the_sieve_of_eratosthenes.py
+++ projects/m2/015-the-sieve-of-eratosthenes/solutions/GabrieleGabellone/the_sieve_of_eratosthenes.py
@@ -1,22 +1,22 @@
 def list_of_primes(limit):
     """Takes a limit number, returns a list of prime numbers between 2 and this limit number."""
-    all_numbers = list(range(2,limit + 1))
+    all_numbers = list(range(2,limit+1))
     prime = []
     p = 2
     while p < limit:
-        if p in all_numbers:
-            for number in range(p,limit+1,p):
-                if number != p and number in all_numbers:
-                    all_numbers[all_numbers.index(number)] = 0
+        if all_numbers[p-2] != 0:
+            for number in range(p*p, limit+1, p):
+                all_numbers[number-2] = 0
         p += 1
     for value in all_numbers:
-        if value != 0: prime.append(value)
-    return(prime)
+        if value != 0: 
+            prime.append(value)
+    return prime
 
 def main():
     limit_number = int(input("Enter a limit number: "))
     prime_numbers = list_of_primes(limit_number)
-    print ("Prime numbers between 2 and {}:\n{}".format(limit_number, prime_numbers))
+    print("Prime numbers between 2 and {}:\n{}".format(limit_number, prime_numbers))
 
 if __name__ == "__main__":
     main()
