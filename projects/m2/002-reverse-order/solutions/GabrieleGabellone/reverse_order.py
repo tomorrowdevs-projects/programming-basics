6892be301ed466144d26ad55362abc5fda891a03
Gabriele.Gabellone <gabellonegabriele@gmail.com>
2022-12-21 10:57:49 +0100

    improved code readability

--- projects/m2/002-reverse-order/solutions/GabrieleGabellone/reverse_order.py
+++ projects/m2/002-reverse-order/solutions/GabrieleGabellone/reverse_order.py
@@ -2,10 +2,11 @@
 entered_numbers = []
 number = input("Enter a number (0 to finish): ")
 while number != "0":
-    if number.isnumeric() == True:
+    if number.isnumeric():
         if int(number) != 0: # I added this condition to avoid adding to the list strings whose integer value would correspond to 0, for example strings like "00" or "000".
             entered_numbers.append(int(number))
-    else: print ("You can only enter integers!")
+    else: 
+        print("You can only enter integers!")
     number = input("Enter a number (0 to finish): ")
 entered_numbers.sort()
 entered_numbers.reverse()
