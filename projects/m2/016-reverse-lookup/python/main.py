80fc55fc9f2ee3a79db92e6b19da950685f395bc
Gabriele.Gabellone <gabellonegabriele@gmail.com>
2022-12-21 19:22:35 +0100

    improved code readability and fixed solution

--- projects/m2/016-reverse-lookup/python/main.py
+++ projects/m2/016-reverse-lookup/python/main.py
@@ -1,26 +1,16 @@
-def reverse_lookup(d, v):
+def reverse_lookup(dictionary, value):
     """Returns a list containing the keys found in the dictionary having the value given as parameter."""
     found_keys = []
-    for k in d:
-        if d[k] == v:
-            found_keys.append(k)
+    for key in dictionary:
+        if dictionary[key] == value:
+            found_keys.append(key)
     return found_keys
 
-def create_dict(s):
-    """Returns a dictionary 
-    having as key-value pair the characters of the string and how many times they appear in the string."""
-    d = dict()
-    for character in s:
-        if character not in d:
-            d[character] = 1
-        else: d[character] += 1
-    return d
-
 def main():
-    string = input("Enter a string to convert to dict: ")
-    value = int(input("Enter a value to look up in the dict: "))
-    user_dict = create_dict(string)
-    print("Keys having the value {}: {}".format(value, reverse_lookup(user_dict, value)))
+    example_dict = {"a": [1], "b": 1, "c": 2, "d": [2, 3], "e": [3], "f": "two", "g": 3, "i": [3]}
+    values_to_search = [1, "one", 2, [2], [3], [2,3]]
+    for v in values_to_search:
+        print("Keys having the value {}: {}".format(v, reverse_lookup(example_dict, v)))
 
 if __name__ == "__main__":
     main()
