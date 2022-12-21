32d39af696ad7c973fd6b87df9e345c8bd6e72b7
Gabriele.Gabellone <gabellonegabriele@gmail.com>
2022-12-21 16:15:49 +0100

    improved code readability and fixes in for loop

--- projects/m2/007-only-the-words/solutions/GabrieleGabellone/only_the_words.py
+++ projects/m2/007-only-the-words/solutions/GabrieleGabellone/only_the_words.py
@@ -1,21 +1,20 @@
 def only_words(string):
     """Takes a string, returns a list with the extracted words without punctuation marks."""
-    puntation_marks = [",", ".", "?", "-", "’", "!", ":", ";"]
+    punctuation_marks = [",", ".", "?", "-", "’", "!", ":", ";"]
     temp_string = ""
     for character in string:
-        if character in puntation_marks:
+        if character in punctuation_marks:
             if string.index(character) == len(string) - 1:
                 continue
             if string[(string.index(character) + 1)] == " ":
                 continue
-            else: temp_string += character
-        else: temp_string += character
+            # punctuation marks in the middle of a word are now also removed, except for apostrophes used to form a contraction
+            if not character == "’":
+                continue 
+        temp_string += character
     extracted_words = temp_string.split()
-    return (extracted_words)
-
-def main():
-    user_string = input("Enter a phrase: ")
-    print (only_words(user_string))
+    return extracted_words
 
 if __name__ == "__main__":
-    main()
+    user_string = input("Enter a phrase: ")
+    print(only_words(user_string))
