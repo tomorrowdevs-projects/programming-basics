b3aa56b9f291cb5867a962140e41989f7368804f
Gabriele.Gabellone <gabellonegabriele@gmail.com>
2022-12-21 12:32:41 +0100

    improved code readability and fixed the while loop

--- projects/m2/004-avoiding-duplicates/solutions/GabrieleGabellone/avoiding_duplicates.py
+++ projects/m2/004-avoiding-duplicates/solutions/GabrieleGabellone/avoiding_duplicates.py
@@ -1,15 +1,15 @@
 def remove_duplicates(original_list):
-    """Takes a list of strings, returns a new list without duplicates."""
+    """Takes a list, returns a new list without duplicates."""
     new_list = []
     for string in original_list:
         if string not in new_list:
             new_list.append(string)
-    return (new_list)
+    return new_list
 
 def main():
     entered_words = []
-    user_word = input("Enter a word (blank to quit): ")
-    while user_word != " ":
+    user_word = input("Enter a word (blank to quit): ").strip()
+    while user_word != "":
         entered_words.append(user_word)
         user_word = input("Enter a word (blank to quit): ")
     no_duplicates = remove_duplicates(entered_words)
