6e7b3269e8f9bfafba2e6d8de050a61aeceb82ec
Gabriele.Gabellone <gabellonegabriele@gmail.com>
2022-12-21 19:48:55 +0100

    improved code readability

--- projects/m2/018-text-messaging/python/main.py
+++ projects/m2/018-text-messaging/python/main.py
@@ -1,9 +1,8 @@
 def check_characters(entered_message):
     allowed_characters = ".,?!:ABCDEFGHIJKLMNOPQRSTUVWXYZ "
     for character in entered_message:
-        if character.upper() not in allowed_characters:
-            print("The character '{}' is not present in the numeric keypad!".format(character))   
-            return False
+        if character not in allowed_characters:
+            return print("The character '{}' is not present in the numeric keypad!".format(character)) 
     return True
 
 def text_messaging(entered_message):
@@ -11,15 +10,15 @@ def text_messaging(entered_message):
     new_message = ""
     for character in entered_message:
         for number in numeric_keypad:
-            if character.upper() in numeric_keypad[number]: 
-                new_character = number * (numeric_keypad[number].index(character.upper()) + 1)
+            if character in numeric_keypad[number]: 
+                new_character = number * (numeric_keypad[number].index(character) + 1)
                 new_message += new_character
     return new_message
 
 def main():
-    user_message = input("Enter a message: ")
-    if check_characters(user_message) == True:
-        print (text_messaging(user_message))
+    user_message = input("Enter a message: ").upper()
+    if check_characters(user_message):
+        print(text_messaging(user_message))
 
 if __name__ == "__main__":
     main()
