c62ff8c3a808889b5b466be9e298e3b81e55132b
Gabriele.Gabellone <gabellonegabriele@gmail.com>
2022-12-21 14:40:02 +0100

    fixed proper_divisors function

--- projects/m2/005-proper-divisors/solutions/GabrieleGabellone/solution_proper_divisors.py
+++ projects/m2/005-proper-divisors/solutions/GabrieleGabellone/solution_proper_divisors.py
@@ -1,27 +1,30 @@
 def proper_divisors(integer):
     """Takes an integer, returns the list of its divisors."""
-    if integer.isnumeric() == False:
-        print("You must enter a positive integer!")
-        return False
-    integer = int(integer)
+    if not type(integer) == int:
+        if integer.isnumeric():
+            integer = int(integer)
+        else:
+            return print("You must enter a positive integer!")
+    if integer == 0:
+        return print("0 has infinitely many divisors")
+
     temp_set = set()
     divisor = 1
-    while divisor not in temp_set:
+
+    while divisor not in temp_set and divisor <= integer:
         if integer % divisor == 0:
             quotient = integer // divisor
             temp_set.add(quotient)
             temp_set.add(divisor)
         divisor += 1
+
     list_of_divisors = list(temp_set)
     list_of_divisors.sort()
-    del list_of_divisors[-1]
     return list_of_divisors
 
-def main():
-    n = input("Enter a number: ")
-    if proper_divisors(n):
-        divisors = proper_divisors(n)
-        print("Divisors of {}: {}".format(n, divisors))
 
 if __name__ == "__main__":
-    main() 
+    n = input("Enter a number: ")
+    divisors = proper_divisors(n)
+    if divisors:
+        print("Divisors of {}: {}".format(n, divisors))
