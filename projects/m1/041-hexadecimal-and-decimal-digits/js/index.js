04b69de40be593d4c996b2808c24485d6e452a4c
Alessia-C <caporasoalessia1611@gmail.com>
2022-12-27 22:06:43 +0100

    feat(m1): add solution exe 041-hexadecimal-and-decimal-digits

---
+++ projects/m1/041-hexadecimal-and-decimal-digits/js/index.js
@@ -0,0 +1,17 @@
+// convert a string containing a single hexadecimal digit to a base 10 integer
+function hex2int(string) {
+  const convertSting = "0x" + string;
+  alert("0x" + string + ": " + parseInt(convertSting));
+}
+// convert integer to a single hexadecimal digit
+function int2hex(number) {
+  if (number >= 0 && number <= 15) {
+    const convertInteger = number.toString(16);
+    alert(number + ": " + convertInteger);
+  } else {
+    // Display an error message if the parameter’s value is outside of the range.
+    alert("Invalid value");
+  }
+}
+hex2int(prompt("Enter letter from A until F"));
+int2hex(parseInt(prompt("Enter number between 0 and 15")));
