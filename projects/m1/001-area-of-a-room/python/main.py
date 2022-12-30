4bfd023337c1442a295cf59330daa3a2cce12907
Raffina92 <raffaellas92@gmail.com>
2022-12-30 16:46:23 +0100

    Published first solution of the exercise

---
+++ projects/m1/001-area-of-a-room/python/main.py
@@ -0,0 +1,5 @@
+unit = input("Please insert units: meters/feet ")
+width = float(input("Insert the width of the room "))
+lenght = float(input("Insert the lenght of the room "))
+
+print ("The area of your room is ",width * lenght,"",unit)
