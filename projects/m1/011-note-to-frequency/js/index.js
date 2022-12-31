87bb160af7162ae6a45a6651fd46a089d12994ec
Gabriele Omar Lakhal <69049549+gabrielelakhal@users.noreply.github.com>
2022-12-31 11:19:26 +0100

    ex M1-e11 done alone

--- projects/m1/011-note-to-frequency/js/index.js
+++ projects/m1/011-note-to-frequency/js/index.js
@@ -1,56 +1,52 @@
-//ask note to user
-let userInput = prompt("Enter a Note and discover its Frequency."); // C3
-userInput = userInput.toUpperCase();
+//index of notes
+const hzA4 = 440.00;
+const hzB4 = 493.88;
+const hzC4 = 261.63;
+const hzD4 = 293.66;
+const hzE4 = 329.63;
+const hzF4 = 349.23;
+const hzG4 = 392.00;
 
-//separate letter and number
-const userLetter = userInput.slice(0, 1); // "C"
-let userNumber = parseInt(userInput.slice(1, 2)); // "3"
+//asking a note to user to convert in uppercase, split the letter and the number to be processed
+let userNote = prompt("Enter a Note to convert it in its Frequency.");
+//convert user note in uppercase to exclude case sensitiveness errors
+userNote = userNote.toUpperCase();
+//separate letter and number and convert number in Int
+let noteLetter = userNote.slice(0, 1);
+let noteNumber = parseInt(userNote.slice(1, 2));
 
-//verify value is a note and if i isn't asking again an input to user
-while(isNaN(userNumber)){
-  userInput = prompt("Value not accepted. Enter a Note and discover its Frequency.")
-  userNumber = parseInt(userInput.slice(1, 2));
+//check if the value is a note, if not ask the user to insert a note again
+while(!isNaN(noteLetter) || isNaN(noteNumber)){
+  userNote = prompt("The value entered is not suitable, enter a note and find out its frequency, e.g. 'A5'.");
+  noteLetter = userNote.slice(0, 1);
+  noteNumber = parseInt(userNote.slice(1, 2));
 }
 
-//frequencies base of notes
-const noteA4 = 440.00;
-const noteB4 = 493.88;
-const noteC4 = 261.63;
-const noteD4 = 293.66;
-const noteE4 = 329.63;
-const noteF4 = 349.23;
-const noteG4 = 392.00;
-
-//function expression to calculate the frequency
-function noteToFrequency(frequencyPos4, valueUserNumber){
-  let frequencyAskedByUser = frequencyPos4 / Math.pow(2, 4-valueUserNumber);
-  alert(userInput + " frequency (Hz) is " + frequencyAskedByUser.toFixed(2));
+//function to convert note in frequency and display the result
+function convertNoteToHz(hzInNoteInInPos4, noteNumberValue){
+  //define var and calculate the note's frequency
+  let noteFreuency = hzInNoteInInPos4 / Math.pow(2, 4-noteNumberValue);
+  //fix frequency decimal, add a dot every 3 digits and a replace the dot for decimal with a comma
+  noteFreuency = noteFreuency.toFixed(2).replace('.',',');
+  //alert to display the result
+  alert("The frequency of " + userNote + " is " + noteFreuency + "Hz");
 }
 
-//display result
-switch(userLetter) {
-    case 'A':
-        noteToFrequency(noteA4, userNumber);
-        break
-    case 'B':
-        noteToFrequency(noteB4, userNumber);
-        break
-    case 'C':
-        noteToFrequency(noteC4, userNumber);
-        break
-    case 'D':
-        noteToFrequency(noteD4, userNumber);
-        break
-    case 'E':
-        noteToFrequency(noteE4, userNumber);
-        break
-    case 'F':
-        noteToFrequency(noteF4, userNumber);
-        break
-    case 'G':
-        noteToFrequency(noteG4, userNumber);
-        break
-    default:
-        alert("Value not accepted. Enter a Note and discover its Frequency.")
-        break
+//display the frequency for all cases and ask to reload the page if the note digited by user is out of index
+if(noteLetter == "A"){
+  convertNoteToHz(hzA4, noteNumber);
+}else if(noteLetter == "B"){
+  convertNoteToHz(hzB4, noteNumber);
+}else if(noteLetter == "C"){
+  convertNoteToHz(hzC4, noteNumber);
+}else if(noteLetter == "D"){
+  convertNoteToHz(hzD4, noteNumber);
+}else if(noteLetter == "E"){
+  convertNoteToHz(hzE4, noteNumber);
+}else if(noteLetter == "F"){
+  convertNoteToHz(hzF4, noteNumber);
+}else if(noteLetter == "G"){
+  convertNoteToHz(hzG4, noteNumber);
+}else{
+  alert("The value entered is not suitable. Please reload the page and enter a note and find out its frequency, e.g. 'A5'.")
 }
