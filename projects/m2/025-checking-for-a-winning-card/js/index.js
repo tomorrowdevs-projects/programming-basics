86938f687fcaa55ec8052d8c23125a6c5de304a5
Your NameSalvatore <salvatorequagliariello0@gmail.com>
2023-01-03 15:10:48 +0100

    fix(m2): refactored solution 025-checking-for-a-winning-card and added import function.

--- projects/m2/025-checking-for-a-winning-card/js/index.js
+++ projects/m2/025-checking-for-a-winning-card/js/index.js
@@ -1,71 +1,123 @@
 
-// i create my bingo card using a constructor with an array.from method using a math.random to generate every number;
-function BingoCard () {
-    this.B = Array.from({length: 5}, () => Math.floor((Math.random() * (15 - 1 + 1)) + 1));
-    this.I = Array.from({length: 5}, () => Math.floor(Math.random() * (30 - 16 + 1)) + 16);
-    this.N = Array.from({length: 5}, () => Math.floor(Math.random() * (45 - 31 + 1)) + 31);
-    this.G = Array.from({length: 5}, () => Math.floor(Math.random() * (60 - 46 + 1)) + 46);
-    this.O = Array.from({length: 5}, () => Math.floor(Math.random() * (75 - 61 + 1)) + 61);
-}
-
-//i declare three different cards;
-let cardOne = new BingoCard();
-let cardTwo = new BingoCard();
-let cardThree = new BingoCard();
+// Imported the constructor function `BingoCard` from the prev exercise.
+const Card = require("./exe-024-create-a-bingo-card");
 
 
-//the i declare three different arrays to test the "win" function. the first one (winHorizontal) is to test an horizontal line. the second one (winVertical) is to test a vertical line, the third one (winDiagonal) to test an horizontal line. while the last one is to test a random pick of numebrs.
-let winHorizontal = [Object.values(cardOne)[0][0], Object.values(cardOne)[1][0], Object.values(cardOne)[2][0], Object.values(cardOne)[3][0], Object.values(cardOne)[4][0]];
-
-let winVertical = Object.values(cardTwo)[0];
-
-let winDiagonal = [Object.values(cardThree)[0][4], Object.values(cardThree)[1][3], Object.values(cardThree)[2][2], Object.values(cardThree)[3][1], Object.values(cardThree)[4][0]];
-
-let winRandom = Array.from({length: 5}, () => Math.floor(Math.random() * (75 - 1 + 1)) + 1);
+const card1 = new Card;
+const card2 = new Card;
+const card3 = new Card;
+const card4 = new Card;
 
 
+// Created 3 function, one for every possibile winning line in Bingo, to show that the software works.
+function winHorizontal (card) {
 
-//then i declare my  main function "getLine" with two parameters. a bingo card and a set of array containing some numbers picks from the 4 i've declared.
-    function getLine(card, winLine) {
-        
+    const extractedNumbers = [Object.values(card)[0][0], Object.values(card)[1][0], Object.values(card)[2][0], Object.values(card)[3][0], Object.values(card)[4][0]];
 
-//to check if a number is present in one of the five keys (B,I,N,G,O) of the card we iterate the elements of the set array (winLine) over every value present in the card. if there's a match the value will be set to "0", to mark it as "called".
-        for (i = 0; i < Object.keys(card).length; i ++ ) {
+    for (number of extractedNumbers) {
 
-                for(j = 0; j < Object.values(card).length; j++){
+        for (key in card) {
 
-                    for (k = 0; k < winLine.length; k++) {
+            if (card[key].includes(number)) {
+                let sortedNumberIndex = card[key].indexOf(number);
+                card[key][sortedNumberIndex] = 0;
+            };
 
-                        if(winLine[k] === (Object.values(card)[i][j])) {
-                        Object.values(card)[i][j] = 0;
-
-                        }
-                    }
-                }
-            }
+        };
+    };
+};
 
 
-//once we have our card marked down i delcare two strings (keyString and outputString) that will be filled with the keys and the values of the modified card to create the "table" that represent  the bingo card. 
-        let keysString = "";
-        let outputString = "";
+function winVertical (card) {
 
-        for (i = 0; i < Object.keys(card).length; i++){
-            keysString +=  "     " + Object.keys(card)[i].toString() + "     ";
-        }
+    const extractedNumbers = [Object.values(card)[0][0], Object.values(card)[0][1], Object.values(card)[0][2], Object.values(card)[0][3], Object.values(card)[0][4]];
 
-        for (i = 0; i < Object.keys(card).length; i++) {
+    for (number of extractedNumbers) {
 
-            for(j = 0; j < 5; j++){
-            outputString += "     " + Object.values(card)[j][i] + "     "; 
-            }
-            outputString += "\n"
-        }
+        for (key in card) {
+
+            if (card[key].includes(number)) {
+                let sortedNumberIndex = card[key].indexOf(number);
+                card[key][sortedNumberIndex] = 0;
+            };
+
+        };
+    };
+};
+
+
+function winDiagonal (card) {
+
+    const extractedNumbers = [Object.values(card)[0][0], Object.values(card)[1][1], Object.values(card)[2][2], Object.values(card)[3][3], Object.values(card)[4][4]];
+
+    for (number of extractedNumbers) {
+
+        for (key in card) {
+
+            if (card[key].includes(number)) {
+                let sortedNumberIndex = card[key].indexOf(number);
+                card[key][sortedNumberIndex] = 0;
+            };
+
+        };
+    };
+};
 
 
 
-        return keysString + "\n" + outputString;
+// Declared the `showGame` function to show the card, using as first parameter a card and as second one of the three functions to check if the card has a winning line.
+function showGame (card) {
+    
+
+    let outputString = "";
+
+    for (i = 0; i < Object.keys(card).length; i++) {
+        outputString += Object.keys(card)[i] + " => " + Object.values(card)[i] + "\n"; 
     }
+    
+    console.log(outputString);
+    
+};
 
 
-alert(getLine(cardOne, winRandom));
 
+// Declared the core function of the software, that iterates over a random array of 75 numbers, using that as a number extractors and check if a given card has the extracted numbers.
+function getGame (card) {
+    
+    const sortedNumbers = Array.from({length: 75}, () => Math.floor(Math.random() * 75));
+
+    const winHorizontal1 = Object.values(card)[0][0] + Object.values(card)[1][0] + Object.values(card)[2][0] + Object.values(card)[3][3] + Object.values(card)[4][0];
+    const winHorizontal2 = Object.values(card)[0][1] + Object.values(card)[1][1] + Object.values(card)[2][1] + Object.values(card)[3][1] + Object.values(card)[4][1];
+    const winHorizontal3 = Object.values(card)[0][2] + Object.values(card)[1][2] + Object.values(card)[2][2] + Object.values(card)[3][2] + Object.values(card)[4][2];
+    const winHorizontal4 = Object.values(card)[0][3] + Object.values(card)[1][3] + Object.values(card)[2][3] + Object.values(card)[3][3] + Object.values(card)[4][3];
+    const winHorizontal5 = Object.values(card)[0][4] + Object.values(card)[1][4] + Object.values(card)[2][4] + Object.values(card)[3][4] + Object.values(card)[4][4];
+
+    const winVertical1 = Object.values(card)[0].reduce((partialSum, a) => partialSum + a, 0);
+    const winVertical2 = Object.values(card)[1].reduce((partialSum, a) => partialSum + a, 0);
+    const winVertical3 = Object.values(card)[2].reduce((partialSum, a) => partialSum + a, 0);
+    const winVertical4 = Object.values(card)[3].reduce((partialSum, a) => partialSum + a, 0);
+    const winVertical5 = Object.values(card)[4].reduce((partialSum, a) => partialSum + a, 0);
+
+    const diagonal1 = Object.values(card)[0][0] + Object.values(card)[1][1] + Object.values(card)[2][2] + Object.values(card)[3][3] + Object.values(card)[4][4];
+    const diagonal2 = Object.values(card)[0][4] + Object.values(card)[1][3] + Object.values(card)[2][2] + Object.values(card)[3][1] + Object.values(card)[4][0];
+
+        for (number of sortedNumbers) {
+
+                for (key in card) {
+
+                    if (card[key].includes(number)) {
+                        let sortedNumberIndex = card[key].indexOf(number);
+                        card[key][sortedNumberIndex] = 0;
+                    };
+
+                };
+                
+            if (winHorizontal1 == 0 || winHorizontal2 == 0 || winHorizontal3 == 0 || winHorizontal4 == 0 || winHorizontal5 == 0 || winVertical1 == 0 || winVertical2 == 0 || winVertical3 == 0 || winVertical4 == 0 || winVertical5 == 0 || diagonal1 == 0 || diagonal2 == 0) { break; };
+        };
+
+};
+
+showGame(card3, winDiagonal(card3));
+showGame(card2, winVertical(card2));
+showGame(card1, winHorizontal(card1));
+showGame(card4, getGame(card4));
