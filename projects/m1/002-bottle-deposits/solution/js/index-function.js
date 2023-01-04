3713b31fa370e80e65dd583ef889f992310713fc
Alessandro <github25@alessandro-pifferi.com>
2023-01-05 00:28:41 +0100

    update (m1): Solution/002-bottle-deposits

--- projects/m1/002-bottle-deposits/solution/js/index-function.js
+++ projects/m1/002-bottle-deposits/solution/js/index-function.js
@@ -1,10 +1,14 @@
 function refoundConteiner() {
-  const deposit_SmallConteiner = 0.10;
-  const deposit_BigConteiner = 0.25;
-  var SmallConteiner = document.getElementById("id-SmallConteiner").value;
-  var BigConteiner = document.getElementById("id-BigConteiner").value;
-  let refoundTot = SmallConteiner * deposit_SmallConteiner + BigConteiner * deposit_BigConteiner;
-  document.getElementById( "id-roomArea" ).innerHTML = `The total reimbursement of the deposited containers is $ 
+  const deposit_smallConteiner = 0.1;
+  const deposit_bigConteiner = 0.25;
+  var smallConteiner = document.getElementById("id-SmallConteiner").value;
+  var bigConteiner = document.getElementById("id-BigConteiner").value;
+  let refoundTot =
+    smallConteiner * deposit_smallConteiner +
+    bigConteiner * deposit_bigConteiner;
+  document.getElementById(
+    "id-roomArea"
+  ).innerHTML = `The total reimbursement of the deposited containers is $ 
    ${refoundTot.toFixed(2)}`;
 }
 refoundConteiner();
--- projects/m1/002-bottle-deposits/solution/js/index.js
+++ projects/m1/002-bottle-deposits/solution/js/index.js
@@ -1,10 +1,21 @@
-const deposit_SmallConteiner = 0.10;
-const deposit_BigConteiner = 0.25;
+// constant statements
+const deposit_smallConteiner = 0.1;
+const deposit_bigConteiner = 0.25;
+//variable declaration
+let smallConteiner, bigConteiner, refoundTot;
+smallConteiner = prompt("Please insert the number of containers small", 0);
+bigConteiner = prompt("Please insert the number of containers big", 0);
+refoundTot =
+  smallConteiner * deposit_smallConteiner + bigConteiner * deposit_bigConteiner;
 
-let SmallConteiner = prompt("Please insert the number of containers small", 0);
-let BigConteiner = prompt("Please insert the number of containers big", 0);
-let refoundTot =   SmallConteiner * deposit_SmallConteiner + BigConteiner * deposit_BigConteiner;
+console.log(
+  `The total reimbursement of the deposited containers is $ ${refoundTot.toFixed(
+    2
+  )}`
+);
 
-console.log( `The total reimbursement of the deposited containers is $ ${refoundTot.toFixed(2)}`);
-
-document.write(`The total reimbursement of the deposited containers is $ ${refoundTot.toFixed(2)}`);
+document.write(
+  `The total reimbursement of the deposited containers is $ ${refoundTot.toFixed(
+    2
+  )}`
+);
