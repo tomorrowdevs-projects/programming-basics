7d21e4a38467abb9c0f3fc65a31f5dc2aa4542e3
Alessandro <github25@alessandro-pifferi.com>
2023-01-04 23:20:46 +0100

    update (m1): Solution/003-making-change

--- projects/m1/001-area-of-a-room/js/index.js
+++ projects/m1/001-area-of-a-room/js/index.js
@@ -1,5 +1,7 @@
-let roomLength = prompt( "Please insert the length of the room in linear metres" );
+let roomLength = prompt(
+  "Please insert the length of the room in linear metres"
+);
 let roomWidth = prompt("Please insert the width of the room in linear metres");
-var roomArea = roomLength * roomWidth;  //calculation of room area
+var roomArea = roomLength * roomWidth; //calculation of room area
 console.log(`The area of the room is ${roomArea.toFixed(2)} square meters`);
 document.write(`The area of the room is ${roomArea.toFixed(2)} square meters`);
