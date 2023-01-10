e3fde4ed34bf22844cc84d7964752458d77d4d3f
Massimiliano Foti <max.foti93@hotmail.it>
2023-01-10 22:57:12 +0100

    Rev. 2 solution-of-units-of-time

--- projects/m1/004-units-of-time/solutions/MassimilianoFoti/solution-of-units-of-time.js
+++ projects/m1/004-units-of-time/solutions/MassimilianoFoti/solution-of-units-of-time.js
@@ -15,3 +15,8 @@ let numberSeconds = prompt ("How many seconds?");
 numberSeconds = parseInt(numberSeconds);
 
 console.log(numberDays * secondsForAfullDay + numberHours * secondsForAhour + numberMinutes * secondsForAminute + numberSeconds);
+
+
+/* Ho inserito il parseInt in quanto senza di esso, quando il prompt mi domanda il numero di giorni o ore o minuti o secondi, se in uno di essi l'utente dovesse rispondere 0,
+questo numero verrebbe inteso come una stringa, perchè il numero è racchiuso sul prompt (stringa). Per cui, il parseInt "annulla" questa stringa e anzi, trasforma questo zero in numero.
+Quindi moltiplicando e quindi alla fine sommando, lo zero verrò contato come numero, come deve essere*/
