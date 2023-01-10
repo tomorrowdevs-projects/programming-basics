2d4729391ea2ba1d247d684c3d4072b44a8f3d3e
Raffina92 <raffaellas92@gmail.com>
2023-01-10 18:09:08 +0100

    variable names have been written in lowercase

--- projects/m1/002-bottle-deposits/python/main.py
+++ projects/m1/002-bottle-deposits/python/main.py
@@ -1,4 +1,4 @@
-Smallcontainers = int(input("How many containers of 1 liter or less do you have? "))
-Bigconteiners = int(input("How many containers of more than 1 liter do you have? "))
-Refund = Smallcontainers*0.1 + Bigconteiners*0.25
-print("The amount of your refund is ","%.2f"%Refund,"$")
+small_containers = int(input("How many containers of 1 liter or less do you have? "))
+big_containers = int(input("How many containers of more than 1 liter do you have? "))
+refund = small_containers*0.1 + big_containers*0.25
+print("The amount of your refund is ","%.2f"%refund,"$")
