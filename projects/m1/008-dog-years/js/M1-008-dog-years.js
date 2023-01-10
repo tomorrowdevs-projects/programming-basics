a918d64d8ecc31bbd43537d9718138537fd1f3e7
Alessandro <github25@alessandro-pifferi.com>
2023-01-10 14:56:45 +0100

    fix (m1): Solution/m1-008-dog-years

--- projects/m1/008-dog-years/js/M1-008-dog-years.js
+++ projects/m1/008-dog-years/js/M1-008-dog-years.js
@@ -1,48 +1,46 @@
 const yearDogPuppy = 10.5; // constant statements
-
 let yearHuman, yearsDogAdult, ageDog; //variable declarations
 
 //yearHuman = 3;
 yearHuman = prompt("How many human years do you want to convert in dog years?");
 
 if (yearHuman <= 0) {
-  console.log("Years Human    ", yearHuman);
-  //consol.log("Error: This value is not valid!");
   alert("Error: The years inserted are invalid.");
 } else {
-  if (yearHuman >= 2) {
-    console.log("Years Human    ", yearHuman); //console.log for error checking
-
+  if (yearHuman > 2) {
     yearsDogAdult = yearDogPuppy * 2; // I have obtained the years of a dog that has reached maturity, that is, it is an adult  ---> 2 anni canini = 10.5 *2
+    ageDog = (yearHuman * 4) + yearsDogAdult;    // I calculated each additional human year as 4 dog years. ---> 1 human year = 4 dog years + adult dog years
+    console.log("Years Human    ", yearHuman); //console.log for error checking
     console.log("Year Dog Adult ", yearsDogAdult); //console.log for error checking
-
-    ageDog = yearHuman * 4 + yearsDogAdult; // I calculated each additional human year as 4 dog years. ---> 1 human year = 4 dog years + adult dog years
+    //  ageDog = (yearHuman * 4) + yearsDogAdult;    // I calculated each additional human year as 4 dog years. ---> 1 human year = 4 dog years + adult dog years
     console.log("Age Dog        ", ageDog); //console.log for error checking
-
-    console.log(`The dog is ${ageDog} years old.`);
     alert(`The dog is ${ageDog} years old.`);
-  } else {
-    console.log(`The dog is ${yearDogPuppy} years old.`);
-    alert(`The dog is ${yearDogPuppy} years old.`);
+  } else if (yearHuman == 2) { // se metto if (yearHuman <= 2)  quando vado ad inserire  yearHuman = 1 mi restituisce  21 anni ??
+    alert(`The Adultis dog is ${yearDogPuppy * 2} years old.`);
+  }
+  else {
+    alert(`The Puppy dog is ${yearDogPuppy} years old.`);
   }
 }
+/*
+How many human years do you want to convert in dog years?> 3
+Years Human     3
+Year Dog Adult  21
+Age Dog         33
+The dog is 33 years old.
+*/
 
 /*
 # ANNI DEL CANE
-
 - i cani raggiungono l'età adulta in circa 2 anni.
 - contare ciascuno dei primi 2 anni umani come 10,5 anni canini,
-- contare ogni anno umano come 4 cani
-
+- contare ogni 1 umano come 4 cani
 Scrivere un programma che implementi la conversione da
 - anni umani in anni canini
-
 Controllare il programma funzioni correttamente per:
-- conversioni inferiori a due anni umani
-- conversioni di due o più anni umani.
-
+- conversioni inferiori a 2 anni umani
+- conversioni di 2 o più anni umani.
 Il programma visualizza un messaggio di errore appropriato se l'utente immette un numero negativo.
-
 # Documentazione
 Per questa soluzione di progetto è possibile utilizzare:
 - Variabili, espressioni, istruzioni
