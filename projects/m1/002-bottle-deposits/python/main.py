33a509e5e800f1c3a3247c8d015cf8206b3675a9
Raffina92 <raffaellas92@gmail.com>
2023-01-10 19:33:07 +0100

    Removed exercise 2 from branch solution/m1-003

--- projects/m1/002-bottle-deposits/python/main.py
+++
@@ -1,4 +0,0 @@
-Smallcontainers = int(input("How many containers of 1 liter or less do you have? "))
-Bigconteiners = int(input("How many containers of more than 1 liter do you have? "))
-Refund = Smallcontainers*0.1 + Bigconteiners*0.25
-print("The amount of your refund is ","%.2f"%Refund,"$")
