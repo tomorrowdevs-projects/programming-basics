0d4a93ac393b8d28898421e8a1f79d55bd35094f
Alessandro <github25@alessandro-pifferi.com>
2023-01-11 11:21:21 +0100

    fix 3 (m1): Solution/m1-006-sum-of-digits-in-a-integer

--- projects/m1/006-sum-of-digits-in-a-integer/js/M1-006-sum-of-digits-in-a-integer.js
+++ projects/m1/006-sum-of-digits-in-a-integer/js/M1-006-sum-of-digits-in-a-integer.js
@@ -1,26 +1,17 @@
-let numberIn, splitNumberInteger, firstNumber, secondNumber, thirdNumber, quarterNumber, sumNumber;
+let numberIn = 0, splitNumberInteger, firstNumber, secondNumber, thirdNumber, quarterNumber, sumNumber;
 
 //numberIn = '3141';
 numberIn = prompt("Please insered the whole four digit number");
-//Subdivision of an integer four digit
-splitNumberInteger = numberIn.split('');
-//convert strings to integers
-firstNumber = parseInt(splitNumberInteger[0]);
-secondNumber = parseInt(splitNumberInteger[1]);
-thirdNumber = parseInt(splitNumberInteger[2]);
-quarterNumber = parseInt(splitNumberInteger[3]);
 
-
-sumNumber = firstNumber + secondNumber + thirdNumber + quarterNumber; // Sum of the entered integers.
-
-if (splitNumberInteger.length == 4) {
-  alert(`The sum of the numbers insered is " ${firstNumber}+${secondNumber}+${thirdNumber}+${quarterNumber}=${sumNumber} "`);
-}
-else if (isNaN(sumNumber) == true) {
-  // alert(`Error! You entered characters instead of whole numbers (if).`);
-  console.log("Error! You entered characters instead of whole numbers (if)", isNaN(sumNumber), sumNumber); // Error check line.
-}
-else {
-  //alert(`The data entered is incorrect`);
-  console.log("The data entered is incorrect (else)", isNaN(sumNumber), sumNumber);
+if ((numberIn.length < 4) || (numberIn.length > 4)) { // I check whether entered numbers are less than or greater than 4.
+  console.log("The data entered is incorrect");
+} else if (numberIn.length == 4) {// I check whether entered numbers are 4.
+  splitNumberInteger = numberIn.split(''); //Subdivision of an integer four digit
+  //convert strings to integers
+  firstNumber = parseInt(splitNumberInteger[0]);
+  secondNumber = parseInt(splitNumberInteger[1]);
+  thirdNumber = parseInt(splitNumberInteger[2]);
+  quarterNumber = parseInt(splitNumberInteger[3]);
+  sumNumber = firstNumber + secondNumber + thirdNumber + quarterNumber; // Sum of the entered integers.
+  alert(`The sum of the numbers insered is  ${firstNumber}+${secondNumber}+${thirdNumber}+${quarterNumber}=${sumNumber} "`);
 }
