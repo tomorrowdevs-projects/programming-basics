9b343230985dcc441fbedac5fd067a6960cfb851
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-01-11 22:50:17 +0100

    feat(m3): added solution to exe 005-possible-distance.

---
+++ projects/m3/005-possible-distance/js/first_solution.js
@@ -0,0 +1,37 @@
+
+
+const coinsDenominations = [0.01, 0.05, 0.10, 0.25];
+
+
+function isPossibleChange (total, numberOfCoins, coinsDenominations) {
+
+  total = +total.toFixed(2);
+
+  if (total === 0 && numberOfCoins === 0) return true;
+  if (total !== 0 && numberOfCoins === 0) return false;
+  if (total === 0 && numberOfCoins !== 0) return false;
+
+  if (( total >= coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] ) && ( numberOfCoins > 0 ) && ( total % coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] !== 0) && (total / coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] !== numberOfCoins)){
+      return isPossibleChange(total - coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))], numberOfCoins - 1, coinsDenominations);
+  };
+
+  if (( total >= coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] ) && ( numberOfCoins > 0 ) && ( total % coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] === 0) && (total / coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] !== numberOfCoins)){
+    return isPossibleChange(total, numberOfCoins, coinsDenominations.slice(0, -1));
+  };
+
+  if (( total >= coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] ) && ( numberOfCoins > 0 ) && ( total % coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] !== 0) && (total / coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] === numberOfCoins)){
+    return isPossibleChange(total, numberOfCoins, coinsDenominations.slice(0, -1));
+  };
+
+  if (( total >= coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] ) && ( numberOfCoins > 0 ) && ( total % coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] === 0) && (total / coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))] === numberOfCoins)){
+    return isPossibleChange(total - coinsDenominations[coinsDenominations.indexOf(Math.max(...coinsDenominations))], numberOfCoins - 1, coinsDenominations);
+  };
+
+
+
+  return isPossibleChange(total, numberOfCoins, coinsDenominations.slice(0, -1));
+};
+
+// console.log(isPossibleChange(0.45, 5, coinsDenominations));
+
+module.exports = isPossibleChange;
