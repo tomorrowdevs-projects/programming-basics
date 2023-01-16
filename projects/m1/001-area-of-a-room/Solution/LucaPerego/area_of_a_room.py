934aa09de14865fbbb6f54d5148ab78eb2556a64
Luca77Perego <luca77perego@gmail.com>
2023-01-16 18:11:16 +0100

    Solution 2 for the area-of-a-room exercise

--- projects/m1/001-area-of-a-room/Solution/LucaPerego/area_of_a_room.py
+++ projects/m1/001-area-of-a-room/Solution/LucaPerego/area_of_a_room.py
@@ -9,4 +9,3 @@ lenght_room = float(input("And now enter the lenght of the room in meters: "))
 area_room = float(width_room * lenght_room)
 
 print(f"Tha area of a room with a width of {width_room} metres and with a lenght of {lenght_room} metres is {area_room} metres")
-
