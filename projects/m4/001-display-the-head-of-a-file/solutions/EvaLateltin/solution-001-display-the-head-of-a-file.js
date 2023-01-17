655688eaf94eb6918b483c8f90c4ae140462f337
Eva Lateltin <evalateltin@gmail.com>
2023-01-17 23:23:28 +0100

    fix(m4): improved solution ex. 001

--- projects/m4/001-display-the-head-of-a-file/solutions/EvaLateltin/solution-001-display-the-head-of-a-file.js
+++ projects/m4/001-display-the-head-of-a-file/solutions/EvaLateltin/solution-001-display-the-head-of-a-file.js
@@ -1,38 +1,26 @@
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 
-const readLines = function (path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, "utf8", function (err, data) {
-      if (err) {
-        reject("The file does not exists");
-      } else {
-        resolve(data);
+const headOfFile = (path, number) => {
+  fsPromises
+    .readFile(path, "utf8")
+    .then((data) => {
+      const lines = data.split("\n");
+      let result = "";
+
+      if (lines.length < number) {
+        number = lines.length;
+        result += `The file only has ${lines.length} lines.\n`;
       }
-    });
-  });
-};
 
-const headOfFile = function (path, number) {
-  return new Promise((resolve, reject) => {
-    readLines(path)
-      .then((data) => {
-        const lines = data.split("\n");
-        const result = [];
-        if (lines.length < number) {
-          number = lines.length;
-          result.push(`The file only has ${lines.length} lines.`);
-        } else {
-          result.push(`Displaying the first ${number} lines of the file.`);
-        }
-        for (let i = 0; i < number; i++) {
-          result.push(lines[i]);
-        }
-        resolve(result);
-      })
-      .catch((err) => console.log(err));
-  })
+      result += `Displaying the first ${number} lines of the file.`;
+
+      for (let i = 0; i < number; i++) {
+        result += `\n${lines[i]}`;
+      }
+      return result;
+    })
     .then((result) => console.log(result))
-    .catch((err) => console.log(err));
+    .catch((err) => console.log(`Cannot find the entered file: ${err.path}`));
 };
 
 headOfFile("text.txt", 10);
