e8dacd55e20eb6088d79ce2e4e97dfbe5d790951
Luca77Perego <luca77perego@gmail.com>
2023-01-17 15:11:13 +0100

    Correction Mispelling area-of-a-room
    This correction is send by terminal: exercise done to verify knowledge of Git Commands from the terminal

--- projects/m1/001-area-of-a-room/Solution/LucaPerego/area_of_a_room.py
+++ projects/m1/001-area-of-a-room/Solution/LucaPerego/area_of_a_room.py
@@ -1,11 +1,13 @@
 #Calculate: Area of a Room (Area=Width*Lenght)
 
-print("Program to calculate the aerea of a room in meter and remember - also 'float numbers' are admitted!")
+print("Program to calculate the aerea of a room in meters and remember - also 'float numbers' are admitted!")
 
-width_room = float(input("Please, enter the width of the room in meters: "))
+width_room = float(input("Please, enter the width of the room in meters (m): "))
 
-lenght_room = float(input("And now enter the lenght of the room in meters: "))
+lenght_room = float(input("And now enter the lenght of the room in meters (m): "))
 
 area_room = float(width_room * lenght_room)
 
-print(f"Tha area of a room with a width of {width_room} metres and with a lenght of {lenght_room} metres is {area_room} metres")
+print(f"Tha area of a room - with a width of {width_room} m and with a lenght of {lenght_room} m - is {area_room} m")
+
+#Mispellings removed
