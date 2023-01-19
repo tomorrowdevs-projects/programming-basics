bd01945dfb2801a505597907de8ded264c825348
Eva Lateltin <evalateltin@gmail.com>
2023-01-19 19:20:27 +0100

    fix(m4): fixed sol. ex. 005

--- projects/m4/005-words-that-occur-most/js/index.js
+++ projects/m4/005-words-that-occur-most/js/index.js
@@ -4,16 +4,15 @@ function checkFrequencies(inputFile) {
   fsPromises
     .readFile(inputFile, "utf8")
     .then((data) => {
-      new Promise((resolve, reject) => {
-        const array = getArray(data);
-        const set = new Set(array);
-        const result = {};
-        set.forEach((letter) => {
-          result[letter] = array.filter((x) => x == letter).length;
-        });
-        resolve(sortObject(result));
-      }).then((result) => console.log(result));
+      const array = getArray(data);
+      const set = new Set(array);
+      const result = {};
+      set.forEach((letter) => {
+        result[letter] = array.filter((x) => x == letter).length;
+      });
+      return sortObject(result);
     })
+    .then((result) => console.log(result))
     .catch((err) => console.log("The file you entered does not exists."));
 }
 
