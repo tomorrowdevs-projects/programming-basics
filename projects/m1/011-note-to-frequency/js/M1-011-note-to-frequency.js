64ea0d4acf9117c5ea17247df2db2ea165cd0021
Alessandro <github25@alessandro-pifferi.com>
2023-01-19 23:25:43 +0100

    Fix update 2 (m1): Solution/m1-011-note-to-frequency

--- projects/m1/011-note-to-frequency/js/M1-011-note-to-frequency.js
+++ projects/m1/011-note-to-frequency/js/M1-011-note-to-frequency.js
@@ -1,18 +1,55 @@
-const Note = ["C4", "D4", "E4", "F4", "G4", "A4", "B4"];
+// Declarations with values of constants.
+const note_C4_Hz = 261.63;
+const note_D4_Hz = 293.66;
+const note_E4_Hz = 329.63;
+const note_F4_Hz = 349.23;
+const note_G4_Hz = 392.00;
+const note_A4_Hz = 400.00;
+const note_B4_Hz = 493.88;
 
-const FrequencyHz = [261.63, 293.66, 329.63, 349.23, 392.00, 392.00, 440.00, 493.88];
+// Declarations with values of local variables.
+let noteIn_User, note_Octave_check, note_Octave_Letter, note_Octave_Number, note_Octave_Number_FreuencyHz;
 
-let noteUser, i, frequenzyHzNote;
-noteUser = prompt("Please insert the name of the Note");
-noteUser=noteUser.toUpperCase();
-//alert(`The Note entered is " ${noteUser} "`);
+// Reading Notes
+noteIn_User = prompt('Please, insered the note:').toUpperCase();
 
-for (i = 0; i < Note.length; i++) {
-  if (noteUser == Note[i]) {
-    frequenzyHzNote = FrequencyHz[i].toFixed(2);
-    alert(`The frequency of the inserted note " ${noteUser} " is " ${frequenzyHzNote} (Hz) "`);
-  }
+// The note inserted must have 2 characters, the first between A and G and the second between 0 and 8
+note_Octave_check = /^[A-G][0-8]$/.test(noteIn_User);
+
+// Check the note inserted must have 2 characters, the first between A and G and the second between 0 and 8
+if (note_Octave_check) {
+  note_Octave_Letter = noteIn_User.slice(0, 1);
+  note_Octave_Number = parseInt(noteIn_User.slice(1, 2));
+} else {
+  alert(`\nError! --> The data entered are not musical notes.\n`);
 }
 
-//  Formula passata da Vittorio Manicanti nella call di lunedi 09/01/2023
-//  (392.00/(Math.pow(2, 4-noteNum))).toFixed(2); 
+// Calculating additional frequencies from the given value (freq. 4th octave) and varying the exponent according to the input received
+if (note_Octave_Letter == "C") {
+  note_Octave_Number_FreuencyHz = (note_C4_Hz / (Math.pow(2, 4 - note_Octave_Number))).toFixed(2);
+  alert(`\nTo The note "${note_Octave_Letter}${note_Octave_Number}" corresponds to the frequency --> ${note_Octave_Number_FreuencyHz} Hz\n`);
+}
+else if (note_Octave_Letter == "D") {
+  note_Octave_Number_FreuencyHz = (note_D4_Hz / (Math.pow(2, 4 - note_Octave_Number))).toFixed(2);
+  alert(`\nTo The note "${note_Octave_Letter}${note_Octave_Number}" corresponds to the frequency --> ${note_Octave_Number_FreuencyHz} Hz\n`);
+}
+else if (note_Octave_Letter == "E") {
+  note_Octave_Number_FreuencyHz = (note_E4_Hz / (Math.pow(2, 4 - note_Octave_Number))).toFixed(2);
+  alert(`\nTo The note "${note_Octave_Letter}${note_Octave_Number}" corresponds to the frequency --> ${note_Octave_Number_FreuencyHz} Hz\n`);
+}
+else if (note_Octave_Letter == "F") {
+  note_Octave_Number_FreuencyHz = (note_F4_Hz / (Math.pow(2, 4 - note_Octave_Number))).toFixed(2);
+  alert(`\nTo The note "${note_Octave_Letter}${note_Octave_Number}" corresponds to the frequency --> ${note_Octave_Number_FreuencyHz} Hz\n`);
+}
+else if (note_Octave_Letter == "G") {
+  note_Octave_Number_FreuencyHz = (note_G4_Hz / (Math.pow(2, 4 - note_Octave_Number))).toFixed(2);
+  alert(`\nTo The note "${note_Octave_Letter}${note_Octave_Number}" corresponds to the frequency --> ${note_Octave_Number_FreuencyHz} Hz\n`);
+}
+else if (note_Octave_Letter == "A") {
+  note_Octave_Number_FreuencyHz = (note_A4_Hz / (Math.pow(2, 4 - note_Octave_Number))).toFixed(2);
+  alert(`\nTo The note "${note_Octave_Letter}${note_Octave_Number}" corresponds to the frequency --> ${note_Octave_Number_FreuencyHz} Hz\n`);
+}
+else if (note_Octave_Letter == "B") {
+  note_Octave_Number_FreuencyHz = (note_B4_Hz / (Math.pow(2, 4 - note_Octave_Number))).toFixed(2);
+  alert(`\nTo The note "${note_Octave_Letter}${note_Octave_Number}" corresponds to the frequency --> ${note_Octave_Number_FreuencyHz} Hz\n`);
+}
