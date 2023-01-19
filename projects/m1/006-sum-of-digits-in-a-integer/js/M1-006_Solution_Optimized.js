324bac1a939815875d987f3ccf169a1ef4d5c0e8
Alessandro <github25@alessandro-pifferi.com>
2023-01-19 19:06:06 +0100

    fix 5  Solution Optimized Update1  (m1): Solution/m1-006-sum-of-digits-in-a-integer

--- projects/m1/006-sum-of-digits-in-a-integer/js/M1-006_Solution_Optimized.js
+++ projects/m1/006-sum-of-digits-in-a-integer/js/M1-006_Solution_Optimized.js
@@ -1,4 +1,4 @@
-let numberIn, checkCharactersLowercase, checkCharactersUppercase, array_split_numberIn, number = sumNumber = i = 0;
+let numberIn, checkCharactersLowercase, checkCharactersUppercase, array_split_numberIn, number = (sumNumber = i = 0);
 
 numberIn = prompt(`Please insered the whole four digit number`);
 
@@ -10,14 +10,18 @@ if (checkCharactersLowercase || checkCharactersUppercase) {
 } else {
   array_split_numberIn = numberIn.split("");
   // console.log("\n", array_split_numberIn, "\n");
+
   for (i = 0; i < array_split_numberIn.length; i++) {
     number = parseInt(array_split_numberIn[i]);
     sumNumber += parseInt(array_split_numberIn[i]);
     // console.log("\n", number);
   }
-  // console.log("\n", sumNumber);
-  array_split_numberIn = array_split_numberIn.join("+");
-  alert(
-    `\nSum of the input numbers inserted --> ${array_split_numberIn}=${sumNumber}\n`
-  );
+
+  if (isNaN(sumNumber)) {
+    alert(`\nError! --> You have insered numbers and characters.\n`);
+  } else {
+    array_split_numberIn = array_split_numberIn.join("+");
+    alert(`\nSum of the input numbers inserted --> ${array_split_numberIn}=${parseInt(
+        sumNumber)}\n`);
+  }
 }
