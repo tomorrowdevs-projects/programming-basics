2c5d02ce44ba4ef8ff3a1f8cac7b8583580468d4
Mattia Cherchi <mattiacherchi93@gmail.com>
2023-01-20 12:58:21 +0100

    refactor(m4):013-redacting-text-in-a-file

--- projects/m4/013-redacting-text-in-a-file/js/index.js
+++ projects/m4/013-redacting-text-in-a-file/js/index.js
@@ -1,25 +1,26 @@
 const fs = require('fs').promises;
-const replaceWord = require('./functions').replaceWord;
-const dataset = 'dataset.txt';
-const sensitiveWord = 'sensitiveWord.txt';
+const replaceWord = require('./functions');
+// const dataset = 'dataset.txt';
+// const sensitiveWord = 'sensitiveWord.txt';
 const output = 'output.txt';
+const arrDataset = ['dataset.txt','sensitiveWord.txt']
 
-const getFileRewritten = async () =>{
-    try{
-        const dataText = await fs.readFile(dataset , 'utf-8');
-        const text = dataText.toLowerCase();
-        
-        let dataSensWord = await fs.readFile(sensitiveWord, 'utf-8');
-        const sensWord = dataSensWord.split(/\W+/);
-      
-       
-       const result = replaceWord(text,sensWord);
-       await fs.writeFile(output, result);
-        
-    }catch(err){
-        console.log(err);
-    }  
+
+
+const getFileRewritten = ()=>{
+     const readArr = arrDataset.map((file)=>{
+        return fs.readFile(file,'utf-8');
+     });
+    Promise.all(readArr)
+        .then((data)=>{
+            const text = data[0].toLowerCase();
+            const sensWord = data[1].split(/\s+/);
+
+            const result = replaceWord(text, sensWord);
+
+            fs.writeFile(output, result);
+
+         }).catch((err) => console.log(err));
 }
-
 
 getFileRewritten();
