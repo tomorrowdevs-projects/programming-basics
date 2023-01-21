b7e0b002b12a93c1242eb980cc1ab68034d28a78
Tino Bokoshev <bokoshevtino@gmail.com>
2023-01-21 17:30:06 +0100

    fix es 024 m2

--- projects/m2/024-create-a-bingo-card/python/main.py
+++ projects/m2/024-create-a-bingo-card/python/main.py
@@ -8,13 +8,15 @@ def bingo():
     "G": sample(range(46,61),k=5),
     "O": sample(range(61,76),k=5)
     }
+    return(bingo_card)
 
+def bingo_display(bingo_card):
     print("{:<2} {:<2} {:<2} {:<2} {:<2}".format("B", "I", "N", "G", "O"))
     for index in range(5):
         print("{:<2} {:<2} {:<2} {:<2} {:<2}".format(bingo_card["B"][index],bingo_card["I"][index],bingo_card["N"][index],bingo_card["G"][index],bingo_card["O"][index]))
 
 if __name__ == "__main__":
-    bingo()
+    bingo_display(bingo())
         
 
     
