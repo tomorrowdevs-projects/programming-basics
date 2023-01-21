8549725f428dc07a4410fc64bc4a0bedf9e9620e
Alessandro <github25@alessandro-pifferi.com>
2023-01-21 22:13:30 +0100

    fix- 9-Loop-do-While-Characters-Special (m1): Solution/m1-006-sum-of-digits-in-a-integer

--- projects/m1/006-sum-of-digits-in-a-integer/js/index.js
+++ projects/m1/006-sum-of-digits-in-a-integer/js/index.js
@@ -1,20 +1,21 @@
 //variable declarations
-let numberIn, checkCharactersLowercase, checkCharactersUppercase, array_split_numberIn, number = (sumNumber = i = 0), array_split_numberIn_Conv;
+let numberIn, checkCharactersLowercase, checkCharactersUppercase, checkCharactersSpecial, array_split_numberIn, number = (sumNumber = i = 0), array_split_numberIn_Conv;
 
 //do-while loop to check that no characters have been inserted
 do {
-  numberIn = prompt(`Please insered the whole four digit number`);
+  numberIn = prompt(`Please insered the number -->`);
 
   // I use the test() function to detect whether the numbers entered contain lowercase or uppercase characters
   checkCharactersLowercase = /[a-z]/.test(numberIn);
   checkCharactersUppercase = /[A-Z]/.test(numberIn);
+  checkCharactersSpecial = /[`,¬,¦,!,",£,$,%,^,&,(,),_,{,},;,:,',@,#,~,.,?,+,-,*]/.test(numberIn);
 
   // if characters are present, I return an 'Error' message.
-  if (checkCharactersLowercase || checkCharactersUppercase ) {
+  if (checkCharactersLowercase || checkCharactersUppercase || checkCharactersSpecial) {
     alert("\nError! --> You have insered numbers and characters. \u{1F648}\n");
-   // console.log("\nError! --> You have insered nu0mbers and characters. \u{1F648}\n");
+    // console.log("\nError! --> You have insered nu0mbers and characters. \u{1F648}\n");
   }
-} while (checkCharactersLowercase || checkCharactersUppercase)
+} while (checkCharactersLowercase || checkCharactersUppercase || checkCharactersSpecial)
 
 // convert the number string into an arrey
 array_split_numberIn = numberIn.split("");
@@ -37,4 +38,4 @@ console.log("String separator ',' replaced by '+' with join('+') -->", array_spl
 
 //print the required result on the screen
 alert(`Sum of the input numbers inserted --> ${array_split_numberIn_Conv}=${sumNumber}  \u{1F973}`);
-//console.log(`Sum of the input numbers inserted --> ${array_split_numberIn_Conv}=${sumNumber}  \u{1F973}`);
+console.log(`Sum of the input numbers inserted --> ${array_split_numberIn_Conv}=${sumNumber}  \u{1F973}`);
