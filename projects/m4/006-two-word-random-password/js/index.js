c792487955b777d5779bc044934773e44f7b6752
Eva Lateltin <evalateltin@gmail.com>
2023-01-22 22:10:03 +0100

    fix(m4): improved sol. ex 006

--- projects/m4/006-two-word-random-password/js/index.js
+++ projects/m4/006-two-word-random-password/js/index.js
@@ -1,31 +1,46 @@
-const fs = require('fs');
-const fileName = 'input.txt'
-const data = getArray(fs.readFileSync(fileName, { encoding: 'utf8' }));
+const fsPromises = require("fs").promises;
+const fileName = "input.txt";
 
 // SOLUTION
-const password = function(data){
-const first = random(data.filter(word => word.length >= 3 && word.length < 7));
-const second = random(data.filter(word => first.length + word.length >= 8 && first.length + word.length <= 10 && word.length >= 3))
-return capitalize(first) + capitalize(second);
-}
-console.log('The following password has been generated:', password(data));
+fsPromises
+  .readFile(fileName, "utf8")
+  .then((data) => {
+    data = getArray(data);
+    const first = random(
+      data.filter((word) => word.length >= 3 && word.length < 7)
+    );
+    const second = random(
+      data.filter(
+        (word) =>
+          first.length + word.length >= 8 &&
+          first.length + word.length <= 10 &&
+          word.length >= 3
+      )
+    );
+    return capitalize(first) + capitalize(second);
+  })
+  .then((data) =>
+    console.log("The following password has been generated:", data)
+  );
 
-// FUNCTIONS
+// HELPER FUNCTIONS
 function random(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-function getArray(input){
+function getArray(input) {
   const array = input.split(/[\r\n]+| /g);
   array.forEach((element, index) => {
-    array[index] = element.replace(/^[^A-Za-z]+/g, "").replace(/[^A-Za-z]+$/g, "")
-    if(array[index] === ''){
-      array.splice(index, 1)
+    array[index] = element
+      .replace(/^[^A-Za-z]+/g, "")
+      .replace(/[^A-Za-z]+$/g, "");
+    if (array[index] === "") {
+      array.splice(index, 1);
     }
-  })
+  });
   return array;
 }
 
-function capitalize(word){
+function capitalize(word) {
   return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
 }
