f2e2e667f0468ad991208af245cd9790cf8e795c
LG <luca.galeno@gmail.com>
2023-01-23 09:50:20 +0100

    fix(m4): fix exe 014

--- projects/m4/014-words-with-six-vowels-in-order/js/index.js
+++ projects/m4/014-words-with-six-vowels-in-order/js/index.js
@@ -9,22 +9,22 @@ const fs = require('fs').promises;
 const file = 'input.txt'
 
 //function to read the content of the file 
-const readFile = file => {
+const readFileFunction = file => {
     return fs.readFile(file, {encoding:'utf-8'})
         .then((data) => {
                         
             const regx = /\w*[a]\w*[e]\w*[i]\w*[o]\w*[u]\w*[y]\w*/gi
-            const word = data.match(regx)
-            return  console.log(`this are the words that contains all the wovels (aeiouy) in order:`) , console.log(word)})
+            const word = (data.match(regx)).join('\n')
+            return  (`this are the words that contains all the wovels (aeiouy) in order:\n${word}`)})
 
         .catch ((error) => {
             if (error.code === 'ENOENT') {
                 console.error(`${file} file does not exist`)
-              } else {
+            } else {
                 console.error(`something went wrong:`, error)
-              }
+            }
             })
 }
 
-readFile(file)
+readFileFunction(file).then(value => console.log(value))
 
