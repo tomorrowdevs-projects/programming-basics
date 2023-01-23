5596aa445268a1ec2d887da1501b93cfa074d0c6
Alessandro <github25@alessandro-pifferi.com>
2023-01-23 11:34:13 +0100

    fix 10-Loop-do-While-Characters-SpecialV3 (m1): Solution/m1-006-sum-of-digits-in-a-integer

--- projects/m1/006-sum-of-digits-in-a-integer/js/M1-006_Solution_Loop_do-while.js
+++ projects/m1/006-sum-of-digits-in-a-integer/js/M1-006_Solution_Loop_do-while.js
@@ -1,46 +1,26 @@
 //variable declarations
-let numberIn, checkCharactersLowercase, checkCharactersUppercase, checkCharactersSpecial, array_split_numberIn, number = (sumNumber = i = 0), array_split_numberIn_Conv;
-
+let numberIn, checkCharacters, array_split_numberIn, number = (sumNumber = i = 0), array_split_numberIn_Conv;
 //do-while loop to check that no characters have been inserted
 do {
   numberIn = prompt(`Please insered the number -->`);
-
-  // I use the test() function to detect whether the numbers entered contain lowercase or uppercase characters
-  checkCharactersLowercase = /[a-z]/.test(numberIn);
-  checkCharactersUppercase = /[A-Z]/.test(numberIn);
-  checkCharactersSpecial = /[`¬¦!"£$€%^&*)(=+;:@'#><?}{,/,\,|]/.test(numberIn);
-
-  // if characters are present, I return an 'Error' message.
-  if (checkCharactersLowercase || checkCharactersUppercase || checkCharactersSpecial) {
-    alert("\nError! --> You have insered numbers and characters. \u{1F648}\n");
-    // console.log("\nError! --> You have insered nu0mbers and characters. \u{1F648}\n");
+  // I use the test() function to detect whether the numbers entered contain  characters
+  checkCharacters = /[a-z,A-Z,`.-¬¦!"£$€%^&*)(=\+\;:@'#><?}{,\/,\\,|\[\]]/.test(numberIn);
+  // if characters are present,
+  if (checkCharacters) {
+    // i return an 'Error' message.
+    alert(`\nError! --> You have insered numbers and characters. \u{1F648}\n`);
   }
-} while (checkCharactersLowercase || checkCharactersUppercase || checkCharactersSpecial)
-
+} while (checkCharacters)
 // convert the number string into an arrey
 array_split_numberIn = numberIn.split("");
-
 //read all the elements of the array and calculate the sum
 for (i = 0; i < array_split_numberIn.length; i++) {
+  // converts the array elements to integers and then performs the sum
   number = parseInt(array_split_numberIn[i]);
+  // sum array elements
   sumNumber += number;
 }
-
-if(isNaN(sumNumber)){
-   alert("\nError! --> You have insered numbers and characters. \u{1F648}\n");
-} else {
-// print arrey
-console.log("\nNormal array -->", array_split_numberIn, "\n");
-
-//Convert array into a string with the "join()" function
-array_split_numberIn_Conv = array_split_numberIn.join();
-console.log("Array converted using the join() function -->", array_split_numberIn_Conv, "\n");
-
-// replace the "," separating the array, also included in the conversion, with the "+" sign
+// converts an array into a string with the join() function function and replaces the ',' character separating the array with a '+' sign.
 array_split_numberIn_Conv = array_split_numberIn.join('+');
-console.log("String separator ',' replaced by '+' with join('+') -->", array_split_numberIn_Conv, "\n");
-
 //print the required result on the screen
-alert(`Sum of the input numbers inserted --> ${array_split_numberIn_Conv}=${sumNumber}  \u{1F973}`);
-console.log(`Sum of the input numbers inserted --> ${array_split_numberIn_Conv}=${sumNumber}  \u{1F973}`);
-}
+alert(`\nSum of the input numbers inserted --> ${array_split_numberIn_Conv}=${sumNumber}  \u{1F973}\n`);
