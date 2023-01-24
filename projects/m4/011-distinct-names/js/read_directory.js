4ad6fc608efd240f4c6964ca5a7d27d1cbc4f8cf
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-01-24 14:18:39 +0100

    feat(m4): added solution to exe 011-distinct-names.

---
+++ projects/m4/011-distinct-names/js/read_directory.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const readDir = () => {
+
+        return new Promise ((resolve, reject) => {
+
+            fs.readdir (`./dataset`, 'utf-8', (err, files) => {
+
+                if (err) reject (`ERROR: The requested directory does not exist!`);
+                resolve (files);
+            });
+
+        });
+
+};
+
+module.exports = readDir;
