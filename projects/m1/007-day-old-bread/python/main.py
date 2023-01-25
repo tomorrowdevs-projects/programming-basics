5c075ff8b97a41fad79d9560e5a3c4cf5819d661
Raffina92 <raffaellas92@gmail.com>
2023-01-25 18:48:53 +0100

    Used more consistent variable names

--- projects/m1/007-day-old-bread/python/main.py
+++ projects/m1/007-day-old-bread/python/main.py
@@ -2,10 +2,10 @@ loaves = int(input("How many day old bread loaves would you like to buy? "))
 regular_price = 3.49
 discount = 0.6
 price_discountfree = (loaves * regular_price)
-price_with_discount = price_discountfree * discount
-total_price = price_discountfree*(1-discount)
+total_discount = price_discountfree * discount
+total_price = price_discountfree - total_discount
 
 print("{:>15} {:>7.2f}{}".format("Regular price",price_discountfree,"$"))
-print("{:>15} {:>7.2f}{}".format("Discount",price_with_discount,"$"))
+print("{:>15} {:>7.2f}{}".format("Discount",total_discount,"$"))
 print("{:>15} {:>7.2f}{}".format("Total price",total_price,"$"))
 
