cb13924a02b163f93a997d7cd7396f71556bf55c
carlolombardini <lombardini.gd@gmail.com>
2023-01-26 14:54:15 +0100

    fix(m1): use const instead let and remove useless spaces in exercise 001

--- projects/m1/001-area-of-a-room/js/index.js
+++ projects/m1/001-area-of-a-room/js/index.js
@@ -1,8 +1,7 @@
-let user_room_width = prompt( 'Please enter the width of the room in meters as floating-point numbers: ', 'Example: 4.00' );
-let user_room_height = prompt( 'And the height of the room in meters: ', 'Example: 4.00' ); 
+const user_room_width = prompt( 'Please enter the width of the room in meters as floating-point numbers:', 'Example: 4.00' );
+const user_room_height = prompt( 'And the height of the room in meters:', 'Example: 4.00' ); 
 
 function room_area( room_width, room_height ) {
-
     room_width = parseFloat( room_width );
     room_height = parseFloat( room_height );
 
@@ -10,7 +9,7 @@ function room_area( room_width, room_height ) {
         return room_width * room_height + ' meters' ;
     }
 
-    return 'You entered one or more non-floating point values, please enter a valid value' ;
+    return 'You entered one or more non-floating point values, please enter a valid value';
 }
 
-console.log ( room_area ( user_room_width, user_room_height ) ) ;
+console.log ( room_area ( user_room_width, user_room_height ) );
