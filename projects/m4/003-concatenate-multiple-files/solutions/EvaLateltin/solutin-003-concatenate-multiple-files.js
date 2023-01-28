2088ae5ee8d6e348beb71c93cc8672ef98696b87
Eva Lateltin <evalateltin@gmail.com>
2023-01-28 18:16:51 +0100

    fix(m4): improved sol. ex. 003

--- projects/m4/003-concatenate-multiple-files/solutions/EvaLateltin/solutin-003-concatenate-multiple-files.js
+++ projects/m4/003-concatenate-multiple-files/solutions/EvaLateltin/solutin-003-concatenate-multiple-files.js
@@ -1,13 +1,11 @@
 const fs = require("fs").promises;
-const inputFiles = ["file1.txt", "file2.txt", "file3.txt"]; // insert in the array the files that will be concatenated
+const inputFiles = ["file1.txt", "file2.txt", "file3.txt"];
 
-// The fist paramether is the array of the files that will be concatenated, the second paramether is the file where all the contented will be concatenated.
-function concatenateFiles(array, output) {
-  array.forEach((file) => {
-    fs.readFile(file, "utf8")
-      .then((data) => fs.appendFile(output, data))
-      .catch((err) => console.log(err));
-  });
+async function concatenateFiles(array, output) {
+  for (const file of array) {
+    const fileContent = await fs.readFile(file, "utf8");
+    await fs.appendFile(output, fileContent + "\n");
+  }
 }
 
 concatenateFiles(inputFiles, "output.txt");
