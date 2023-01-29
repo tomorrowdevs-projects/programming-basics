c1c0052be72967c1a0657a9cc3a47b1fd4a4fbd7
Tino Bokoshev <bokoshevtino@gmail.com>
2023-01-29 21:21:20 +0100

    fix exercise

--- projects/m4/001-display-the-head-of-a-file/python/main.py
+++ projects/m4/001-display-the-head-of-a-file/python/main.py
@@ -2,9 +2,9 @@ import sys
 
 try:
     with open(sys.argv[1], 'r') as file:    #argv is automatically a list of strings representing the arguments (as separated by spaces) on the command-line.
-                                        #command-Line arguments are in argv[1+n] and argv[0] is the script name itself and can be ignored.       
-        for line in range(10):
-            print(file.readline())      #
+        lines = file.readlines()                              #command-Line arguments are in argv[1+n] and argv[0] is the script name itself and can be ignored.       
+        for line in lines[0:10]:
+            print(line)      #
 except FileNotFoundError:
     print("File does not exist.")
 except IndexError:
