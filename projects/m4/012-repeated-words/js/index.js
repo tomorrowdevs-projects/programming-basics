80e5211ff73203b80cba45dd7d574c9aefeefcff
Eva Lateltin <evalateltin@gmail.com>
2023-01-29 21:15:22 +0100

    fix(m4): improved sol.ex. 012

--- projects/m4/012-repeated-words/js/index.js
+++ projects/m4/012-repeated-words/js/index.js
@@ -1,32 +1,45 @@
-const fs = require("fs");
-const fsPromises = fs.promises;
+const fsPromises = require("fs").promises;
 const path = require("path");
-const input = path.join(__dirname, "input.txt");
+const input = path.join(__dirname, "inputo.txt");
 
 // MAIN FUNCTION
 function findRepeatedWords(inputFile) {
-  fsPromises.readFile(inputFile, "utf8").then((data) => {
-    const lines = data.split("\n");
-    let tempWord = "";
-    let lineNumber = 1;
+  return fsPromises
+    .readFile(inputFile, "utf8")
+    .then((data) => {
+      const repeatedWords = [];
+      const lines = data.split("\n");
+      let tempWord = "";
+      let lineNumber = 1;
 
-    lines.forEach((line) => {
-      const arrayLine = getArray(line);
-      arrayLine.forEach((word) => {
-        if (word === tempWord) {
-          console.log(
-            `The word "${word}" was repeated twice on line ${lineNumber}`
-          );
-        } else {
-          tempWord = word;
-        }
+      lines.forEach((line) => {
+        const arrayLine = getArray(line);
+        arrayLine.forEach((word) => {
+          if (word === tempWord) {
+            const repetition = {};
+            repetition.word = word;
+            repetition.line = lineNumber;
+            repeatedWords.push(repetition);
+          } else {
+            tempWord = word;
+          }
+        });
+        lineNumber++;
       });
-      lineNumber++;
-    });
-  });
+      return repeatedWords;
+    })
+    .catch((err) => Promise.reject(err));
 }
 
-findRepeatedWords(input);
+findRepeatedWords(input)
+  .then((data) => {
+    for (const repetition of data) {
+      console.log(
+        `The word "${repetition.word}" was repeated twice on line ${repetition.line}`
+      );
+    }
+  })
+  .catch((err) => console.log(catchErr(err)));
 
 // HELPER FUNCTIONS
 // Transform a sentence into an array of words, where the punctuation marks at the beginning and at the end of the sentence are removed.
@@ -42,47 +55,9 @@ function getArray(input) {
   });
   return array;
 }
-const fs = require("fs");
-const fsPromises = fs.promises;
-const path = require("path");
-const input = path.join(__dirname, "input.txt");
 
-// MAIN FUNCTION
-function findRepeatedWords(inputFile) {
-  fsPromises.readFile(inputFile, "utf8").then((data) => {
-    const lines = data.split("\n");
-    let tempWord = "";
-    let lineNumber = 1;
-
-    lines.forEach((line) => {
-      const arrayLine = getArray(line);
-      arrayLine.forEach((word) => {
-        if (word === tempWord) {
-          console.log(
-            `The word "${word}" was repeated twice on line ${lineNumber}`
-          );
-        } else {
-          tempWord = word;
-        }
-      });
-      lineNumber++;
-    });
-  });
-}
-
-findRepeatedWords(input);
-
-// HELPER FUNCTIONS
-// Transform a sentence into an array of words, where the punctuation marks at the beginning and at the end of the sentence are removed.
-function getArray(input) {
-  const array = input.toLowerCase().split(/[\r\n]+| /g);
-  array.forEach((element, index) => {
-    array[index] = element
-      .replace(/^[^A-Za-z]+/g, "")
-      .replace(/[^A-Za-z]+$/g, "");
-    if (array[index] === "") {
-      array.splice(index, 1);
-    }
-  });
-  return array;
+function catchErr(err) {
+  const errorPath = err.path.split("/");
+  const notFoundFile = errorPath[errorPath.length - 1];
+  return `Cannot find the file "${notFoundFile}"`;
 }
