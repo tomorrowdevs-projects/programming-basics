dc0aba6528f8fc51956bcb200d76f368fbac7067
Daniele Piani <danielepiani@MBP-di-Daniele.homenet.telecomitalia.it>
2023-01-30 21:37:38 +0100

    M1 - Exercise 4 - Units of time

---
+++ projects/m1/004-units-of-time/python/main.py
@@ -0,0 +1,20 @@
+total_hours = int(input("Enter the amount of hour you've spend: ")) #By convention I take user input as hours
+
+#inizialize hours variable
+days = 0
+hours = total_hours
+minutes = 0
+seconds = 0
+ 
+# convert hours to days:
+days = hours * 0.0416667 #in this step the // round down
+
+# convert hours to minutes:
+minutes = hours * 60 #in this step the
+
+# convert hours to seconds:
+seonds = hours * 3600 #in this step the
+
+# Print the amount of seconds in the hours:
+
+print(minutes)
