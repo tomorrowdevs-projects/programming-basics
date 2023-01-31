868771dfe9683f9838aabae49005a46e272ab866
Alessandro <github25@alessandro-pifferi.com>
2023-01-31 20:04:15 +0100

    feat Update3 (m1): Solution/m1-015-is-it-a-leap-year

--- projects/m1/015-is-it-a-leap-year/js/index.js
+++ projects/m1/015-is-it-a-leap-year/js/index.js
@@ -6,7 +6,7 @@ yearLeapUserIn = parseInt(prompt("Insert the year to be verified"));
 // if the inserted year is divided by 400 or by 4 and has a rest of 0 --> leap year
 if (yearLeapUserIn % 400 == 0) {
   alert("The year inserted is a leap year.");
-  } else if  ((yearLeapUserIn % 100) != 0)  { // else if the year is divisible by 100 and has rest and is different from 0 --> non-leap year 
+  } else if  (!(yearLeapUserIn % 100) == 0)  { // else if the year is divisible by 100 and has rest and is different from 0 --> non-leap year 
   alert("The year inserted is not a leap year.");
 } else if (yearLeapUserIn % 4 == 0) { // if the inserted year is divided by 4 and has a rest of 0 --> leap year
   alert("The year inserted is a leap year.")
