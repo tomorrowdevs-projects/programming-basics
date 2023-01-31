0f16ae647a3fa49382b6f4fa8aa5c139d46b16a2
Alessandro <github25@alessandro-pifferi.com>
2023-01-31 19:46:37 +0100

    feat Update2 (m1): Solution/m1-015-is-it-a-leap-year

--- projects/m1/015-is-it-a-leap-year/js/index_forma_ristretta.js
+++ projects/m1/015-is-it-a-leap-year/js/index_forma_ristretta.js
@@ -3,17 +3,18 @@ let yearLeapUserIn;
 
 yearLeapUserIn = parseInt(prompt("Insert the year to be verified"));
 
-// if the inserted year is divided by 400 or by 4 and has a remainder of 0 --> leap year
-if (yearLeapUserIn % 400 == 0 || yearLeapUserIn % 4 == 0) {
-  alert("The year inserted is a leap year")
-} else if (yearLeapUserIn != 0) { // if the inserted year is divided by 100 it has remainder ( other than 0 ) --> non-leap year
-  alert("The year inserted is not a leap year")
+// if the year is divisible by 400 with rest 0 --> leap year or the year is ( divisible by 100 with rest so it is different from 0 --> 1 ) which is different from zero and the year is divisible by 4 and the rest is 0 --> leap year
+if ((yearLeapUserIn % 400 == 0) || (!(yearLeapUserIn % 100 == 0) && (yearLeapUserIn % 4 == 0))) {
+  alert("The year inserted is a leap year");
+} else { // else it is a leap year
+  alert("The year inserted is not a leap year");
 }
-
-// **************** QUI SOTTO VI É IL RAGIONAMENTO LOGICOCHE HO FATTO LEGGENDO L'ESERCIZIO *****************
-/*let year = parseInt(1584);
+// ********************* RAGIONAMENTO LOGICO CHE HO FATTO LEGGENDO L'ESERCIZIO *****************************
+/*
+let year = parseInt(2066);
 console.log("Year  --->", year);
 console.log("Leap year         -->  rest 0  (NO  rest) --->  ", "Division", year / 400, " Rest", year % 400);
 console.log("Not a leap year.  -->  rest 1  (YES rest) --->  ", "Division", year / 100, "Rest", year % 100);
-console.log("Leap year         -->  rest 0  (NO  rest) --->  ", "Division", year / 4, "  Rest", year % 4);*/
-//***********************************************************************************************************
+console.log("Leap year         -->  rest 0  (NO  rest) --->  ", "Division", year / 4, "  Rest", year % 4);
+*/
+//**********************************************************************************************************
