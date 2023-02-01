5d93e45d9d4d8625fdf7d0b8205426524522bca3
Daniele Piani <danielepiani@MBP-di-Daniele.homenet.telecomitalia.it>
2023-02-01 19:12:59 +0100

    M1 - Exercise 007Day old bread, done

--- projects/m1/007-day-old-bread/python/main.py
+++ projects/m1/007-day-old-bread/python/main.py
@@ -3,6 +3,6 @@ bread_regular_cost = 3.49 * total_bread  #here i calculate the normal price of t
 old_bread_cost = (3.49 * 0.60) * total_bread #Here I calculate the price of the discounted bread
 total_price = (3.49 * 0.60) * total_bread + 0  #here there is the sum of discounted bread + 0 (the number of not discounted bread)
 #inizialize hours variable
-print(f"The regular price is {bread_regular_cost:.2f}€") #:.2f helps me to display only 2 digits after the comma
+print(f"The regular price is {bread_regular_cost:.2f}€") #:.2f helps me to display only 2 digits after the comma.
 print(f"The discounted price is {old_bread_cost:.2f}€ ")
 print(f"The total you pay now is {total_price:.2f}€")
