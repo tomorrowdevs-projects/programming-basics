001020cd3873ea6ab329f7277bf2c78b09372d52
ddecataldo <davidedecataldo92@gmail.com>
2023-02-01 14:48:24 +0100

    exercise 42 complete

---
+++ projects/m1/042-arbitrary-base-conversions/js/index.js
@@ -0,0 +1,39 @@
+const numberUser = prompt("Enter a number").toUpperCase();
+const baseNumber = prompt("Enter the base of the entered number (it only accepts values between 2 and 16)");
+const baseConvertNumber = prompt("Enter the base in which you want to convert the number (it only accepts values between 2 and 16)");
+
+const convertNumberUserBase10 = convertNumberBase10(numberUser, baseNumber, baseConvertNumber);
+const convertNumberUserBaseArbitrary = convertNumberBaseArbitrary(numberUser, baseNumber, baseConvertNumber);
+
+let message = `The number ${numberUser} written in base ${baseNumber} corresponds to ${baseConvertNumber === "10" ? convertNumberUserBase10 : convertNumberUserBaseArbitrary}`;
+
+if(!convertNumberUserBase10 || !convertNumberUserBaseArbitrary){
+  alert("The entered values are not valid");
+} else {
+  alert(message);
+}
+
+
+function convertNumberBase10(number, base){
+
+  number = parseInt(number, base).toString(10).toUpperCase();
+
+  if(base < 2 || base > 16){
+    return false;
+  }
+
+  return number;
+
+}
+
+function convertNumberBaseArbitrary(number, base, baseConvertNumber){
+
+  number = parseInt(number, base).toString(baseConvertNumber).toUpperCase();
+
+  if((base < 2 || base > 16) || ((baseConvertNumber < 2 || baseConvertNumber > 16))){
+    return false;
+  }
+
+  return number;
+
+}
