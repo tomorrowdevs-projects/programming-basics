b51b5da2afeeecead03e74fc9f4f08c651ee0c86
Alessia-C <caporasoalessia1611@gmail.com>
2023-02-01 18:22:22 +0100

    fix(m1): solutions 041-hexadecimal-and-decimal-digits

--- projects/m1/041-hexadecimal-and-decimal-digits/js/index.js
+++ projects/m1/041-hexadecimal-and-decimal-digits/js/index.js
@@ -1,17 +1,17 @@
 // convert a string containing a single hexadecimal digit to a base 10 integer
-function hex2int(string) {
-  const convertSting = "0x" + string;
-  alert("0x" + string + ": " + parseInt(convertSting));
+function hex2int(string){
+  const convertSting = '0x' + string
+  return ('0x' + string + ': ' + parseInt(convertSting))
 }
 // convert integer to a single hexadecimal digit
-function int2hex(number) {
-  if (number >= 0 && number <= 15) {
-    const convertInteger = number.toString(16);
-    alert(number + ": " + convertInteger);
+function int2hex(number){
+  if(number >= 0 && number <= 15){
+  const convertInteger = number.toString(16)
+  return (number + ': ' + convertInteger)
   } else {
-    // Display an error message if the parameter’s value is outside of the range.
-    alert("Invalid value");
+  // Display an error message if the parameter’s value is outside of the range.
+  alert('Invalid value')
   }
 }
-hex2int(prompt("Enter letter between A and F"));
-int2hex(parseInt(prompt("Enter number between 0 and 15")));
+console.log(`${hex2int(prompt('Enter letter between A and F'))}`)
+console.log(`${int2hex(parseInt(prompt('Enter number between 0 and 15')))}`)
