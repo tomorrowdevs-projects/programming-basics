cc00c69a974f03c69b21a3b6fd76a8fe7ece5cd9
Alessia-C <caporasoalessia1611@gmail.com>
2023-02-01 17:50:17 +0100

    fix(m1): solutions 030-maximum-integer

--- projects/m1/030-maximum-integer/solutions/alessiacaporaso/maximum-integer.js
+++ projects/m1/030-maximum-integer/solutions/alessiacaporaso/maximum-integer.js
@@ -1,13 +1,12 @@
 // Create a program that begins by selecting a random integer between 1 and 100.
 // After the initial integer has been selected, generate 99 additional random integers between 1 and 100.
 // After you have displayed 100 integers your program should display the maximum value encountered, along with the number of times the maximum value was updated during the process.
-let casualNumber;
 let collectionNumber = [];
 let maximumValue = 0;
 let changeValue = 0;
 
 while (collectionNumber.length <= 99) {
-  casualNumber = Math.floor(Math.random() * 100 + 1); //generate random integer
+  const casualNumber = Math.floor(Math.random() * 100 + 1); //generate random integer
   collectionNumber.push(casualNumber);
   for (let value of collectionNumber) { // check element in the array
     if (value > maximumValue) {
