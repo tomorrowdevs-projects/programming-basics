ca1fc55af8c42902506942c23d3565584e4c67e0
SteMar99 <stefanomarchioni@live.it>
2023-02-03 23:17:23 +0100

    Esecuzione primo esercizio JS

---
+++ projects/m1/001-area-of-a-room/js/index.js
@@ -0,0 +1,5 @@
+let larghezza = prompt("quanti metri è larga la stanza ?", 5.50)
+let lunghezza = prompt("quanti metri è lunga la stanza ?", 5.50)
+let areastanza = (larghezza * lunghezza)
+
+alert(`la tua stanza è larga ${areastanza} m²`);
