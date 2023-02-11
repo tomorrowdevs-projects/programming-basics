56c2f48791d565ed1e284f4656730601f7a9d16e
gabriele <gabrielesagnelli@hotmail.it>
2023-02-11 15:38:12 +0100

    solution/M1-024-fizz-buzz

---
+++ projects/m1/024-fizz-buzz/js/index.js
@@ -0,0 +1,17 @@
+//fizz-buzz
+
+let number ;
+
+for (let i = 1; i <= 100; i++) {
+number = +prompt('what number do you say?');
+  
+  if (number % 3 === 0 && number % 5 === 0) {
+    alert('Fizz Buzz!');
+  } else if (number % 3 === 0) {
+    alert('Fizz!');
+  } else if (number % 5 === 0) {
+    alert('Buzz!');
+  } else {
+    alert(number);
+  }
+}
