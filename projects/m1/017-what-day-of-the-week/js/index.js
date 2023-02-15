82aa6ed9dd853ffd249e9e1d11ba3bed8415353e
Alessandro <github25@alessandro-pifferi.com>
2023-02-15 17:09:40 +0100

    feat 2 (m1): Solution/m1-017-what-day-of-the-week - add function print

--- projects/m1/017-what-day-of-the-week/js/index.js
+++ projects/m1/017-what-day-of-the-week/js/index.js
@@ -2,25 +2,33 @@
 //const prompt = require("prompt-sync")();
 // *******Costant prompt Syn *******************/
 
-let year = parseInt(prompt("Please insert the date --> "));
+let year = parseInt(prompt("Please insert the date"));
 
-//variable declaration with formula
-let day_of_the_week = ((year + Math.floor((year - 1) / 4) - Math.floor((year - 1) / 100) + Math.floor((year - 1) / 400)) % 7);
+let nameDay=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-switch (day_of_the_week) {
-    case 0: { console.log("01-01-" + year + " is a Sunday" + " -->  Formula result: -->  " + day_of_the_week); }
-        break;
-    case 1: { console.log("01-01-" + year + " is a Monday" + " -->  Formula result: -->  " + day_of_the_week); }
-        break;
-    case 2: { console.log("01-01-" + year + " is a Tuesday" + " -->  Formula result: -->  " + day_of_the_week); }
-        break;
-    case 3: { console.log("01-01-" + year + " is a Wednesday" + " -->  Formula result: -->  " + day_of_the_week); }
-        break;
-    case 4: { console.log("01-01-" + year + " is a Thursday" + " -->  Formula result: -->  " + day_of_the_week); }
-        break;
-    case 5: { console.log("01-01-" + year + " is a Friday" + " -->  Formula result: -->  " + day_of_the_week); }
-        break;
-    case 6: { console.log("01-01-" + year + " is a Saturday" + " -->  Formula result: -->  " + day_of_the_week); }
-        break;
-    default: console.log(`ERROR`);
+// Date print function
+function printDayOfTheWeek(year_Print,nameDayPrint,dayOfTheWeek_print) {
+console.log("01-01-"+ year_Print +" is "+ nameDayPrint +" -->  Formula result: -->  " + dayOfTheWeek_print);
+  return printDayOfTheWeek;
+}
+
+// Variable declaration with formula
+let day_of_the_week= ((year + Math.floor((year - 1) / 4) - Math.floor((year - 1) / 100) + Math.floor((year - 1) / 400)) % 7);
+
+switch(day_of_the_week) {
+   case 0: { printDayOfTheWeek(year,nameDay[0],day_of_the_week);}
+    break;
+   case 1: { printDayOfTheWeek(year,nameDay[1],day_of_the_week);}
+    break;
+   case 2: { printDayOfTheWeek(year,nameDay[2],day_of_the_week);}
+    break;
+   case 3: { printDayOfTheWeek(year,nameDay[3],day_of_the_week);}
+    break;
+   case 4: { printDayOfTheWeek(year,nameDay[4],day_of_the_week);}
+    break;
+   case 5: { printDayOfTheWeek(year,nameDay[5],day_of_the_week);}
+    break;
+   case 6: { printDayOfTheWeek(year,nameDay[6],day_of_the_week);}
+    break;
+   default: console.log(`ERROR`);
 }
