4213c23e4330b12afd2ff1871abe62aa619bf995
Alessandro <github25@alessandro-pifferi.com>
2023-02-25 00:24:01 +0100

    feat update-3 (m1): Solution/m1-018-roulette-payouts

--- projects/m1/018-roulette-payouts/js/index.js
+++ projects/m1/018-roulette-payouts/js/index.js
@@ -1,59 +1,75 @@
 // Array containing the numbers of the red areas of the roulette wheel
-const areasReadGreen = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+const areasRead = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
 
-// Insert Arrey areas grenn in to Arrey Areas Red for case "0 and case "00
-areasReadGreen.push(["00", "0"]);
-
-// Array containing the numbers of the Roulette payout range from 1 to 18
+// Array containing the numbers of one of the two payment ranges --> 1 to 18 of the roulette wheel
 const intervalPayment = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 
-// Variable declaration of type Number
-let valueOddEven = 0, searchAreasRed = 0, searchIntervalPayment = 0;
+// Array containing the numbers of one of the two payment ranges --> 1 to 18 of the roulette wheel
+const pay0Pay00_PayInterval = ["0", "00", "1-18", "19-36"];
 
-// Variable declaration of type String
+// Variable declarations
+let numbersRandom, i;
+
+// String variable declaration
 let roulette_PayOutsResul = "";
 
 // Random number generator " Math.random() " to simulate the spin of the American Rulette wheel 
-let nambersRandom = Math.floor(Math.random() * (36 + 1 + 1));
+numbersRandom = Math.floor(Math.random() * 37);
 
 // Function for searching an element of an Array
-function searchElementsArrayRedGreen(arrayReadGreen, numberSearch) { for (let i = 1; i < (arrayReadGreen.length - 1); i++) { if (arrayReadGreen[i] === numberSearch) { return true; } } }
+function arraySearchComparingNumbers(arraySeekElement, numberSearch) {
+    for (i = 0; i < arraySeekElement.length; i++) {
+        if (arraySeekElement[i] === numberSearch) { return true; }
+    }
+}
 
-// Function for searching an element of an Array
-function searchElementsArray(contentArray, numberSearch) { for (let i = 1; i < contentArray.length; i++) { if (contentArray[i] === numberSearch) { return true; } } }
-//  Conditional if statement to establish whether the generated numbers are even or odd 
-if (nambersRandom % 2) { valueOddEven = false; }
-else { valueOddEven = true; }
+// Function to establish whether a number or equal or odd.
+function OddEven(namberCheck) {
+    if (namberCheck % 2) { return false; }
+    else { return true; }
+}
 
-// Call to the function for elements in an array for Red areas.
-searchAreasRed = searchElementsArrayRedGreen(areasReadGreen, nambersRandom);
+//  Function to establish what are the red areas of American roulette.
+let arraySeekToCompareRedAreas = arraySearchComparingNumbers(areasRead, numbersRandom);
 
-// Call to the function to search for elements in an array for the payout interval of the American Rule.
-searchIntervalPayment = searchElementsArray(intervalPayment, nambersRandom);
+// Function to establish the payment interval of American roulette.
+let arrayintervalPayment = arraySearchComparingNumbers(intervalPayment, numbersRandom);
+
+// Function for verification of equal or odd gods.
+let checkNumberOddEven = OddEven(numbersRandom);
 
 // Conditional if statement to display whether PAYS 00 or PAYS 0 
-if (nambersRandom === 37) { roulette_PayOutsResul = "Pay Area Green - Spin --> " + areasReadGreen[18][0]; }
-else if (nambersRandom === 0) { roulette_PayOutsResul += "Pay Area Green - Spin --> " + areasReadGreen[18][1]; }
-else {
-    // Conditional if stacktement to display SPIN and PAY NUMBER
-    if (searchAreasRed === true) {
-        roulette_PayOutsResul += "The spin resulted in --> " + nambersRandom + "\nPay                  --> " + nambersRandom + "\nPay                  --> Area Red";
+if (numbersRandom === 37) {
+    roulette_PayOutsResul = ("Pay  Areas Grenn -->    " + pay0Pay00_PayInterval[1]);
+} else if (numbersRandom === 0) {
+    roulette_PayOutsResul += ("Pay  Areas Grenn -->    " + pay0Pay00_PayInterval[0]);
+} else {
+
+    // Conditional if statement to display SPIN and PAY NUMBER
+    if (arraySeekToCompareRedAreas === true) {
+        roulette_PayOutsResul += "The spin resulted in --> " + numbersRandom + "\nPay                  --> " + numbersRandom + "\nPay                  --> Area Red\n";
     } else {
-        roulette_PayOutsResul += "The spin resulted in --> " + nambersRandom + "\nPay                  --> " + nambersRandom + "\nPay                  --> Area Black";
+        roulette_PayOutsResul += "The spin resulted in --> " + numbersRandom + "\nPay                  --> " + numbersRandom + "\nPay                  --> Area Black\n";
     }
+
     // Conditional statement if to display if the extracted number is EVEN or ODD
-    if (valueOddEven === true) {
-        roulette_PayOutsResul += "\nPay                  --> Number Even";
+    if (checkNumberOddEven /*=== true*/) {
+        roulette_PayOutsResul += "Pay                  --> Number Even\n";
     } else {
-        roulette_PayOutsResul += "\nPay                  --> Number Odd";
+        roulette_PayOutsResul += "Pay                  --> Number Odd\n";
     }
+
     // Conditional statement if to display the RANGE of NUMBERS that have to PAY 
-    if (searchIntervalPayment === true) {
-        roulette_PayOutsResul += "\nPay from             -->  1 to 18";
+    // according to the number that came out of the spin of the Roulette wheel
+    if (arrayintervalPayment/* === true*/) {
+        roulette_PayOutsResul += "Pay Interval         -->  " + pay0Pay00_PayInterval[2] + "\n";
     } else {
-        roulette_PayOutsResul += "\nPay from             --> 19 to 36";
+        roulette_PayOutsResul += "Pay Interval         --> " + pay0Pay00_PayInterval[3] + "\n"
     }
 }
 
 // Final printout of the results stored in the variables
-console.log(roulette_PayOutsResul);
+console.log("\n" + roulette_PayOutsResul);
+
+//Check Areas Red
+console.log("Areas Red --> " + areasRead);
