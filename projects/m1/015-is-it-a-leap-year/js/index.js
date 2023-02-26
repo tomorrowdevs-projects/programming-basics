e2cd8acdd190d8e6c7a002b34bbdf25ad37fd3f2
Alessandro <github25@alessandro-pifferi.com>
2023-02-26 12:37:31 +0100

    feat update-4  (m1): Solution/m1-015-is-it-a-leap-year -  unique variable 'resultYearsLeep' console.log

--- projects/m1/015-is-it-a-leap-year/js/index.js
+++ projects/m1/015-is-it-a-leap-year/js/index.js
@@ -1,25 +1,18 @@
 //variable declaration
-let yearLeapUserIn;
+let yearLeapUserIn=0;
+let resultYearsLeep="";
 
 yearLeapUserIn = parseInt(prompt("Insert the year to be verified"));
 
 // if the inserted year is divided by 400 or by 4 and has a rest of 0 --> leap year
 if (yearLeapUserIn % 400 === Number()) {
-  alert("The year inserted is a leap year");
-  } else if ((yearLeapUserIn % 100) === Number())  { // else if the year is divisible by 100 and has rest and is different from 0 --> non-leap year 
-  alert("The year inserted is not a leap year");
-} else if (yearLeapUserIn % 4 === Number()) { // if the inserted year is divided by 4 and has a rest of 0 --> leap year
-  alert("The year inserted is a leap year")
+  resultYearsLeep = "\nThe " + yearLeapUserIn + " is a leap year\n";
+} else if ((yearLeapUserIn % 100) === Number()) {  
+  resultYearsLeep += "\nThe " + yearLeapUserIn + " is not a leap year\n";
+} else if (yearLeapUserIn % 4 === Number()) { 
+  resultYearsLeep += "\nThe " + yearLeapUserIn + " is a leap year\n";
 } else {
-    alert("The year inserted is not a leap year.");
+  resultYearsLeep += "\nThe " + yearLeapUserIn + " is not a leap year\n";
 }
 
-// ********************* RAGIONAMENTO LOGICO CHE HO FATTO LEGGENDO L'ESERCIZIO *****************************
-/*
-let year = parseInt(2066);
-console.log("Year  --->", year);
-console.log("Leap year         -->  rest 0  (NO  rest) --->  ", "Division", year / 400, " Rest", year % 400);
-console.log("Not a leap year.  -->  rest 1  (YES rest) --->  ", "Division", year / 100, "Rest", year % 100);
-console.log("Leap year         -->  rest 0  (NO  rest) --->  ", "Division", year / 4, "  Rest", year % 4);
-*/
-//**********************************************************************************************************
+console.log(resultYearsLeep);
