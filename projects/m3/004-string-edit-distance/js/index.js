ae9537c64f3d15ce5ec7b7f59c4fb130d441a371
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-02-27 11:40:47 +0100

    fix(m3-004): excessive vertical spacing eliminated.

--- projects/m3/004-string-edit-distance/js/index.js
+++ projects/m3/004-string-edit-distance/js/index.js
@@ -1,7 +1,5 @@
-
 let s = "sitting";
 let t = "kitten";
-
 
 function getEditDistance(s, t) {
 // If one of the two string is empty, return the whole length of the other string.
@@ -9,7 +7,6 @@ function getEditDistance(s, t) {
     if (t.length === 0) return s.length;
 
     let cost = 0;
-
 
 // Setting d1, d2 and d3 as the exercise description said if the last character fo the two strings isn't the same.
     if (s[s.length - 1] !== t[t.length - 1]) {
@@ -19,17 +16,12 @@ function getEditDistance(s, t) {
         const d2 = getEditDistance(s, t.slice(0, -1)) + 1;
         const d3 = getEditDistance(s.slice(0, -1), t.slice(0, -1)) + cost;
 
-
 // Using the Math.min method to return the minimum of d1, d2, d3.
         return Math.min(d1, d2, d3);
 
     };
 
-
     return getEditDistance(s.slice(0, -1), t.slice(0, -1));
-
-
 };    
-
 
 console.log(getEditDistance(s, t));
