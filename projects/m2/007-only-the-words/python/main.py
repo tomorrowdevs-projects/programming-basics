06185efea40a6b45a8e5eb2612deebda99805396
Raffina92 <raffaellas92@gmail.com>
2023-02-27 10:20:35 +0100

    Exercise 007 of m2 solved with .split()

--- projects/m2/007-only-the-words/python/main.py
+++ projects/m2/007-only-the-words/python/main.py
@@ -5,33 +5,20 @@ def remove_punctuation (string: str) -> list:
 
     punctuation = ['.',',',':',';',"'",'!','?','-','_',' ','’',' ','"','”','“']
     
-    #first_index is the index of the first character of the word will be extrapolated from 'string'
-    first_index=0
+    words = string.split(" ")
 
-    #last_index is the index of the last char of the word will be extrapolated from 'string
-    last_index=0
+    punctuation_free_words = []
 
-    
-    words = []
-    
-    for i in range(len(string)):
-        # If the string[i] isn't a punctuation or it's an apostrophe but was preceded by a char != punctation 
-        # then last_index will increment by 1
-        if string[i] not in punctuation or ((string[i]=="'" or string[i] == "’")and not was_punctation):
-            was_punctation = False
-            last_index += 1
-        else:
-            if was_punctation:
-                #if string[i] is a punctation and it was preceded by a punctation, skip it incremented first_index and last_index
-                first_index = i + 1
-                last_index = first_index
-            else:
-                words.append(string[first_index:last_index])
-                first_index = i + 1
-                last_index = first_index
-                was_punctation = True
-    return words
-
-string = input("Insert a string. I'll show every word without punctation ")
-print(remove_punctuation(string))
+    for string in words:
+        if string[0] in punctuation:
+            string = string[1:]
+        if string[-1] in punctuation:
+            string = string[:-1]
+        punctuation_free_words.append(string)
+        
+    return punctuation_free_words
+        
+if __name__=="__main__":
+    string = input("Insert a string. I'll show every word without punctation ")
+    print(remove_punctuation(string))
 
