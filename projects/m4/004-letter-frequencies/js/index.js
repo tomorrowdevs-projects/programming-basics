9269bbc720e6fb68de0498fb5b827cf3d259b1bd
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-02-27 11:54:28 +0100

    fix(m4-004): excessive vertical spacing eliminated.

--- projects/m4/004-letter-frequencies/js/index.js
+++ projects/m4/004-letter-frequencies/js/index.js
@@ -2,20 +2,15 @@ const fs = require('fs');
 const readFile = require('./file_read');
 const prompt = require(`prompt-sync`)();
 
-
-
 const letterFrequencies = () => {
 
     const fileName = prompt(`Insert the name of a file, to see the frequencies of all of it's letters: `);
     
     readFile(fileName)
     .then((data) =>  {
-
         return data.toLowerCase().replace(/\W|_/g, '');
-
     })
     .then((string) => {
-
         const freq = {};
         
         for (i = 0; i < string.length; i++) {
@@ -24,9 +19,7 @@ const letterFrequencies = () => {
             else freq[character] = 1;
             };
         
-        
         console.log(`LETTER FREQUENCIES =>`, Object.entries(freq).sort());
-
     })
     .catch((err) => console.error(err));   
 };
