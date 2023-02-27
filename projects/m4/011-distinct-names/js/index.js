ba0fdb0cbe6bfa2baeea4d39fd8c764f958b0357
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-02-27 12:12:35 +0100

    fix(m4-011): excessive vertical spacing eliminated.

--- projects/m4/011-distinct-names/js/index.js
+++ projects/m4/011-distinct-names/js/index.js
@@ -2,45 +2,30 @@ const fs = require(`fs`);
 const readDir = require(`./read_directory.js`);
 const { info } = require("console");
 
-
-
 readDir()
 .then((files) => {
-
-    
     const babiesData = getBabiesData(files); 
     return babiesData;
 })
 .then((babiesData) => {
-
    const uniqueNames = getUniqueNames(babiesData);
    return uniqueNames;
-
 })
 .then((uniqueNames) => console.log(`The male names contained in the data set are: ${uniqueNames.maleNames.join(`, `)}\nThe female names contained in the data set are ${uniqueNames.femaleNames.join(`, `)}`))
 .catch((err) => console.log(err));
-
-
-
-
-
 
 const getBabiesData = filesArray => {
 
     const allNames = [];
 
     filesArray.forEach((file) => {
-    
         const data = (fs.readFileSync(`./dataset/${file}`, {encoding:'utf8', flag:'r'}));
-
         text = data.replace(/\r/g, "").split(`\n`);
         allNames.push(text)
     })
 
     return allNames.flat();
-
 };
-
 
 
 const getUniqueNames = babiesData => {
@@ -51,7 +36,6 @@ const getUniqueNames = babiesData => {
     };
 
     babiesData.forEach((baby) => {
-        
         baby = baby.split(`,`);
         
         if (!babiesNames.maleNames.includes(`${baby[0]}`) && baby[1] == `M`) babiesNames.maleNames.push(baby[0]);
--- projects/m4/011-distinct-names/js/read_directory.js
+++ projects/m4/011-distinct-names/js/read_directory.js
@@ -3,15 +3,12 @@ const fs = require('fs');
 const readDir = () => {
 
         return new Promise ((resolve, reject) => {
-
             fs.readdir (`./dataset`, 'utf-8', (err, files) => {
 
                 if (err) reject (`ERROR: The requested directory does not exist!`);
                 resolve (files);
             });
-
         });
-
 };
 
 module.exports = readDir;
