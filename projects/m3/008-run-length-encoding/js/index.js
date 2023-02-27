b9decc1eeb5efacf23b9eaeb1e838af6a51ea5f7
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-02-27 11:45:17 +0100

    fix(m3-008): excessive vertical spacing eliminated.

--- projects/m3/008-run-length-encoding/js/index.js
+++ projects/m3/008-run-length-encoding/js/index.js
@@ -1,8 +1,5 @@
-
 const prompt = require("prompt-sync")();
-
 const list = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B","B","A","A","A","A","A","A","B"];
-
 
 function getList () {
 
@@ -12,7 +9,6 @@ function getList () {
     let input = `.`;
 
     const onlyLetters = /^[a-zA-Z]+$/;
-
 
 // Declaring a while loop to create an array of elements for the encoding, by using a regex to test the user's input and push it to an array.
         while (input !== "") {
@@ -30,21 +26,17 @@ function getList () {
     return list;
 };
 
-
 function encodeList (list, counter = 0) {
-
 // Setting up base case, when function is on last item on list, we push counter to  the list and return it.
     if ((list.length === 1)) {
         list.push(counter + 1);
         return list;
     };
 
-
 // If two consecutive elements are equal, we slice the second from the list and add 1 to the counter.
     if (list[0] === list[1]) {
         return encodeList(list.slice(1), counter += 1);
     };
-
 
 //  When two near elements in the list are different we push the counter in the list and recursively recall it on the next item.
     if (list[0] !== list[1]){
@@ -52,7 +44,6 @@ function encodeList (list, counter = 0) {
         let l2 = encodeList(list.slice(1));
         return l1.concat(l2);
     };
-
 
     return list;
 
