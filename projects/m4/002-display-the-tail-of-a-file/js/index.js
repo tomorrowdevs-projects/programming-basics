d3803d6375ec6c17e3f28f6e88a5759a31f7e3a8
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-02-27 11:46:33 +0100

    fix(m4-002): excessive vertical spacing eliminated.

--- projects/m4/002-display-the-tail-of-a-file/js/index.js
+++ projects/m4/002-display-the-tail-of-a-file/js/index.js
@@ -5,7 +5,6 @@ const command  = prompt(`Enter your command (tail): `)
 const fileName = prompt(`Please, enter the name of the file to open: `)
 
 const getFile = (fileName) => {
-
     return new Promise  ((resolve, reject) => {
 
         fs.readFile (fileName, 'utf-8', function read(err, data) {
@@ -13,9 +12,7 @@ const getFile = (fileName) => {
             reject (`Your file doesn't exist!`)
         });
     });
-    
 };
-
 
 getFile(fileName)
 .then((data) => {
