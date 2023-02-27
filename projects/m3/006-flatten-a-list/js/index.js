7c83b391cf35354236289667fbd1cf99b7eff57a
salvatorequagliariello <salvatorequagliariello0@gmail.com>
2023-02-27 11:43:18 +0100

    fix(m3-006): excessive vertical spacing eliminated.

--- projects/m3/006-flatten-a-list/js/index.js
+++ projects/m3/006-flatten-a-list/js/index.js
@@ -1,15 +1,8 @@
-
-
 const nestedArray = [1, [2, 3], [4, [5, [6, 7]]], [[[8], 9], [10]]];
 
-
-
 function getFlattenArray (array) {
-
-
 // If the arguments of the fucntion is an array and it's empty return the array.
     if (Array.isArray(array) && array.length == 0 ) return array;
-
 
 // Declaring two conditions as the track said.
     if (Array.isArray(array[0])){
@@ -25,7 +18,5 @@ function getFlattenArray (array) {
     };
 
 };
-
-
 
 console.log(getFlattenArray(nestedArray));
