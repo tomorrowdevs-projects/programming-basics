c489e0728be5949ea8f4322f0c8e0462c637223c
stefandhub <stefandifede@gmail.com>
2023-03-01 10:02:48 +0100

    Solution M1-032-taxi-fare

--- projects/m1/032-taxi-fare/solutions/stefandifede/taxi-fare.js
+++ projects/m1/032-taxi-fare/solutions/stefandifede/taxi-fare.js
@@ -1,4 +1,4 @@
-const distance = prompt("Insert the distance travelled to calculate the fare in kilometers")
+const distance = prompt("Insert the distance travelled in kilometers to calculate the fare")
 
 function totalFee() {
     const additionalFee = (distance * 1000) / 140 * 0.25
