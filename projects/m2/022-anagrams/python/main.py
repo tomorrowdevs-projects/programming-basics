00ff5f3650479f29c55657697e6e67d442c8227f
Raffina92 <raffaellas92@gmail.com>
2023-03-02 15:25:57 +0100

    Exercise 022 of m2 solved

---
+++ projects/m2/022-anagrams/python/main.py
@@ -0,0 +1,13 @@
+string_1 = input("First string: ")
+string_2 = input("Second string: ")
+
+list_1 = list(string_1)
+list_2 = list(string_2)
+
+list_1.sort()
+list_2.sort()
+
+if list_1 == list_2 :
+    print("There are anagrams")
+else:
+    print("There aren't anagrams")
