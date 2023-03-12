b25f3a5ec214992209d1a7872fe8ecef3da0ed09
Silvio2008 <77686111+Silvio2008@users.noreply.github.com>
2023-03-12 21:45:24 +0100

    GIOVARRUSCIO - ESERCIZIO 1

---
+++ projects/m1/001-area-of-a-room/solutions/AREA.py
@@ -0,0 +1,7 @@
+# Questo programma permette di calcolare l'area in mq
+# di una stanza inserendo la lunghezza e la larghezza in m
+
+
+w = float(input("Inserire la lunghezza: m "))
+h = float(input("Inserire la lerghezza: m "))
+print (f"L'area della stanza è : mq {w*h}")
