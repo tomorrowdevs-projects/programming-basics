5349f942eae1c7909164aa87915e6e818dc05552
Gfazio57 <127107662+Gfazio57@users.noreply.github.com>
2023-03-14 20:23:04 +0100

    Add files via upload
    file M001

---
+++ giuseppe Fazio/main.py
@@ -0,0 +1,6 @@
+# esercizio sul calcolo dell'area di un rettangolo
+print ("Cacola area di un rettangolo")
+print ("inserisci la base")
+b= int(input ("inserisci il  valore della base: "))
+h= int(input ("inserisci un valore dell'altezza: "))
+print (" area della stanza =   {b*h}" )
