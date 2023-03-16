12c5b62393318cca5d31d8547e8259b99d5e67fd
Francesco Marino <fra070@gmail.com>
2023-03-16 15:04:42 +0100

    refactor with ES6 syntax

--- projects/m4/012-repeated-words/js/custom-utils-class.js
+++ projects/m4/012-repeated-words/js/custom-utils-class.js
@@ -1,3 +1,7 @@
+// Modules
+const readline = require('readline');
+const fs = require('fs');
+
 class CustomUtils {
     /**
      * Check the existance of required args
@@ -16,6 +20,65 @@ class CustomUtils {
             return false;
         }
         return true;
+    }
+
+    /**
+     * Parse a file and search repeated words
+     * @param {string} filePath 
+     * @returns {object} - An object with n line as key, array of repeated word as values
+     */
+    searchRepeatedWords(filePath) {
+
+        return new Promise( (resolve, reject) => {
+            const output = {};
+            let prevRow = [];
+            let row = 1;
+    
+            const stream = readline.createInterface({
+                input: fs.createReadStream(filePath),
+            });
+    
+            stream.on('line', line => {
+                // Format current line as array of word
+                const wordsInRow = line.toLowerCase().split(' ').map( word => word.replace(/[^a-z|0-9]/gi, '')).filter( el => el != '');
+    
+                // Check repeated words
+                const repeatedWords = [];
+    
+                wordsInRow.forEach( (word, wordIndex, wordList) => {
+                    // If i'm controlling the first word of the row
+                    // and there are words in the previous row
+                    // and the last word of the previous row is the same of the first word of the current row
+                    // then it is a repeated word between the two lines
+                    if(
+                        wordIndex === 0 
+                        && prevRow.length > 0 
+                        && prevRow[prevRow.length - 1] === word
+                    ) {
+                        output.hasOwnProperty(row - 1) ? output[row - 1].push(word) : output[row - 1] = [word];
+                    }
+    
+                    // If word is the same of the previous word (row[wordIndex - 1])
+                    // then it is a repeated word
+                    if(word === wordList[wordIndex - 1] && !repeatedWords.includes(word)) {
+                        repeatedWords.push(word);
+                    }
+                });
+    
+                // If repeated words are found, add them to the output obj
+                if(repeatedWords.length > 0) output[row] = repeatedWords;
+    
+                // Save current row as prevRow for next iteration
+                prevRow = wordsInRow;
+    
+                // Increase row counter
+                row++;
+            });
+    
+            stream.on('close', () => resolve(output));
+            stream.on('error', err => reject(err));
+        });
+        
     }
 }
 
--- projects/m4/012-repeated-words/js/index.js
+++ projects/m4/012-repeated-words/js/index.js
@@ -8,9 +8,6 @@ const inputExists = customUtils.checkInputExistance({
 
 if(!inputExists) return;
 
-// Modules
-const readline = require('readline');
-const fs = require('fs');
 
 // Global vars
 const filePath = process.argv[2];
@@ -21,58 +18,12 @@ main();
 
 /* FUNCTIONS */
 async function main() {
-    const repeatedWordsList = await parseFile(filePath);
-    repeatedWordsPrinter(repeatedWordsList);
-}
+    // Parse the file and searches repeated words
+    const repeatedWords = await customUtils.searchRepeatedWords(filePath);
 
-function parseFile(filePath) {
-    return new Promise( (resolve, reject) => {
-        const output = {};
-        let prevRow = [];
-        let row = 1;
-
-        const stream = readline.createInterface({
-            input: fs.createReadStream(filePath),
-        });
-
-        stream.on('line', line => {
-            // Format current line as array of word
-            const a = line.toLowerCase().split(' ').map( w => w.replace(/[^a-z|0-9]/gi, '')).filter( e => e != '');
-
-            // Check repeated words
-            const repeatedWords = [];
-            for(let i = 0; i < a.length - 1; i++) {
-                // Check the first word with the last word of the previous row
-                const repeatedBetweenLines = i == 0 && prevRow.length > 0 && prevRow[prevRow.length - 1] === a[i];
-
-                if(repeatedBetweenLines) {
-                    output[row - 1].push(a[i]);
-                }
-
-                // Check repeated words in line
-                if(a[i] === a[i + 1] && !repeatedWords.includes(a[i]) || repeatedBetweenLines) {
-                    repeatedWords.push(a[i]);
-                }
-            }
-
-            // If repeated words are found, add them to the output obj
-            if(repeatedWords.length > 0) output[row] = repeatedWords;
-
-            // Save current row as prevRow for next iteration
-            prevRow = a;
-
-            // Increase row counter
-            row++;
-        });
-
-        stream.on('close', () => resolve(output));
-        stream.on('error', err => reject(err));
+    // Print the results
+    Object.keys(repeatedWords).forEach( (nRow, i) => {
+        if(i === 0) console.log('Repeated words:');
+        console.log(`Line ${nRow}: ${repeatedWords[nRow].join(', ')}`);
     });
-}
-
-function repeatedWordsPrinter(repeatedWords) {
-    for(const row in repeatedWords) {
-        if(row == '1') console.log('Repeated words:');
-        if(repeatedWords[row].length > 0) console.log(`Line ${row}: ${repeatedWords[row].join(', ')}`);
-    }
 }
