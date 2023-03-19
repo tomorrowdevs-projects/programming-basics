a0f70f3aba0077ceb4f766dc6e99be9db8f3a155
MLCorsi <127219366+MLCorsi@users.noreply.github.com>
2023-03-19 12:12:44 +0100

    Update MLCorsiM005.py

--- projects/m1/005-units-of-time-again/solutions/MLCorsiM005.py
+++ projects/m1/005-units-of-time-again/solutions/MLCorsiM005.py
@@ -5,8 +5,8 @@ hh = int(secondi) // 3600
 secondi = int(secondi) % 3600
 mm = int(secondi) // 60
 secondi = int(secondi) % 60
-print(gg)
-print(hh)
-print(mm)
-print(secondi)
+#print(gg)
+#print(hh)
+#print(mm)
+#print(secondi)
 print('{0:2d}:{1:2d}:{2:2d}:{3:2d}'.format(gg, hh, mm , secondi))
