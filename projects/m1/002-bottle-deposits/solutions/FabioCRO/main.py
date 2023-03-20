8bd855d1479efa1751b3a228d13a4c63edae12e0
Fabio <127136351+FabioCRO@users.noreply.github.com>
2023-03-20 19:20:07 +0100

    es002 Fabio Crocetti

---
+++ projects/m1/002-bottle-deposits/solutions/FabioCRO/main.py
@@ -0,0 +1,7 @@
+# calcola rimborso restituzione contenitori
+conp =int(input("inserisci il numero delle bottiglie piccole"))
+cong =int(input("inserisci il numero delle bottiglie grandi"))
+rimborso= conp*0.10 + cong*0.25
+print('il rimborso totale è di $ {:.2f}'.format(rimborso))
+# specifica 2 cifre decimali sul dato restituito
+
