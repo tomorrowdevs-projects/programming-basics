ec6666f127f7c43ba95ca0c5e3a1395a1bec15c8
Fabio <127136351+FabioCRO@users.noreply.github.com>
2023-03-20 19:14:01 +0100

    es001 Fabio Crocetti

---
+++ projects/m1/001-area-of-a-room/solutions/FabioCRO/main.py
@@ -0,0 +1,5 @@
+# acquisisce da input le misure dei lati di una stanza e calcola l'area
+l1 =float(input("inserisci la misura del primo lato della stanza"))
+l2 =float(input("inserisci la misura del secondo lato della stanza"))
+print("l'area della stanza è",l1*l2,"mq")
+
