cf5e6aebc6f0003a3b49a928e146db3775f75272
Francesco <cicciodb@hotmail.it>
2023-03-21 16:11:01 +0100

    Delete giuseppe Fazio_M002 directory

--- giuseppe Fazio_M002/main.py
+++
@@ -1,9 +0,0 @@
-print ("Calco del Total deposit")
-depMinor = 0.10
-depMaior = 0.25
-
-Minor= int(input ("inserisci il numero di bottiglie <= 1 liter: "))
-Maior= int(input ("inserisci il numero di bottiglie  > 1 liter: "))
-print (" deposito bottiglie piccole = $", Minor*depMinor )
-print (" deposito bottiglie grandi = $",  Maior*depMaior )
-print (" deposito bottiglie totale = $",  Minor*depMinor+Maior*depMaior )
