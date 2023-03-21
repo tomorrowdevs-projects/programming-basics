3dcc10caf3e05baf70af931915bc7fe2d22a38d3
Francesco <cicciodb@hotmail.it>
2023-03-21 16:11:18 +0100

    Delete giuseppe Fazio_M004 directory

--- giuseppe Fazio_M004/main.py
+++
@@ -1,8 +0,0 @@
-# calcolo secondi
-giorni = int(input ("inserisci il numero dei giorni: "))
-ore = int(input ("inserisci il  numero delle ore : "))
-minuti = int(input ("inserisce il numero dei minuti: "))
-secondi = int(input ("inserisci il  valore dei secondi: "))
-
-totale = giorni*86400 + ore * 3600  + minuti * 60 + secondi
-print ("totale in secondi = ", totale)
