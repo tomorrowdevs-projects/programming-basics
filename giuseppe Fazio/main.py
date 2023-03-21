751b8620bf6892e10064eca7719f4ea9e5ae57ac
Francesco <cicciodb@hotmail.it>
2023-03-21 16:10:52 +0100

    Delete giuseppe Fazio directory

--- giuseppe Fazio/main.py
+++
@@ -1,6 +0,0 @@
-# esercizio sul calcolo dell'area di un rettangolo
-print ("Cacola area di un rettangolo")
-print ("inserisci la base")
-b= int(input ("inserisci il  valore della base: "))
-h= int(input ("inserisci un valore dell'altezza: "))
-print (" area della stanza =   {b*h}" )
