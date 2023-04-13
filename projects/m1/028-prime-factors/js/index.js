d036c773d0cbb0d07f817f0bad36d9d0d7a5f490
carlolombardini <lombardini.gd@gmail.com>
2023-04-13 17:45:35 +0200

    fix(m1): edited primeFactorization function, now it returns an array and doesn't print the value directly in exercise 028

--- projects/m1/028-prime-factors/js/index.js
+++ projects/m1/028-prime-factors/js/index.js
@@ -4,41 +4,44 @@ function askInteger(){
     const askedInteger = parseInt( prompt( 'Enter an integer number greater than or equal to 2:', 'Example: 6' ) );
 
     if ( ! isNaN( askedInteger ) ){
-        if ( askedInteger < 2 ){
-            console.log( 'The entered number is an integer but is is less than 2.' );
-            return askInteger();
-        }
-
          return askedInteger;
     }
 
     console.log( 'Please enter a valid integer number greater than or equal to 2.' );
-    return askInteger();
+    return;
 }
 
 function primeFactorization( integerNumber ){
 
+    const factorsSequence = [];
     let factor = 2;
 
     while ( factor <= integerNumber ) {
         if ( integerNumber % factor === 0 ){
-            console.log( factor );
-            // JavaScript hasn't a floor division operator like in Python (Double Slash Operator //) , then we apply Math.floor to the division result.
-            integerNumber = Math.floor( integerNumber / factor );
+            factorsSequence.push( factor );
+            // JavaScript hasn't a floor division operator like in Python (Double Slash Operator //) , then we apply Math.trunc to the division result.
+            integerNumber = Math.trunc( integerNumber / factor );
         } else {
             factor++;
         }
-
     }
 
-    console.log( 'Closing the program...' );
-    return;
+    return factorsSequence;
 }
 
 function init(){
     const askedInteger = askInteger();
-    primeFactorization( askedInteger );
+
+    if ( askedInteger < 2 ) {
+        console.log( 'The entered number is an integer but is is less than 2.' );
+    } else {
+        const factorsSequence = primeFactorization( askedInteger );
+
+        for (let i = 0; i < factorsSequence.length; i++) {
+            console.log( factorsSequence[i] );
+        }
+    }
+
     return;
 }
-
 init();
