bd0982a928d693fc3b316b92a6df2be2c9c5c9d4
ilnuovonumero2 <129592242+ilnuovonumero2@users.noreply.github.com>
2023-04-14 14:19:55 +0200

    Correct Exercise m1/002-bottle-deposits

--- projects/m1/002-bottle-deposits/js/index.js
+++ projects/m1/002-bottle-deposits/js/index.js
@@ -1,22 +1,25 @@
-//calcolo numero contenitori totali prelevati dall'utente
-let bottleOneL = 2;
-let bottlePlusOneL = 5;
-let cashRefoundOneL = 0.10;
-let cashRefoundPlusOneL = 0.25;
+// Data type request
+const prompt = require('prompt-sync')();
+const bottleOneL = parseInt(prompt('Enter the number of 1-liter bottles: '), 10);
+const bottlePlusOneL = parseInt(prompt('Enter the number of bottles larger than 1 liter: '), 10);
+
+// Calculate total containers collected by the user
+const cashRefoundOneL = 0.10;
+const cashRefoundPlusOneL = 0.25;
 let cashRefoundTotal = 0;
 
-//calcolo rimborso generale
+// Calculate overall refund
 let totalRefoundOneL = 0;
 let totalRefoundPlusOneL = 0;
-let totalBottle = bottleOneL + bottlePlusOneL;
+const totalBottle = bottleOneL + bottlePlusOneL;
 
 if (bottleOneL >= 1) {
   totalRefoundOneL = bottleOneL * cashRefoundOneL;
-  console.log("Il numero dei contenitori da un litro che hai prelevato è " + bottleOneL);
+  console.log("The number of 1-liter containers you collected is " + bottleOneL);
 }
 if (bottlePlusOneL >= 1) {
   totalRefoundPlusOneL = bottlePlusOneL * cashRefoundPlusOneL;
-  console.log("il numero dei contenitori che eccedono un litro che hai prelevato è " + bottlePlusOneL);
+  console.log("The number of containers larger than 1 liter you collected is " + bottlePlusOneL);
 }
 cashRefoundTotal = totalRefoundOneL + totalRefoundPlusOneL;
-console.log("Il totale del tuo rimborso previsto in caso di restituzione di " + totalBottle + " contenitori " + "sarà pari a " + cashRefoundTotal.toFixed(2) + " $");
+console.log("The total refund you can expect for returning " + totalBottle + " containers is $" + cashRefoundTotal.toFixed(2));
