13317ff9259e6b539698d40dc4052c00325de1c4
GiovanniPuddu <giovannipuddu@blu.it>
2023-04-26 07:37:22 +0200

    soluzione esercizio 1

---
+++ projects/m1/001-area-of-a-room/js/index.js
@@ -0,0 +1,13 @@
+let Larghezza 
+Larghezza = 2
+let Lunghezza 
+Lunghezza = 3
+let Area=(Larghezza)*(Lunghezza)+("m²")
+
+
+
+console.log(Area)
+
+git add *
+git commit -m "soluzione esercizio 001"
+git push
