8ea35106042a25d8614cda333cc5734afe873adb
Wainar <royaltiger090@gmail.com>
2023-04-27 03:12:38 +0200

    Wainar/solution.py

---
+++ projects/m1/001-area-of-a-room/python/main.py
@@ -0,0 +1,12 @@
+width_room   = 12.30
+lenght_room  = 24.20
+
+
+print('width_room :    ' + str (lenght_room )+' meters')
+print('lenght_room:    ' + str (width_room  ) +' meters') 
+
+print()
+
+area_room = width_room * lenght_room
+
+print("area_room is: " + str(area_room) +' square meters')
