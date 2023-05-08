16892fb7a16c8c80738051c20dc2c635eb3aa1bd
Daniele Piani <danielepiani1993@gmail.com>
2023-05-08 16:03:56 +0200

    M1 034 - Integer to ordinal

--- projects/m1/034-integer-to-ordinal/python/main.py
+++ projects/m1/034-integer-to-ordinal/python/main.py
@@ -1,19 +1,37 @@
-def ordinal_number(number):
-    if number == 1:
-        return "1st"
-    if number == 2:
-        return "2nd"
-    if number == 3:
-        return "3rd"
-    if number in [4, 5, 6, 7, 8, 9, 10, 11, 12]:
-        return str(number) + "th"
-  
-        
-if __name__ == "__main__":
-    number = int(input("Enter a number from 1 to 12: "))
-    ordinal_numbers = ordinal_number(number)
-    
-    if number == 0 or number >= 13:
-        print ("")
+def decimal_to_ordinal(number):
+    """
+    This is a function that transforms decimal numbers into ordinal numbers.
+
+    Args:
+        number (int): A number from 1 to 12
+
+    Returns:
+        ordinal_number (str): A string that represents the number in ordinal form
+    """
+    #create a dictionary with int and corrispondent ordinal number
+    ordinal_numbers = {
+        1: 'first',
+        2: 'second',
+        3: 'third',
+        4: 'fourth',
+        5: 'fifth',
+        6: 'sixth',
+        7: 'seventh',
+        8: 'eighth',
+        9: 'ninth',
+        10: 'tenth',
+        11: 'eleventh',
+        12: 'twelfth'
+    }
+    #if number in ordinal i create a variable that return the value
+    if number in ordinal_numbers:
+        ordinal_number = ordinal_numbers[number]
+        return ordinal_number
     else:
-        print(f'Your {number} means that you are the {ordinal_numbers}')
+        print(f"{number} is not a valid input for decimal_to_ordinal() function.")
+
+if __name__ == '__main__':
+    number = int(input("Insert a number from 1 to 12: "))
+    ordinal_number = decimal_to_ordinal(number)
+    if ordinal_number:
+        print(f"The ordinal form of {number} is {ordinal_number}")
