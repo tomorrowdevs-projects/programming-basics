85901d2c9fac252482876171437e36f39a777ca2
Wainar <101635063+Wainar@users.noreply.github.com>
2023-05-14 23:16:02 +0200

    fixed exercised

--- projects/m1/002-bottle-deposits/python/main.py
+++ projects/m1/002-bottle-deposits/python/main.py
@@ -2,10 +2,10 @@ small_containers = input('Drink containers holding one liter or less: ')
 big_containers = input ('Drink containers holding more than one liter: ')
 
 #Calculate the refound
-refund_small_containers= int (small_containers) * 0.10
-refund_big_containers= int (big_containers) * 0.25
+refund_small_containers= float (small_containers) * 0.10
+refund_big_containers= float (big_containers) * 0.25
 
-refund_total = int (refund_small_containers) + int (refund_big_containers)
+refund_total = float (refund_small_containers) + float (refund_big_containers)
 
 
 print('refund total is {} $'. format(refund_total))
