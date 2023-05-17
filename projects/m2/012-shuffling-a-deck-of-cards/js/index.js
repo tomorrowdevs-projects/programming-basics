7792fe916c690c6326b217a69459e0598d8d655d
Alessia-C <caporasoalessia1611@gmail.com>
2023-05-17 11:14:53 +0200

    fix: solutions 012-shuffling-a-deck-of-cards

--- projects/m2/012-shuffling-a-deck-of-cards/js/index.js
+++ projects/m2/012-shuffling-a-deck-of-cards/js/index.js
@@ -1,27 +1,30 @@
 function createDesk(){
+
     // 52 card: values 2 through 9 and characters T, J, Q, K, A. Suit card: s, h, d, c
     const values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
     const suitCard = ['s', 'h', 'd', 'c']
-    const card = []
-    // Use loop for creating list of cards
-    for(let suit of suitCard){
-        for(let item of values){
-        card.push(item + suit)
-        }
-    }
-    return card
-}
 
-// function shuffle order random the desk
-function shuffle(){
-    const desk = createDesk();
-    const newDesk = []
-    while(newDesk.length <= 52){
-        let randomDesk = Math.floor(Math.random() * desk.length)
-        newDesk.push(desk[randomDesk])
-    }
+    // Use loop for creating list of cards
+    const newDesk = [];
+    values.forEach(item => {
+        suitCard.forEach(card => newDesk.push(item + card))
+    })
+
     return newDesk
 }
 
-console.log('desk: ' + createDesk())
-console.log('shuffle desk: ' + shuffle())
+// function shuffle order random the desk
+function shuffle(desk){
+    const deskClone =[...desk]
+    const newDesk = []
+
+    while(newDesk.length < 52){
+        const randomDesk = Math.floor(Math.random() * deskClone.length)
+        newDesk.push(deskClone[randomDesk])
+        deskClone.splice(randomDesk,1)
+    }
+
+    return newDesk
+}
+
+console.log('shuffle desk: ' + shuffle(createDesk()))
