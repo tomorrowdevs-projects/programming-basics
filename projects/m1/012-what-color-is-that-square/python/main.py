1d4a9c868227a4899c7c378fb65f02a2be185149
EleSar1 <131043105+EleSar1@users.noreply.github.com>
2023-05-17 22:21:32 +0200

    [Fixed] Solution m1-012 what color is that square

--- projects/m1/012-what-color-is-that-square/python/main.py
+++ projects/m1/012-what-color-is-that-square/python/main.py
@@ -1,10 +1,10 @@
-input_number = input()
+input_number = input('Please insert your coordinate: ')
 
 letter = input_number[0]
 number = int(input_number[1])
 
-begin_black = ['A', 'C', 'E', 'G', 'I', 'K']
-begin_white = ['B', 'D', 'F', 'H', 'J', 'L']
+begin_black = ['A', 'C', 'E', 'G', 'I', 'K', 'M', 'O', 'Q', 'S', 'U', 'W', 'Y']
+begin_white = ['B', 'D', 'F', 'H', 'J', 'L', 'N', 'P', 'R', 'T', 'V', 'X', 'Z']
 
 if letter in begin_black: 
     print('The colum begin with a black square.')
