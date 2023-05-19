6e59122e74890afa421511013c926afad953fa35
Daniele Piani <danielepiani1993@gmail.com>
2023-05-19 16:20:43 +0200

    Solution of M1/001 Area of a Room

---
+++ projects/m1/001-area-of-a-room/__init__.py
@@ -0,0 +1,7 @@
+width = float(input("Enter the width of the room: "))
+
+lenght = float(input("Enter the lenght of the room: "))
+
+area = width * lenght #I perform operation with the values I saved
+
+print(f"{area} metres is the area of the room")
