70f596a00509a13c7fa82a10b8ecd328cf06e3bf
Wainar <royaltiger090@gmail.com>
2023-05-19 22:06:50 +0200

    fixed mistakes

--- projects/m1/003-making-change/python/main.py
+++ projects/m1/003-making-change/python/main.py
@@ -1,14 +1,58 @@
-value_cents = int(input("Insert the value of your coins express in cents: "))
+exchangetoCoin = float(input("Please insert the amount of change"))*100
 
-value_coin_now = [360,160,80,40,20,10]
+print("We are going to give you: ")
 
-for i in range(0,7,1):
-    coin_print_now = value_cents // value_coin_now[i]
-    change = value_cents % value_coin_now[i]
-    if change != 0: 
-        if value_coin_now != 0:
-            print("Your change is " + str(coin_print_now) + " " + str(value_coin_now[i]/100) + " cad(s) coin(s)")
-            value_cents = change
-    else: 
-        print("Your change is " + str(coin_print_now) + " " + str(value_coin_now[i]/100) + " cad(s) coin(s)")
-        exit()
+currentCoin = 200
+if exchangetoCoin >= currentCoin:
+    changeIn2Euros = exchangetoCoin / currentCoin
+    changeLeft = int(exchangetoCoin) % int(currentCoin)
+    print(str(int(changeIn2Euros)) + " coins of 2 euros")
+
+
+currentCoin = 100
+if changeLeft >= currentCoin:
+    changeIn1Euros = changeLeft / currentCoin
+    changeLeft = int(changeLeft) % int(currentCoin)
+    print(str(int(changeIn1Euros)) + " coins of 1 euros")
+
+
+currentCoin = 50
+if changeLeft >= currentCoin:
+    changeIn50Cents = changeLeft / currentCoin
+    changeLeft = int(changeLeft) % int(currentCoin)
+    print(str(int(changeIn50Cents)) + " coins of 50 cents")
+
+
+currentCoin = 20
+if changeLeft >= currentCoin:
+    changeIn20Cents = changeLeft / currentCoin
+    changeLeft = int(changeLeft) % int(currentCoin)
+    print(str(int(changeIn20Cents)) + " coins of 20 cents")
+
+
+currentCoin = 10
+if changeLeft >= currentCoin:
+    changeIn10Cents = changeLeft / currentCoin
+    changeLeft = int(changeLeft) % int(currentCoin)
+    print(str(int(changeIn10Cents)) + " coins of 10 cents")
+
+
+currentCoin = 5
+if changeLeft >= currentCoin:
+    changeIn5Cents = changeLeft / currentCoin
+    changeLeft = int(changeLeft) % int(currentCoin)
+    print(str(int(changeIn5Cents)) + " coins of 5 cents")
+
+
+currentCoin = 2
+if changeLeft >= currentCoin:
+    changeIn2Cents = changeLeft / currentCoin
+    changeLeft = int(changeLeft) % int(currentCoin)
+    print(str(int(changeIn2Cents)) + " coins of 2 cents")
+
+
+currentCoin = 1
+if changeLeft >= currentCoin:
+    changeIn1Cents = changeLeft / currentCoin
+    changeLeft = int(changeLeft) % int(currentCoin)
+    print(str(int(changeIn1Cents)) + " coins of 1 cents")
