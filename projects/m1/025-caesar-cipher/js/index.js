b3b9e1836416b5db7593df064b7ca7d7ca9aaecb
Sparuz <vbiagio1@gmail.com>
2023-05-19 22:26:35 +0200

    feat(m1) fix solution exe-025 caesar-cipher

--- projects/m1/025-caesar-cipher/js/index.js
+++ projects/m1/025-caesar-cipher/js/index.js
@@ -1,3 +1,19 @@
 const prompt = require("prompt-sync")();
 
-const prova = prompt("prova:");
+const string = prompt("insert string: ");
+const shift = prompt("insert number to shift: ");
+
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+const newArray = [];
+
+for (let i = 0; i < string.length; i++) {
+    let index = alphabet.indexOf(string[i]);
+    let newIndex = parseFloat(index) + parseFloat(shift);
+    let moduleTest = newIndex % alphabet.length;
+    let newString = alphabet[moduleTest];
+    newArray.push(newString);
+}
+
+console.log(newArray.join(""));
+
