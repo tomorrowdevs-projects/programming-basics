c4ffa89c96b22a24eff0a9c2d5d954d2e184daee
Myname <myname@changeit.com>
2023-05-20 21:57:37 +0200

    008-dog-years

---
+++ projects/m1/008-dog-years/python/008-dog-years.py
@@ -0,0 +1,10 @@
+#calculate dog's years considering  1 year = 10,5
+
+age= int(input("WOW A LOVELY DOGGO!how old is this GOOD BOY?? "))
+
+if age > 0 and ange <= 2:
+  print (age*10.5, "human years?rly?awww a lovely little puppy!")
+if age >= 3:
+  print (21 +((age -2)*4), "human years !wow!that's my good boy!")
+else:
+  print ("impossibru!!!!")
