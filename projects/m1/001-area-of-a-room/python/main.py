fb8715f2174ca058a051d7ad566eeb7e7dbf2cc9
Isabel <isabel.lombardi96@gmail.com>
2023-05-20 18:36:50 +0200

    [FIX] test action execution

---
+++ projects/m1/001-area-of-a-room/python/main.py
@@ -0,0 +1,6 @@
+width = float(input("Enter the width of the room: "))
+length = float(input("Enter the length of the room: "))
+
+area = width * length
+
+print(f"{area} metres is the area of the room")
