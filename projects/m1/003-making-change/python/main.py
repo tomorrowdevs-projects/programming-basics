dbba6ba09804142dc80e0aac2d1b8af4717652ff
EleSar1 <131043105+EleSar1@users.noreply.github.com>
2023-06-01 23:11:51 +0200

    [FIXED] Making Change

--- projects/m1/003-making-change/python/main.py
+++ projects/m1/003-making-change/python/main.py
@@ -2,16 +2,18 @@
 change_in_cents = int(input("Insert the value of your coins express in cents: "))
 
 current_coin = [200,100,25,10,5,1]
+coin_name = ['toonies', 'loonies', 'quarters', 'dimes', 'nickels', 'pennies']
 
 for i in range(0,6,1):
     coin_to_print = change_in_cents // current_coin[i]
     change = change_in_cents % current_coin[i]
     if change != 0: 
         if coin_to_print != 0:
-            print("Your change is " + str(coin_to_print) + " " + str(current_coin[i]/100) + " euro(s) coin(s)")
+            print("Your change is " + str(coin_to_print) + " " + coin_name[i])
             change_in_cents = change
     else: 
-        print("Your change is " + str(coin_to_print) + " " + str(current_coin[i]/100) + " euro(s) coin(s)")
+        print("Your change is " + str(coin_to_print) + " " + coin_name[i])
+        break
 
 
 
