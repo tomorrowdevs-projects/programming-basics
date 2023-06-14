601907011026fee80e16057cd9f66deccc256188
Wainar <royaltiger090@gmail.com>
2023-06-15 00:18:50 +0200

    1 fixed

--- projects/m1/012-what-color-is-that-square/python/main.py
+++ projects/m1/012-what-color-is-that-square/python/main.py
@@ -1,24 +1,21 @@
 #Target focus "The letter identifies the column, while the number identifies the row by the User!"
 
-#AI briefing
+introduction = input(" Hello what's your name ")
 
-def introduction():
-    print(input("Hey what's your name"))
-    print("Nice to meet you i'm a bot doradora")
-    
-introduction()
+reply = " Nice to meet you i'm robot dora "
 
-#Assigning variables
+print(" so " + introduction + reply)
 
-blackwhite_column = input("Please select the column black or white for examples(a-h) ").upper()
+blackwhite_column = input("Please select the column black or white for examples(a-h): ").lower()
 
-row_number = int(input("Please select the raw to begin for examples(1-8) "))
+row_number = int(input("Please select the row to begin for examples(1-8): "))
 
-#Start the core of the project
+#Start the core
 
-if blackwhite_column == "A" or blackwhite_column == "C" or blackwhite_column == "E" or blackwhite_column == "G":
-    if row_number % 2 == 0: print("The {}{} square is white".format(blackwhite_column,row_number))
+if blackwhite_column == "a" or blackwhite_column == "c" or blackwhite_column == "e" or blackwhite_column == "g":
+    if row_number % 2 == 1: print("The {}{} square is white".format(blackwhite_column,row_number))
     else: print("The {}{} square is black".format(blackwhite_column,row_number))
-if row_number == "B" or row_number == "D" or row_number == "F" or row_number == "H":
-    if row_number % 2 == 0: print("The {}{} square is black".format(blackwhite_column,row_number))
+
+if blackwhite_column == "b" or blackwhite_column == "d" or blackwhite_column == "f" or blackwhite_column == "h":
+    if row_number % 2 == 1: print("The {}{} square is black".format(blackwhite_column,row_number))
     else: print("The {}{} square is white".format(blackwhite_column,row_number))
