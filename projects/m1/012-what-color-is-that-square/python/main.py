ce50e941b9f38f2d990203c8116113b9641bf2ef
Wainar <royaltiger090@gmail.com>
2023-06-18 22:42:11 +0200

    New Fixed

--- projects/m1/012-what-color-is-that-square/python/main.py
+++ projects/m1/012-what-color-is-that-square/python/main.py
@@ -1,21 +1,23 @@
 #Target focus "The letter identifies the column, while the number identifies the row by the User!"
 
-introduction = input(" Hello what's your name ")
+blackwhite_column_input = str.lower(input(" Please select the column black or white for examples(a1-h8) to start: "))
 
-reply = " Nice to meet you i'm robot dora "
+black_column = ["a", "c", "e", "g"]
 
-print(" so " + introduction + reply)
+white_column = ["b", "d", "f", "h"]
 
-blackwhite_column = input("Please select the column black or white for examples(a-h): ").lower()
+alphabet = blackwhite_column_input[0]
 
-row_number = int(input("Please select the row to begin for examples(1-8): "))
+row_number = int(blackwhite_column_input[1])
 
-#Start the core
+if alphabet in black_column:
+    if row_number % 2 == 0: 
+        print("The square is White")
+    else: 
+        print("The square is Black")
 
-if blackwhite_column == "a" or blackwhite_column == "c" or blackwhite_column == "e" or blackwhite_column == "g":
-    if row_number % 2 == 1: print("The {}{} square is white".format(blackwhite_column,row_number))
-    else: print("The {}{} square is black".format(blackwhite_column,row_number))
-
-if blackwhite_column == "b" or blackwhite_column == "d" or blackwhite_column == "f" or blackwhite_column == "h":
-    if row_number % 2 == 1: print("The {}{} square is black".format(blackwhite_column,row_number))
-    else: print("The {}{} square is white".format(blackwhite_column,row_number))
+elif alphabet in white_column:
+    if row_number % 2 == 0: 
+        print("The square is Black")
+    else: 
+        print("The square is White")
