1f502ba611caa596cd17743beaa21eded7de4214
ilnuovonumero2 <129592242+ilnuovonumero2@users.noreply.github.com>
2023-06-27 16:58:08 +0200

    feat(m1): add solution exe-033 shipping-calculator

--- projects/m1/033-shipping-calculator/js/index.js
+++ projects/m1/033-shipping-calculator/js/index.js
@@ -1,12 +1,9 @@
 let ordersArray = [];
-
 function startInput() {
-   
     let newOrder = "";
 
     while (newOrder !== "n") {
         newOrder = prompt("Inserisci un articolo nel carrello (inserisci n per terminare):");
-
         if (newOrder !== "n") {
             ordersArray.push(newOrder);
         }
@@ -14,16 +11,16 @@ function startInput() {
 }
 
 function definitiveCost(totalOrders) {
-    let basicCost = 10.99;
-    let otherCost = 2.99;
-    let totalCost = basicCost + (otherCost * totalOrders);
+    const basicCost = 10.99;
+    const otherCost = 2.99;
+    const totalCost = (totalOrders > 1) ? (basicCost + (otherCost * (totalOrders - 1))) : basicCost;
     return totalCost;
 }
 
 function calculateAndShow() {
     startInput(); 
     const totalOrders = ordersArray.length; 
-    let totalshipping = definitiveCost(totalOrders); 
-    let ordersList = ordersArray.join(", "); 
+    const totalshipping = definitiveCost(totalOrders); 
+    const ordersList = ordersArray.join(", "); 
     document.getElementById('output').innerHTML = `Il costo totale della spedizione per gli articoli ${ordersList} è ${totalshipping.toFixed(2)}`;
 }
