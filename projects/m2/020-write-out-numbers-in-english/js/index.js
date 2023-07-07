0d0fecddf8caca9bebd20dcebfbd7038c8818fb7
Alessia-C <caporasoalessia1611@gmail.com>
2023-07-07 16:56:50 +0200

    fix(m2): 020-write-out-number

--- projects/m2/020-write-out-numbers-in-english/js/index.js
+++ projects/m2/020-write-out-numbers-in-english/js/index.js
@@ -1,35 +1,6 @@
-const listInteger = {
-    0: 'zero',
-    1:'one',
-    2: 'two', 
-    3: 'three', 
-    4: 'four ', 
-    5: 'five', 
-    6: 'six', 
-    7: 'seven', 
-    8: 'eight', 
-    9: 'nine', 
-    10: 'ten', 
-    11: 'eleven', 
-    12: 'twelve ', 
-    13: 'thirteen', 
-    14: 'fourteen', 
-    15: 'fifteen', 
-    16: 'sixteen', 
-    17: 'seventeen', 
-    18: 'eighteen', 
-    19: 'nineteen', 
-    20: 'twenty', 
-    30: 'thirty', 
-    40: 'forty', 
-    50: 'fifty', 
-    60: 'sixty', 
-    70: 'seventy', 
-    80: 'eighty', 
-    90: 'ninety'
-}
+
   
-const userNumber = prompt('Enter the number', '142')
+// const userNumber = prompt('Enter the number', '142')
 
 function writeNumber(number, list){
     let message = ''
@@ -53,24 +24,26 @@ function writeNumber(number, list){
             unit = list[calcUnits]
             
             if(hundred !== 'zero'){
-                message += hundred + ' hundred '
+                message += hundred + ' hundred'
             } 
             
             if(ten !== 'zero') {
                 message += ten + '-'
             } 
             
+            if(unit !== "zero") {
             message += unit
+            }
             }
     }
 
     return message
 }
   
-console.log(writeNumber(userNumber, listInteger))
+// console.log(writeNumber(userNumber, listInteger))
 //   console.log(writeNumber(15, listInteger))
 //   console.log(writeNumber(22, listInteger))
 //   console.log(writeNumber(970, listInteger))
 //   console.log(writeNumber(5, listInteger))
 //   console.log(writeNumber(300, listInteger))
-  
+module.exports = writeNumber
