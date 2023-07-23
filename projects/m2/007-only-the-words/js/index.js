e97b52a5bc43fa3744075d2c514119d10a22b0ca
carlolombardini <lombardini.gd@gmail.com>
2023-07-23 13:29:33 +0200

    fix(m2): change function onlyWords adding a condition to verify if given argument phrase is longer than 0 so trim() doesn't give an error, in exercise 007

--- projects/m2/007-only-the-words/js/index.js
+++ projects/m2/007-only-the-words/js/index.js
@@ -1,22 +1,25 @@
 function onlyWords(phrase){
-    const exceptions = [',','.','?','‐','\'','!',':',';']
     const words = [''];
-    let j = 0;
 
-    for (let i = 0; i < phrase.trim().length; i++) {
-        const letter = phrase.charAt(i);
+    if(phrase.length > 0){
+        const exceptions = [',','.','?','‐','\'','!',':',';']
+        let j = 0;
 
-        if( letter !== ' ' ){
-            words[j] += letter;
-        } else {
-            j++;
-            words[j] = '';
+        for (let i = 0; i < phrase.trim().length; i++) {
+            const letter = phrase.charAt(i);
+
+            if( letter !== ' ' ){
+                words[j] += letter;
+            } else {
+                j++;
+                words[j] = '';
+            }
         }
-    }
 
-    for (let i = 0; i < words.length; i++) {
-        while( exceptions.includes( words[i].charAt(words[i].length-1) ) ){
-            words[i] = words[i].slice(0,-1);
+        for (let i = 0; i < words.length; i++) {
+            while( exceptions.includes( words[i].charAt(words[i].length-1) ) ){
+                words[i] = words[i].slice(0,-1);
+            }
         }
     }
 
