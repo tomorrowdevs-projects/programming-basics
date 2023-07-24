1d27b3ce3658bad83274a72671096ec2d21377ee
carlolombardini <lombardini.gd@gmail.com>
2023-07-24 16:39:27 +0200

    fix(m2): loded correct program, in exercise 011

--- projects/m2/011-pig-latin/js/index.js
+++ projects/m2/011-pig-latin/js/index.js
@@ -1,72 +1,35 @@
 // const prompt = require("prompt-sync")({ sigint: true}) ;
 
-function getPhrases(){
-    const phrases = [];
-    
-    let phrase = prompt('Enter the items, leave a blank line when you want to stop the program.');
+function pigLatin(text){
+    if(typeof text === 'string' && text.length > 0){
+        const vowels = ['a','e','i','o','u'];
 
-    while ( phrase.length > 0 && phrase.trim() !== '') {
-        phrases.push(phrase);
-        phrase = prompt('Enter the items, leave a blank line when you want to stop the program.');
-    }
-    return phrases;
-}
+        let consonantsBeforeVowel = '';
+        for (let i = 0; i < text.length; i++) {
+            const letter = text[i];
 
-function getWords(phrase){
-
-    const words = [''];
-
-    if(typeof phrase === 'string' && phrase.length > 0){
-        const punctuation = [',','.','?','‐','\'','!',':',';'];
-        let j = 0;
-
-        for (let i = 0; i < phrase.trim().length; i++) {
-            const letter = phrase.charAt(i);
-
-            if( letter !== ' ' ){
-                if( ! punctuation.includes(letter) ){
-                    words[j] += letter;
-                }
+            if( ! vowels.includes( letter ) ){
+                consonantsBeforeVowel += letter;
+                
             } else {
-                j++;
-                words[j] = '';
+                break;
             }
         }
-    }
 
-    return words;
-}
-
-function generateItemsList(words){
-    let items = '';
-    
-    if (words.length > 1){
-        for (let i = 0; i < words.length-1; i++) {
-            items += words[i] + ', ';
+        if( consonantsBeforeVowel === '' ){
+            text += 'way';
+        } else {
+            text += consonantsBeforeVowel + 'ay';
+            text = text.slice(consonantsBeforeVowel.length);
         }
-
-        items = items.slice(0,-2);
-        items += ' and ' + words[words.length-1];
-    } else {
-        items += words[0];
     }
 
-    return items;
+    return text;
 }
 
 function init(){
-    const askedPhrases = getPhrases();
-
-    let words = [];
-    if( askedPhrases.length > 0 ){
-        for (let i = 0; i < askedPhrases.length; i++) {
-            words = words.concat( getWords(askedPhrases[i]) );
-        }
-    }
-
-    if( words.length > 0 ){
-        console.log(generateItemsList(words));
-    }
+    const text = prompt('Enter a line of text.');
+    console.log( pigLatin(text) );
 
     return;
 } 
