0832e2cf3655629c0a04b08900f54298df491347
ilnuovonumero2 <129592242+ilnuovonumero2@users.noreply.github.com>
2023-07-25 10:26:24 +0200

    feat(m2): add solution exe-019 morse-code

--- projects/m2/019-morse-code/js/index.js
+++ projects/m2/019-morse-code/js/index.js
@@ -1,67 +1,61 @@
 const prompt = require('prompt-sync')();
 const originalPhrase = prompt("Inserisci una frase: ");
 
-let extractLetters = phrase => phrase.toUpperCase().split("");
+const dictionary = {
+  'A': '.-',
+  'B': '-...',
+  'C': '-.-.',
+  'D': '-..',
+  'E': '.',
+  'F': '..-.',
+  'G': '--.',
+  'H': '....',
+  'I': '..',
+  'J': '.---',
+  'K': '-.-',
+  'L': '.-..',
+  'M': '--',
+  'N': '-.',
+  'O': '---',
+  'P': '.--.',
+  'Q': '--.-',
+  'R': '.-.',
+  'S': '...',
+  'T': '-',
+  'U': '..-',
+  'V': '...-',
+  'W': '.--',
+  'X': '-..-',
+  'Y': '-.--',
+  'Z': '--..',
+  '1': '.----',
+  '2': '..---',
+  '3': '...--',
+  '4': '....-',
+  '5': '.....',
+  '6': '-....',
+  '7': '--...',
+  '8': '---..',
+  '9': '----.',
+  '0': '-----',
+  'Á': '.--.-',
+  'Ä': '.-.-',
+  'É': '..-..',
+  'Ñ': '--.--',
+  'Ö': '---.',
+  'Ü': '..--',
+};
 
-const letters = extractLetters(originalPhrase);
+const letters = originalPhrase.toUpperCase().split("");
 
-let dictionary = {};
-
-const associateLetters = (letter, code) => {
-  dictionary[letter] = code;
-}
-
-associateLetters('A', '.-');
-associateLetters('B', '-...');
-associateLetters('C', '-.-.');
-associateLetters('D', '-..');
-associateLetters('E', '.');
-associateLetters('F', '..-.');
-associateLetters('G', '--.');
-associateLetters('H', '....');
-associateLetters('I', '..');
-associateLetters('J', '.---');
-associateLetters('K', '-.-');
-associateLetters('L', '.-..');
-associateLetters('M', '--');
-associateLetters('N', '-.');
-associateLetters('O', '---');
-associateLetters('P', '.--.');
-associateLetters('Q', '--.-');
-associateLetters('R', '.-.');
-associateLetters('S', '...');
-associateLetters('T', '-');
-associateLetters('U', '..-');
-associateLetters('V', '...-');
-associateLetters('W', '.--');
-associateLetters('X', '-..-');
-associateLetters('Y', '-.--');
-associateLetters('Z', '--..');
-associateLetters('1', '.----');
-associateLetters('2', '..---');
-associateLetters('3', '...--');
-associateLetters('4', '....-');
-associateLetters('5', '.....');
-associateLetters('6', '-....');
-associateLetters('7', '--...');
-associateLetters('8', '---..');
-associateLetters('9', '----.');
-associateLetters('0', '-----');
-associateLetters('Á', '.--.-');
-associateLetters('Ä', '.-.-');
-associateLetters('É', '..-..');
-associateLetters('Ñ', '--.--');
-associateLetters('Ö', '---.');
-associateLetters('Ü', '..--');
-
-let translatePhrase = phrase => {
-  let numbers = [];
-  for (let letter of phrase) {
+const translatePhrase = (letters) => {
+  let morseCode = '';
+  for (let letter of letters) {
     if (dictionary[letter]) {
-      numbers.push(dictionary[letter]);
+      morseCode += dictionary[letter] + ' ';
     }
   }
-  return numbers.join(" ");
-}
+  return morseCode.trim();
+};
 
 console.log(translatePhrase(letters));
