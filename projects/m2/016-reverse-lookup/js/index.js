d1d45d29a24bccaaeeb3c9e6699595fc573996d7
carlolombardini <lombardini.gd@gmail.com>
2023-07-25 16:13:32 +0200

    feat(m2): add solution exercise 016 completed

---
+++ projects/m2/016-reverse-lookup/js/index.js
@@ -0,0 +1,29 @@
+function reverseLookup(dictionary, value){
+    const keys = [];
+
+    Object.keys( dictionary ).forEach( key => {
+        if( dictionary[key] === value ){
+            keys.push(key);
+        }
+    });
+
+    return keys;
+}
+if( require.main === module ){
+    function init(){
+
+        const gamesDistributionPlatforms = {
+            'pc': 'Tekken 8',
+            'ps4': 'Tekken 8',
+            'ps5': 'Tekken 8',
+            'switch': 'Pikmin 4',
+            'xboxseriesx': 'The Crew Motorfest',
+            'xboxOne': 'The Crew Motorfest',
+        };
+
+        console.log( reverseLookup(gamesDistributionPlatforms, 'Tekken 8') );
+        console.log( reverseLookup(gamesDistributionPlatforms, 'Pikmin 4') );
+        console.log( reverseLookup(gamesDistributionPlatforms, 'Zelda') );
+    }
+    init();
+}
