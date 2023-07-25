deeeddc9aca9f36e8fb56bff10fce8fd9369a68d
ilnuovonumero2 <129592242+ilnuovonumero2@users.noreply.github.com>
2023-07-25 10:32:08 +0200

    feat(m2): add solution exe-025 checking-for-a-winning-card

--- projects/m2/025-checking-for-a-winning-card/js/index.js
+++ projects/m2/025-checking-for-a-winning-card/js/index.js
@@ -1,14 +1,14 @@
 function generateBingoCard() {
-    let ranges = [[1, 15], [16, 30], [31, 45], [46, 60], [61, 75]];
-    let headers = ['B', 'I', 'N', 'G', 'O'];
-    let bingoColumns = {};
+    const ranges = [[1, 15], [16, 30], [31, 45], [46, 60], [61, 75]];
+    const headers = ['B', 'I', 'N', 'G', 'O'];
+    const bingoColumns = {};
 
     for (let i = 0; i < 5; i++) {
-        let column = [];
+        const column = [];
         for (let j = 0; j < 5; j++) {
-            let min = ranges[i][0];
-            let max = ranges[i][1];
-            let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+            const min = ranges[i][0];
+            const max = ranges[i][1];
+            const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
             column.push(randomNum);
         }
         bingoColumns[headers[i]] = column;
@@ -30,47 +30,38 @@ function printBingoCard(bingoColumns) {
 
 function checkBingoCard(card) {
     let headers = ['B', 'I', 'N', 'G', 'O'];
-
-    for (let i = 0; i < 5; i++) {
-        if (headers.every(header => card[header][i] === 0)) {
-            return true;
-        }
+  
+    for (let i = 0; i < headers.length; i++) {
+      if (headers.every(header => card[header][i] === 0)) {
+        return true;
+      }
     }
-
+  
     for (let header of headers) {
-        if (card[header].every(num => num === 0)) {
-            return true;
-        }
-    }
-
-    if (headers.every((header, i) => card[header][i] === 0)) {
+      if (card[header].every(num => num === 0)) {
         return true;
+      }
     }
-    if (headers.every((header, i) => card[header][4 - i] === 0)) {
-        return true;
-    }
-
+  
     return false;
-}
+  }
+  
 
 function main() {
-   
-    let card1 = generateBingoCard();
-    let card2 = generateBingoCard();
-    let card3 = generateBingoCard();
-    let card4 = generateBingoCard();
+    const card1 = generateBingoCard();
+    const card2 = generateBingoCard();
+    const card3 = generateBingoCard();
+    const card4 = generateBingoCard();
 
-    
-    card1['B'] = [0, 0, 0, 0, 0]; 
-    card2['B'][0] = card2['I'][1] = card2['N'][2] = card2['G'][3] = card2['O'][4] = 0; 
-    card3['B'][0] = card3['I'][0] = card3['N'][0] = card3['G'][0] = card3['O'][0] = 0; 
-    card4['B'][0] = card4['I'][1] = 0; 
+    card1['B'] = [0, 0, 0, 0, 0]; // A winning vertical line
+    card2['B'][0] = card2['I'][1] = card2['N'][2] = card2['G'][3] = card2['O'][4] = 0; // A winning diagonal line
+    card3['B'][0] = card3['I'][0] = card3['N'][0] = card3['G'][0] = card3['O'][0] = 0; // A winning horizontal line
+    card4['B'][0] = card4['I'][1] = 0; // A losing card
 
- 
     [card1, card2, card3, card4].forEach((card, i) => {
         console.log(`Card ${i + 1}:`);
         printBingoCard(card);
-        console.log(checkBingoCard(card) ? "Questa carta ha una linea vincente!" : "Questa carta non ha una linea vincente.");
+        console.log(checkBingoCard(card) ? "This card has a winning line!" : "This card does not have a winning line.");
     });
 }
 
