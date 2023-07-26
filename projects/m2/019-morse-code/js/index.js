81e4039e009710e2a3e43a619b3037526f23c9c7
carlolombardini <lombardini.gd@gmail.com>
2023-07-26 12:14:31 +0200

    feat(m2): add solution exercise 019 completed

---
+++ projects/m2/019-morse-code/js/index.js
@@ -0,0 +1,77 @@
+// const prompt = require("prompt-sync")({ sigint: true}) ;
+
+function convertToMorseCode(message, dictionary){
+    message = message.toUpperCase();
+    message = message.replaceAll(' ', '');
+
+    let convertedMessage = '';
+
+    for (let i = 0; i < message.length; i++) {
+        const letter = message[i];
+
+        if(Object.keys(dictionary).includes(letter)){
+            convertedMessage += dictionary[letter] + ' ';
+        }
+    }
+
+    if( convertedMessage.charAt(convertedMessage.lenght-1) === ' ' ){
+        convertedMessage.slice(0,-1);
+    }
+
+    return convertedMessage;
+}
+
+function init(){
+    const morseCodeDictionary = {
+        A: '.-',
+        B: '-...',
+        C: '-.-.',
+        D: '-..',
+        E: '.',
+        F: '..-.',
+        G: '--.',
+        H: '....',
+        I: '..',
+        J: '.---',
+        K: '-.-',
+        L: '.-..',
+        M: '--',
+        N: '-.',
+        O: '---',
+        P: '.--.',
+        Q: '--.-',
+        R: '.-.',
+        S: '...',
+        T: '-',
+        U: '..-',
+        V: '...-',
+        W: '.--',
+        X: '-..-',
+        Y: '-.--',
+        Z: '--..',
+        1: '.----',
+        2: '..---',
+        3: '...--',
+        4: '....-',
+        5: '.....',
+        6: '-....', 
+        7: '--...', 
+        8: '---..', 
+        9: '----.', 
+        0: '-----',
+        Á: '.--.-',
+        É: '..-..',
+        Ö: '---.',
+        Ä: '.-.-',
+        Ñ: '--.--',
+        Ü: '..--',
+    };
+
+    const message = prompt('Enter a message');
+    const convertedMessage = convertToMorseCode(message, morseCodeDictionary);
+
+    console.log(convertedMessage);
+
+    return;
+} 
+init();
