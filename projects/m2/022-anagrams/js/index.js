4ff7245cec74d4b7717ed4c387fbc228bd99e77b
carlolombardini <lombardini.gd@gmail.com>
2023-07-26 17:03:18 +0200

    feat(m2): add solution exercise 022 completed

---
+++ projects/m2/022-anagrams/js/index.js
@@ -0,0 +1,26 @@
+// const prompt = require("prompt-sync")({ sigint: true}) ;
+
+function isAnagram(originalText, potentialAnagram){
+    if(originalText.length === potentialAnagram.length){
+        for (let i = 0; i < potentialAnagram.length; i++) {
+            if( ! originalText.includes( potentialAnagram.charAt(i) )){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return false;
+}
+
+function init(){
+
+    const askedOriginalText = prompt('Enter the original text.');
+    const askedPotentialAnagram = prompt('Enter its anagram to verify.');
+
+    console.log( isAnagram(askedOriginalText, askedPotentialAnagram) );
+
+    return;
+} 
+init();
