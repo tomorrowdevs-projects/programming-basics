bed050744efcb786a5bff7238274d1f1befc9817
carlolombardini <lombardini.gd@gmail.com>
2023-07-27 09:25:58 +0200

    fix(m2): rewrote the entire program, in exercise 024

--- projects/m2/024-create-a-bingo-card/js/index.js
+++ projects/m2/024-create-a-bingo-card/js/index.js
@@ -3,32 +3,70 @@ function generateBingoCardNumber(){
     const gameName = 'BINGO';
 
     for (let i = 1; i <= gameName.length; i++) {
-        const minValueForThisColumn = i*15 - 14;
-        const maxValueForThisColumn = i*15;
+        const pointsForColumn = 15
+        const minValueForThisColumn = i*pointsForColumn - (pointsForColumn - 1);
+        const maxValueForThisColumn = i*pointsForColumn;
 
         bingoCardNumber[gameName.charAt(i-1)] = Math.floor( Math.random()*(maxValueForThisColumn - minValueForThisColumn + 1) + minValueForThisColumn);
-
     }
 
     return bingoCardNumber;
-
 }
 
-function designBingoCard(bingoCardNumber){
-    let table = '';
+function designDashedLine(tableLength){
+    let dashedLine = '';
+    for (let i = 0; i < tableLength; i++) {
+        dashedLine += '-';
+    }
+
+    return dashedLine;
+}
+
+function designBingoCardHead(){
+    let tableHead = '';
+    const gameName = 'BINGO';
+
+    for (let i = 0; i < gameName.length; i++) {
+        tableHead += `${i === 0 ? '|' : ''}  ${gameName.charAt(i)}  |`;
+    }
+
+    return tableHead;
+}
+
+function designBingoCardRow(bingoCardNumber){
+    let tableRow = '';
+    let count = 0;
     
     Object.keys(bingoCardNumber).forEach( key => {
-        table += `|  ${key}  |  ${bingoCardNumber[key].toString().padStart(2, ' ')}  |\n`;
+        const spaceRight = bingoCardNumber[key].toString().length === 1 ? '  ' : ' ';
+        tableRow += `${count === 0 ? '\n|' : ''}  ${bingoCardNumber[key]}${spaceRight}|`;
+        count++;
     });
 
-    return table;
+    return tableRow;
+}
+
+function designBingoCard(bingoCardRecords){
+    const tableHead = designBingoCardHead();
+    const tableLength = tableHead.length;
+    const dashedLine = designDashedLine(tableLength);
+    let bingoCardRows = '';
+
+    for (let i = 0; i < bingoCardRecords.length; i++) {
+        bingoCardRows += `${designBingoCardRow(bingoCardRecords[i])}\n${dashedLine}`;
+    }
+
+    return `${dashedLine}\n${tableHead}\n${dashedLine}${bingoCardRows}`;
 }
 
 if (require.main === module) { 
     function init(){
-        const randomBingoCardNumber = generateBingoCardNumber();
-        const table = designBingoCard(randomBingoCardNumber);
-        
+        const setOf5BingoCards = [];
+        for(let i = 0; i < 5; i++){
+            setOf5BingoCards.push( generateBingoCardNumber() );
+        }
+
+        const table = designBingoCard(setOf5BingoCards);
         console.log(table);
 
         return;
