dfe11eb73e9c5c94efb0afef33349ed0c270c593
Gianki 1995 <giancarlosquitieri@gmail.com>
2023-08-07 17:50:34 +0200

    Solution/m1-045

---
+++ projects/m1/045-magic-dates/js/index.js
@@ -0,0 +1,33 @@
+// # Magic Dates
+
+// A magic date is a date where the day multiplied by the month is equal to the two digit year. 
+// For example, June 10, 1960 is a magic date because June is the sixth month, and 6 times 10 is 60, which is equal to the two digit year. 
+// Write a function that determines whether or not a date is a magic date. 
+// Use your function to create a main program that finds and displays all of the magic dates in the 20th century.
+
+function magicDate(){
+
+    let startDate = new Date("1900, 1, 1");
+    let endDate = new Date("1999, 12, 31");
+    let years = [];
+
+    for (d = startDate; d <= endDate; d.setDate(d.getDate() + 1)) {
+
+            month = parseInt(d.getMonth() + 1);
+            day =  parseInt(d.getDate());
+            year = parseInt(d.getFullYear() - 1900);
+
+
+                if (month * day === year) {
+                years.push(`${d.getDate()}/${(d.getMonth() + 1)}/${d.getFullYear()}`);
+                }
+
+
+    }
+
+    console.log(years.join(" Magic Date!\n" ));
+}
+
+
+
+magicDate();
