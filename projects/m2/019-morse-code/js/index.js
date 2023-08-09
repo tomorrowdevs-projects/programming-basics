f94362b0b0fa247639a53ce0d9d3775287d016c1
carlolombardini <lombardini.gd@gmail.com>
2023-08-09 11:50:50 +0200

    fix(m2): added the morse code dictionary directyly in convertToMorseCode function to avoid useless dependency, in exercise 019

--- projects/m2/019-morse-code/js/index.js
+++ projects/m2/019-morse-code/js/index.js
@@ -1,27 +1,10 @@
 // const prompt = require("prompt-sync")({ sigint: true}) ;
 
-function convertToMorseCode(message, dictionary){
+function convertToMorseCode(message){
     message = message.toUpperCase();
     message = message.replaceAll(' ', '');
 
     let convertedMessage = '';
-
-    for (let i = 0; i < message.length; i++) {
-        const letter = message[i];
-
-        if(letter in dictionary){
-            convertedMessage += dictionary[letter] + ' ';
-        }
-    }
-
-    if( convertedMessage.charAt(convertedMessage.lenght-1) === ' ' ){
-        convertedMessage.slice(0,-1);
-    }
-
-    return convertedMessage;
-}
-
-function init(){
     const morseCodeDictionary = {
         A: '.-',
         B: '-...',
@@ -67,8 +50,24 @@ function init(){
         Ü: '..--',
     };
 
+    for (let i = 0; i < message.length; i++) {
+        const letter = message[i];
+
+        if(letter in morseCodeDictionary){
+            convertedMessage += morseCodeDictionary[letter] + ' ';
+        }
+    }
+
+    if( convertedMessage.charAt(convertedMessage.lenght-1) === ' ' ){
+        convertedMessage.slice(0,-1);
+    }
+
+    return convertedMessage;
+}
+
+function init(){
     const message = prompt('Enter a message');
-    const convertedMessage = convertToMorseCode(message, morseCodeDictionary);
+    const convertedMessage = convertToMorseCode(message);
 
     console.log(convertedMessage);
 
