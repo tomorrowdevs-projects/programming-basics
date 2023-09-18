0ae81bed6abd7c5ba76c6e250b8b884ed9465354
ilnuovonumero2 <129592242+ilnuovonumero2@users.noreply.github.com>
2023-09-18 06:34:34 +0200

    feat(m4): add solution exe-006 two-word-random-password

--- projects/m4/006-two-word-random-password/js/index.js
+++ projects/m4/006-two-word-random-password/js/index.js
@@ -8,36 +8,46 @@ if (!nomeFile) {
     process.exit(1);
 }
 
-try {
-    let parole = [];
+async function generaPassword() {
+    let parole = await leggiFile(nomeFile);
+    let password;
+    do {
+        let parola1 = parole[Math.floor(Math.random() * parole.length)];
+        let parola2 = parole[Math.floor(Math.random() * parole.length)];
+        password = parola1.charAt(0).toUpperCase() + parola1.slice(1) + parola2.charAt(0).toUpperCase() + parola2.slice(1);
+    } while (password.length < 8 || password.length > 10);
+    return password;
+}
 
-    let rl = readline.createInterface({
-        input: fs.createReadStream(nomeFile),
-        output: process.stdout,
-        terminal: false
-    });
+function leggiFile(nomeFile) {
+    return new Promise((resolve, reject) => {
+        let parole = [];
+        const rl = readline.createInterface({
+            input: fs.createReadStream(nomeFile),
+            terminal: false
+        });
 
-    rl.on('line', (linea) => {
-        let parola = linea.trim();
-        if (parola.length >= 3) {
-            parole.push(parola);
-        }
-    });
+        rl.on('line', (linea) => {
+            let parola = linea.trim();
+            if (parola.length >= 3) {
+                parole.push(parola);
+            }
+        });
 
         rl.on('close', () => {
-            let password = '';
-    
-            do {
-                let parola1 = parole[Math.floor(Math.random() * parole.length)];
-                let parola2 = parole[Math.floor(Math.random() * parole.length)];
-                
-                password = parola1.charAt(0).toUpperCase() + parola1.slice(1) + parola2.charAt(0).toUpperCase() + parola2.slice(1);
-            } while (password.length < 8 || password.length > 10);
-            
-            console.log(`La password generata è: ${password}`);
+            resolve(parole);
         });
-    
 
-} catch (errore) {
-    console.error(`Si è verificato un errore durante la lettura del file: ${errore.message}`);
+        rl.on('error', (err) => {
+            reject(err);
+        });
+    });
 }
+
+generaPassword()
+    .then(password => {
+        console.log(`La password generata è: ${password}`);
+    })
+    .catch(errore => {
+        console.error(`Si è verificato un errore durante la lettura del file: ${errore.message}`);
+    });
