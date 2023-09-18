25e97ae69b5005e1e9628b0836924164324ddf71
ilnuovonumero2 <129592242+ilnuovonumero2@users.noreply.github.com>
2023-09-18 06:37:38 +0200

    feat(m4): add solution exe-007 weird-words

--- projects/m4/007-weird-words/js/index.js
+++ projects/m4/007-weird-words/js/index.js
@@ -8,35 +8,48 @@ if (!nomeFile) {
     process.exit(1);
 }
 
-try {
+async function analizzaFile(nomeFile) {
     let paroleCorrette = new Set();
     let paroleErrate = new Set();
 
-    let rl = readline.createInterface({
-        input: fs.createReadStream(nomeFile),
-        output: process.stdout,
-        terminal: false
-    });
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: fs.createReadStream(nomeFile),
+            terminal: false
+        });
 
-    rl.on('line', (linea) => {
-        let parole = linea.trim().split(/\s+/);
-        parole.forEach((parola) => {
-            if (/ei/.test(parola) && !/cei/.test(parola) || /ie/.test(parola) && /cie/.test(parola)) {
-                paroleErrate.add(parola);
-            } 
-            else if (/ei/.test(parola) || /ie/.test(parola)) {
-                paroleCorrette.add(parola);
-            }
+        rl.on('line', (linea) => {
+            let parole = linea.trim().split(/\s+/);
+            parole.forEach((parola) => {
+                if (/ei/.test(parola) && !/cei/.test(parola) || /ie/.test(parola) && /cie/.test(parola)) {
+                    paroleErrate.add(parola);
+                } else if (/ei/.test(parola) || /ie/.test(parola)) {
+                    paroleCorrette.add(parola);
+                }
+            });
+        });
+
+        rl.on('close', () => {
+            resolve({ paroleCorrette, paroleErrate });
+        });
+
+        rl.on('error', (err) => {
+            reject(err);
         });
     });
+}
 
-    rl.on('close', () => {
+async function main() {
+    try {
+        const { paroleCorrette, paroleErrate } = await analizzaFile(nomeFile);
+
         console.log(`Parole corrette secondo la regola "I prima di E eccetto dopo C":\n${[...paroleCorrette].join(', ')}\n`);
         console.log(`Parole errate secondo la regola "I prima di E eccetto dopo C":\n${[...paroleErrate].join(', ')}\n`);
         console.log(`Numero totale di parole corrette: ${paroleCorrette.size}`);
         console.log(`Numero totale di parole errate: ${paroleErrate.size}`);
-    });
-
-} catch (errore) {
-    console.error(`Si è verificato un errore durante la lettura del file: ${errore.message}`);
+    } catch (errore) {
+        console.error(`Si è verificato un errore durante la lettura del file: ${errore.message}`);
+    }
 }
+
+main();
