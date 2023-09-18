91a494863c151edb9b68cfcec62a291c4017acd1
ilnuovonumero2 <129592242+ilnuovonumero2@users.noreply.github.com>
2023-09-18 06:28:18 +0200

    feat(m4): add solution exe-003 concatenate-multiple-files

--- projects/m4/003-concatenate-multiple-files/js/index.js
+++ projects/m4/003-concatenate-multiple-files/js/index.js
@@ -1,28 +1,26 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
 let args = process.argv.slice(2);
 
-if(args.length === 0) {
+if (args.length === 0) {
     console.error('Error: No files provided.');
     process.exit(1);
 }
 
 function catFiles(files) {
-    
-    if(files.length === 0) return;
+    if (files.length === 0) return;
 
-    
     let file = files[0];
 
-    
-    fs.readFile(file, 'utf8', (err, data) => {
-        if(err) {
-            console.error(`Error: Cannot display file ${file}`);
-        } else {
+    fs.readFile(file, 'utf8')
+        .then(data => {
             console.log(data);
-        }
-        catFiles(files.slice(1));
-    });
+            catFiles(files.slice(1));
+        })
+        .catch(err => {
+            console.error(`Error: Cannot display file ${file}`);
+            catFiles(files.slice(1));
+        });
 }
 
 catFiles(args);
