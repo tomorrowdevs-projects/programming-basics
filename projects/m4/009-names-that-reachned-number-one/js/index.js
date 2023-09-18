032a25016d5bb882c19a1f1869900909e18ee51c
ilnuovonumero2 <129592242+ilnuovonumero2@users.noreply.github.com>
2023-09-18 06:41:10 +0200

    feat(m4): add solution exe-009 names-that-reachned-number-one

--- projects/m4/009-names-that-reachned-number-one/js/index.js
+++ projects/m4/009-names-that-reachned-number-one/js/index.js
@@ -5,44 +5,47 @@ const path = require('path');
 let nomiRagazziPopolari = {};
 let nomiRagazzePopolari = {};
 
-fs.readdirSync('./data').forEach(file => {
-    let rl = readline.createInterface({
-        input: fs.createReadStream(path.join('./data', file)),
-        terminal: false
+function processFile(file) {
+    return new Promise((resolve) => {
+        let rl = readline.createInterface({
+            input: fs.createReadStream(path.join('./data', file)),
+            terminal: false
+        });
+
+        let nomiMaschili = [];
+        let nomiFemminili = [];
+        let year = file.slice(3, 7);
+
+        rl.on('line', (line) => {
+            let [nome, genere, conteggio] = line.split(',');
+            conteggio = parseInt(conteggio);
+
+            if (genere === 'M') {
+                nomiMaschili.push({ name: nome, count: conteggio });
+            } else if (genere === 'F') {
+                nomiFemminili.push({ name: nome, count: conteggio });
+            }
+        });
+
+        rl.on('close', () => {
+            nomiMaschili.sort((a, b) => b.count - a.count);
+            if (nomiMaschili.length > 10) {
+                nomiMaschili = nomiMaschili.slice(0, 10);
+            }
+            nomiRagazziPopolari[year] = nomiMaschili.map(({ name }) => name);
+
+            nomiFemminili.sort((a, b) => b.count - a.count);
+            if (nomiFemminili.length > 10) {
+                nomiFemminili = nomiFemminili.slice(0, 10);
+            }
+            nomiRagazzePopolari[year] = nomiFemminili.map(({ name }) => name);
+
+            resolve();
+        });
     });
+}
 
-    let nomiMaschili = [];
-    let nomiFemminili = [];
-
-    let year = file.slice(3, 7);
-
-    rl.on('line', (line) => {
-        let [nome, genere, conteggio] = line.split(',');
-        conteggio = parseInt(conteggio);
-
-        if (genere === 'M') {
-            nomiMaschili.push({ name: nome, count: conteggio });
-        } else if (genere === 'F') {
-            nomiFemminili.push({ name: nome, count: conteggio });
-        }
-    });
-
-    rl.on('close', () => {
-        nomiMaschili.sort((a, b) => b.count - a.count);
-        if (nomiMaschili.length > 10) {
-            nomiMaschili = nomiMaschili.slice(0, 10);
-        }
-        nomiRagazziPopolari[year] = nomiMaschili.map(({ name }) => name);
-
-        nomiFemminili.sort((a, b) => b.count - a.count);
-        if (nomiFemminili.length > 10) {
-            nomiFemminili = nomiFemminili.slice(0, 10);
-        }
-        nomiRagazzePopolari[year] = nomiFemminili.map(({ name }) => name);
-    });
-});
-
-process.on('exit', () => {
+Promise.all(fs.readdirSync('./data').map(processFile)).then(() => {
     console.log("Nomi più popolari per i ragazzi:");
     for (let year in nomiRagazziPopolari) {
         console.log(`${year}: ${nomiRagazziPopolari[year].join(', ')}`);
