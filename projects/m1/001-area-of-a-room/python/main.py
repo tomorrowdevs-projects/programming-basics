e59a23829b7e6cff6a712743a606f475ff8e9a84
Andrea Cultraro <cultraro.andrea@gmail.com>
2023-09-19 18:33:57 +0200

    solution/m1-001-area-of-the-room

---
+++ projects/m1/001-area-of-a-room/python/main.py
@@ -0,0 +1,8 @@
+print("\nHi! \nThis is a little program to calculate the area of a room, just follow the instruction and enjoy.");
+
+length = float(input("\nInsert length of the room: "));
+width = float(input("Insert width of the room: "));
+
+area = length*width;
+
+print("\nThis is the result:", area);
