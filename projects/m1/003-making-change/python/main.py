043c5a5e33ebea6a10a94f442c3f08fce0a84219
Andrea Cultraro <cultraro.andrea@gmail.com>
2023-10-06 17:27:36 +0200

    solution/003

--- projects/m1/003-making-change/python/main.py
+++ projects/m1/003-making-change/python/main.py
@@ -21,27 +21,21 @@ while rest > 0:
     if rest >= toonie:
         num_toonie += int(rest / toonie)
         rest = rest % toonie
-        print(f"{num_toonie}, {rest}")
     elif rest >= loonie:
         num_loonie += int(rest / loonie)
         rest = rest % loonie
-        print(f"{num_loonie}, {rest}")
     elif rest >= quarter:
         num_quarter += int(rest / quarter)
         rest = rest % quarter
-        print(f"{num_quarter}, {rest}")
     elif rest >= dime:
         num_dime += int(rest / dime)
         rest = rest % dime
-        print(f"{num_dime}, {rest}")
     elif rest >= nickel:
         num_nickel += int(rest / nickel)
         rest = rest % nickel
-        print(f"{num_nickel}, {rest}")
     elif rest >= penny:
         num_penny += int(rest / penny)
         rest = rest % penny
-        print(f"{num_penny}, {rest}")
 
 
 print(f"{cents_user} cents = {num_toonie} toonies, {num_loonie} loonies, "
