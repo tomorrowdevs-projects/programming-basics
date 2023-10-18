96cd92c7e177be4066817e1b32794e249482c237
Andrea Cultraro <cultraro.andrea@gmail.com>
2023-10-18 23:49:37 +0200

    coding challenge

--- projects/workshops/coding challenge/kata.py
+++ projects/workshops/coding challenge/kata.py
@@ -12,31 +12,31 @@ Good luck!
 
 '''
 
-def common(array1, array2, array3):
-
+def common(a, b, c):
+    
     sum = 0
-    array1.sort()
-    array2.sort()
-    array3.sort()
+    # sort arrays to have at position 0 lower number
+    a.sort()
+    b.sort()
+    c.sort()
 
+    # check if in position 0 we have the same number in all three arrays and sum it, 
+    # otherwise we pop the lower number from his array
     while True:
-        if array1[0] == array2[0] == array3[0]:
-            sum += array1[0]
-            array1.pop(0)
-            array2.pop(0)
-            array3.pop(0)
-        elif array1[0] <= array2[0] and array1[0] <= array3[0]:
-            array1.pop(0)
-        elif array2[0] <= array1[0] and array2[0] <= array3[0]:
-            array2.pop(0)
-        elif array3[0] <= array1[0] and array3[0] <= array2[0]:
-            array3.pop(0)
-        
-        if len(array1) == 0 or len(array2) == 0 or len(array3) == 0:
+        if a[0] == b[0] == c[0]:
+            sum += a[0]
+            a.pop(0)
+            b.pop(0)
+            c.pop(0)
+        elif a[0] <= b[0] and a[0] <= c[0]:
+            a.pop(0)
+        elif b[0] <= a[0] and b[0] <= c[0]:
+            b.pop(0)
+        elif c[0] <= a[0] and c[0] <= b[0]:
+            c.pop(0)
+            
+        # when we reach at least one array without number exit    
+        if len(a) == 0 or len(b) == 0 or len(c) == 0:
             break
     
     return sum
-
-
-print(common([1,2,3],[5,3,2],[7,3,2]))
-print(common([1,2,2,3],[5,3,2,2],[7,3,2,2]))
