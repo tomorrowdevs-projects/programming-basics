a125dd5d6ac9c0f140c5732d7bf84348ac4b69c3
flaviopantaleo <flavio.pantaleo@yahoo.com>
2023-12-14 12:40:46 +0100

    controll if user inserts a number

--- projects/m1/001-area-of-a-room/js/index.js
+++ projects/m1/001-area-of-a-room/js/index.js
@@ -1,14 +1,23 @@
-let userWidth = Number(prompt('Insert width of room in meter: (please use . for to divide the decimals)'));
-let userLength  = Number(prompt('Insert length  of room in meter: (please use . for to divide the decimals)'));
+let userWidth = prompt('Insert width of room in meter: (please use . for to divide the decimals)');
+let userLength  = prompt('Insert length  of room in meter: (please use . for to divide the decimals)');
 
 const areaRoom = userLength * userWidth
 
 const area = () => {
+    
+    if (typeof userWidth === 'number' && typeof userLength === 'number') {
 
-    setTimeout(() => {
-        console.log(` your width: ${userWidth} m, your width: ${userLength} m.
-        The area of your room is ${areaRoom} meter2`);
-    }, 2500);
+        setTimeout(() => {
+            console.log(` your width: ${userWidth} m, your width: ${userLength} m.
+            The area of your room is ${areaRoom} meter2`);
+        }, 2500);
+
+    } else {
+        
+        console.log('Sorry u didnt insert numbers.');
+
+    }
+    
 }
 
 area();
