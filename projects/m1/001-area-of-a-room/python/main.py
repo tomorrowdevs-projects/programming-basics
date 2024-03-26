657261e63513577bd64212250dc7593f7e563186
Stefano Agnelli <stefano.agnelli.jobs@gmail.com>
2024-03-27 00:57:58 +0100

    added 1st exercise

---
+++ projects/m1/001-area-of-a-room/python/main.py
@@ -0,0 +1,10 @@
+
+print("insert length in m")
+length=float(input())
+
+print("insert width in m")
+width=float(input())
+
+result=length*width
+
+print("The area of the room is " + str(result) + " sqm")
